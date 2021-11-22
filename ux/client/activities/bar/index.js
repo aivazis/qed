3afa61805440ef72fb8168a5cb137b7cf244bde4
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-11-22 11:45:00 -0800

    ux/activities: replaced} {experiment} with {debug} in the activity {bar}

--- ux/client/activities/bar/index.js
+++ ux/client/activities/bar/index.js
@@ -9,14 +9,14 @@ import React from 'react'
 
 // locals
 // the activities
-import { About, Archive, Compose, Deploy, Experiment, Help, Kill, Visualize } from '~/activities'
+import { About, Debug, Help, Kill } from '~/activities'
 // widgets
 import { Toolbar, Spacer } from '~/widgets'
 // styles
 import styles from './styles'
 
 
-// teh activity bar
+// the activity bar
 const bar = () => {
     // decide on a size
     const size = 32
@@ -24,8 +24,8 @@ const bar = () => {
     // paint me
     return (
         <Toolbar direction="column" style={styles} >
-            <Experiment size={size} style={styles} />
             <Help size={size} style={styles} />
+            <Debug size={size} style={styles} />
 
             <Spacer />
 
