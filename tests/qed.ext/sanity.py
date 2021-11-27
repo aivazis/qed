d2c5c4b73b3326fb91998920a9bd14d2ad339a08
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-11-27 13:27:03 -0800

    qed.ext: primed the bindings test suite

---
+++ tests/qed.ext/sanity.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# the driver
+def test():
+    """
+    Sanity check: attempt to access the bindings
+    """
+    # get the package
+    import qed
+    # verify the bindings are accessible
+    assert qed.libqed
+    # all done
+    return 0
+
+
+# bootstrap
+if __name__ == "__main__":
+    # invoke the driver
+    status = test()
+    # and share the status with the shell
+    raise SystemExit(status)
+
+
+# end of file
