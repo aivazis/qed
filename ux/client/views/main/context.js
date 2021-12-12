e34520498ac3ad4fa14d2a76b0c3de0bfb29aff6
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-12 15:52:35 -0800

    ux/views: {main} is now a context provider, currently of a flag that controls whether the activity specific panel should be shown

---
+++ ux/client/views/main/context.js
@@ -0,0 +1,47 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// the error message to show consumers that are not nested within a provider
+const complaint = "while accessing the 'main' context: no provider"
+
+// setup the flex context
+export const Context = React.createContext(
+    // the default value that consumers see when accessing the context outside a provider
+    {
+        // the activity panel state and its mutator
+        activityPanel: null,
+        setActivityPanel: () => { throw new Error(complaint) },
+    }
+)
+
+
+// the provider factory
+export const Provider = ({
+    // children
+    children
+}) => {
+    // setup the activity panel flag
+    const [activityPanel, setActivityPanel] = React.useState(true)
+    // build the current value of the context
+    const context = {
+        // the activity panel state flag and its mutator
+        activityPanel, setActivityPanel,
+    }
+
+    // provide for my children
+    return (
+        <Context.Provider value={context} >
+            {children}
+        </Context.Provider >
+
+    )
+}
+
+
+// end of file
---
+++ ux/client/views/main/useActivityPanel.js
@@ -0,0 +1,34 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+// locals
+// context
+import { Context } from './context'
+
+
+// flex support
+export const useActivityPanel = () => {
+    // grab the state and its mutator
+    const { activityPanel, setActivityPanel } = React.useContext(Context)
+
+    // the state managers
+    const showActivityPanel = () => { setActivityPanel(true) }
+    const hideActivityPanel = () => { setActivityPanel(false) }
+    const toggleActivityPanel = () => { setActivityPanel(old => !old) }
+
+    // build and return the context relevant to the activity panel
+    return {
+        // the flag
+        activityPanel,
+        // and its mutators
+        showActivityPanel, hideActivityPanel, toggleActivityPanel,
+    }
+}
+
+
+// end of file
