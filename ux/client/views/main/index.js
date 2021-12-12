2f790751103a9c4075a81290baebf55f2202faa3
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-12 15:54:25 -0800

    ux/views: exported the {main} view renderer and its context hooks

--- ux/client/views/main/index.js
+++ ux/client/views/main/index.js
@@ -4,40 +4,10 @@
 // (c) 1998-2021 all rights reserved
 
 
-// externals
-// framework
-import React from 'react'
-// routing
-import { Outlet } from 'react-router-dom'
-
-// locals
-import styles from './styles'
-// view
-import { Status } from '~/views'
-// activities
-import { ActivityBar } from '~/activities'
-
-
-// the main app working area
-// the layout is simple: the activity bar and activity dependent routing
-const Panel = () => {
-    // lay out the main page
-    return (
-        <section style={styles.page} >
-            <section style={styles.panel} >
-                {/* navigation bar */}
-                <ActivityBar style={styles.activitybar} />
-                {/* the client area */}
-                <Outlet />
-            </section >
-            <Status />
-        </section >
-    )
-}
-
-
-// publish
-export default Panel
+// publish the view
+export { Main } from './main'
+// and the hooks
+export { useActivityPanel } from './useActivityPanel'
 
 
 // end of file
