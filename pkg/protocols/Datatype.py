a6be4195f7e8d817dd3a8f65a3be6f492962a224
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-17 14:53:20 -0800

    qed.protocols: {datatype} got a {byteswap} flag and logic to process a byte order marker in its specification

--- pkg/protocols/Datatype.py
+++ pkg/protocols/Datatype.py
@@ -4,6 +4,8 @@
 # (c) 1998-2021 all rights reserved
 
 
+# externals
+import sys
 # support
 import qed
 # my superclass
@@ -13,17 +15,72 @@ from .Specification import Specification
 # the dataset layout
 class Datatype(Specification, family="qed.datatypes"):
     """
-    The datatype metadats
+    The datatype metadata
     """
 
 
     # public data
-    # individual metadata, used to assemble a default layout
-    cell = qed.properties.str()
-    cell.doc = "the format string that specifies the type of the dataset payload"
+    byteswap = qed.properties.bool()
+    byteswap.doc = "control whether byte swapping is necessary"
 
     channels = qed.properties.strings()
     channels.doc = "the names of the channels provided by this datatype"
+
+
+    # value processing hooks
+    # the byte order is indicated by a single character prefix that must be stripped before
+    # normal name resolution can take place; this is done in {pyre_convert}. the byte order
+    # choice is applied after {pyre_instantiate} realizes the {datatype}
+    @classmethod
+    def pyre_convert(cls, value, **kwds):
+        # check whether there is a byte order specifier
+        if value[0] in cls.markers:
+            # and strip it; we'll get a chance to handle this later
+            value = value[1:]
+        # run it through the table of aliases
+        value = cls.aliases.get(value, value)
+        # chain up
+        return super().pyre_convert(value=value, **kwds)
+
+
+    @classmethod
+    def pyre_instantiate(cls, spec, component, name, locator):
+        # chain up to build the {datatype} instance
+        instance = super().pyre_instantiate(spec=spec,
+                                           component=component, name=name, locator=locator)
+
+        # the byte order marker is at the beginning of the {spec}
+        marker = spec[0]
+        # look it up and fall back to the default setting of the datatype
+        instance.byteswap = cls.byteorder.get(marker, instance.byteswap)
+
+        # return
+        return instance
+
+
+    # implementation details
+    # the type alias table
+    aliases = {
+        # single precision complex numbers
+        "c4": "qed.datatypes.complex4",
+        "complex4": "qed.datatypes.complex4",
+
+        # double precision complex numbers
+        "c8": "qed.datatypes.complex8",
+        "complex8": "qed.datatypes.complex8",
+    }
+
+    # the byte order truth table
+    byteorder = {
+        "<": sys.byteorder == "big",
+        ">": sys.byteorder == "little",
+        "!": sys.byteorder == "little",
+        "@": False,
+        "=": False,
+    }
+
+    # the set of byte order markers
+    markers = tuple(byteorder.keys())
 
 
 # end of file
