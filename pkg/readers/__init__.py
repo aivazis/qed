97d26bf7ba6158c9fd8c5ee5adccbc0b5874d178
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-17 21:47:26 -0800

    qed.readers: primed the {isce2.rslc} reader; it only has metadata, for now

---
+++ pkg/readers/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# publish
+from . import isce2
+
+
+# end of file
---
+++ pkg/readers/isce2/RSLC.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2021 all rights reserved
+
+
+# support
+import qed
+
+
+# the RSLC reader
+class RSLC(qed.flow.factory, family="qed.readers.isce2.rslc", implements=qed.protocols.reader):
+    """
+    The reader of RSLC files
+    """
+
+
+    # public data
+    uri = qed.properties.path()
+    uri.doc = "the uri of the data source"
+
+    shape = qed.properties.tuple(schema=qed.properties.int())
+    shape.doc = "the size of the dataset in (lines, samples)"
+
+    datasets = qed.properties.list(schema=qed.protocols.dataset.output())
+    datasets.doc = "the list of data sets provided by the reader"
+
+
+    # metamethods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+
+        # there is only one dataset in the file
+        # so build it and attach it
+
+        # all done
+        return
+
+
+# end of file
