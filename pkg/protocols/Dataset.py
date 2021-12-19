30c30e2266836669014caa7b1d7cf13e3c96bcb3
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-19 15:34:04 -0800

    qed.protocols: a {dataset} now has a {selector}, a key/value table of selector names and their values that identify a dataset to its reader

--- pkg/protocols/Dataset.py
+++ pkg/protocols/Dataset.py
@@ -34,7 +34,7 @@ class Dataset(Specification, family="qed.datasets"):
     shape = qed.properties.tuple(schema=qed.properties.int())
     shape.doc = "the shape of the dataset"
 
-    selector = qed.properties.catalog()
+    selector = qed.properties.kv()
     selector.doc = "a key/value map that identifies the datatset to a reader"
 
 
