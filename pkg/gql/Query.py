d3fe8c995d01da5df92af6886bd6fa522fdaab09
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-22 13:32:52 -0800

    qed.gql: the main {query} now understands a request for known {readers} and responds with a {connection}

--- pkg/gql/Query.py
+++ pkg/gql/Query.py
@@ -12,7 +12,6 @@ import qed
 from .Version import Version
 # the known datasets
 from .ReaderConnection import ReaderConnection
-from .Datasets import Datasets
 
 
 # the query
@@ -22,36 +21,22 @@ class Query(graphene.ObjectType):
     """
 
     # known datasets
-    datasets = graphene.Field(Datasets, required=True)
     readers = graphene.relay.ConnectionField(ReaderConnection)
     # server version info
     version = graphene.Field(Version, required=True)
 
 
     # datasets
-    def resolve_readers(root, info, **kwds):
+    def resolve_readers(root, info,
+                        first=1, last=None, after=None, before=None,
+                        **kwds):
         """
         Generate a list of all known readers
         """
         # grab the plexus
         plexus = info.context["plexus"]
-        # get a list of the dataset readers and pass them on
-        return list(plexus.datasets)
-
-
-    def resolve_datasets(root, info, **kwds):
-        """
-        Generate a list of all known datasets
-        """
-        # this resolver must exist; its job is to build an object that gets handed to the
-        # {Datasets} resolvers; here we would prep such an object using the query execution
-        # context and the variable bindings in {kwds}, but for {Datasets} this is not necessary
-        #
-        #     root: should be {None}; this is the root
-        #     info: has {.context} with whatever was built by the executioner
-        #     kwds: contains the variable bindings for this resolution
-        #
-        return kwds
+        # get the datasets and return them
+        return plexus.datasets
 
 
     # version
