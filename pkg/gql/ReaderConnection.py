df2509378123d1652a6ab0590d4818f0b0b06be3
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-22 13:32:00 -0800

    qed.gql: added {count} to the connection over {readers}

--- pkg/gql/ReaderConnection.py
+++ pkg/gql/ReaderConnection.py
@@ -14,13 +14,26 @@ from .Reader import Reader
 # a reader connection
 class ReaderConnection(graphene.relay.Connection):
     """
-    A connection to a list of dataset readers
+    A customized connection to a list of dataset readers
     """
 
     # {graphene} metadata
     class Meta:
         # register my node type
         node = Reader
+
+
+    # my extra fields
+    count = graphene.Int()
+
+
+    # resolvers
+    def resolve_count(connection, info, *_):
+        """
+        Count the number of readers
+        """
+        # get the number of registered {readers} from the {plexus}
+        return len(info.context["plexus"].datasets)
 
 
 # end of file
