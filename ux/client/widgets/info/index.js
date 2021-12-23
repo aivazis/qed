4cf9521cd8fc566ae48d71e49ab9ad11408360c9
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-22 17:23:33 -0800

    ux/widgets: added {info}, a structured way to render a {name,value} pair

---
+++ ux/client/widgets/info/index.js
@@ -0,0 +1,32 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+// locals
+import styles from './styles'
+
+
+// a widget that can act as a header or a title
+export const Info = ({ name, value, style }) => {
+    // mix my styles
+    const infoStyle = { ...styles.info, ...style?.info }
+    const nameStyle = { ...styles.name, ...style?.name }
+    const valueStyle = { ...styles.value, ...style?.value }
+    const separatorStyle = { ...styles.separator, ...style?.separator }
+
+    // paint me
+    return (
+        <div style={infoStyle} >
+            <span style={nameStyle}>{name}</span>
+            <span style={separatorStyle}>:</span>
+            <span style={valueStyle}>{value}</span>
+        </div>
+    )
+}
+
+
+// end of file
---
+++ ux/client/widgets/info/styles.js
@@ -0,0 +1,45 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// get colors
+import { wheel, theme } from '~/palette'
+
+
+// publish
+export default {
+
+    info: {
+        // positioning
+        padding: "0.125rem 1.0em",
+
+        // colors
+        color: theme.header.color,
+
+        // text
+        fontSize: "50%",
+        fontFamily: "rubik-light",
+
+        // disable text selection
+        userSelect: "none",
+    },
+
+    name: {
+        textTransform: "uppercase",
+    },
+
+    value: {
+        color: wheel.gray.aluminum,
+    },
+
+    separator: {
+        display: "inline-block",
+        width: "1.0em",
+        textAlign: "center",
+    }
+}
+
+
+// end of file
