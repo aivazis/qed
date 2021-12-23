706032a18e4a977b3f19480a6f356d5d0e82dbd4
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-22 18:30:07 -0800

    ux/activities: dimmed the {bar} by a bit

--- ux/client/activities/bar/styles.js
+++ ux/client/activities/bar/styles.js
@@ -13,7 +13,7 @@ export default {
     // for my toolbar
     box: {
         // paint
-        backgroundColor: "hsl(0deg, 0%, 21%, 1)",
+        backgroundColor: "hsl(0deg, 0%, 15%, 1)",
     },
 
     // my opinions on badges
