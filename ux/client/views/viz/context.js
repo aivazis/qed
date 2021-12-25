c9170d516a22252f9bda15cfe45493621726c5a8
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-25 14:50:07 -0800

    ux/views: in {viz}, added {context} management of the active view contents

--- ux/client/views/viz/context.js
+++ ux/client/views/viz/context.js
@@ -18,7 +18,11 @@ export const Context = React.createContext(
         views: [],
         splitView: () => { throw new Error(complaint) },
         collapseView: () => { throw new Error(complaint) },
-
+        // the active view
+        activeView: null,
+        setActiveView: () => { throw new Error(complaint) },
+        // update the contents of the active view
+        viewChannel: () => { throw new Error(complaint) },
     }
 )
 
@@ -30,14 +34,37 @@ export const Provider = ({
 }) => {
     // setup the views
     const [views, setViews] = React.useState(new Array())
+    // the active is an index into the {views}
+    const [activeView, setActiveView] = React.useState(0)
+
     // split a view
     const splitView = (view) => { }
+    // collapse a view
     const collapseView = (view) => { }
+
+    // update the contents of the active view
+    const viewChannel = (channel) => {
+        // adjust the view
+        setViews((old) => {
+            // make a copy of the old state
+            const clone = [...old]
+            // adjust the entry that corresponds to the active view
+            clone[activeView] = channel
+            // and hand off the new state
+            return clone
+        })
+        // all done
+        return
+    }
 
     // build the current value of the context
     const context = {
         // the views
         views, collapseView, splitView,
+        // active view
+        activeView, setActiveView,
+        // modify the contents of the active view
+        viewChannel,
     }
 
     // provide for my children
