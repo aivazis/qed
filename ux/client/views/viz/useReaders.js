75afe9f4bda083ded0b142e99ef6cfd57b59251b
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-26 11:37:53 -0800

    ix/views: added {useReaders}, a {viz} hook to access the results of the {readers} query

---
+++ ux/client/views/viz/useReaders.js
@@ -0,0 +1,24 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// hook to pull the dataset readers out the outlet context
+export const useReaders = () => {
+    // grab the readers
+    const { readers } = React.useContext(Context)
+    // and return them
+    return readers
+}
+
+
+// end of file
