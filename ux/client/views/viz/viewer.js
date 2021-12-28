f080fa7cf03930bdff2de526cb06079621cb06ec
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-28 09:58:32 -0800

    ux/views: primed the {viz} data {viewer}

---
+++ ux/client/views/viz/viewer.js
@@ -0,0 +1,26 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// locals
+// components
+import { Blank } from './blank'
+// styles
+import styles from './styles'
+
+
+// display the datasets associated with this reader
+export const Viewer = () => {
+    // render
+    return (
+        <Blank />
+    )
+}
+
+
+// end of file
