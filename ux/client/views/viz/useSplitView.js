bedb251c5bcca9432e654eccef018392e9105cd1
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-29 23:34:58 -0800

    ux/views: {viz} now has {useSplitView} that builds a handler to split a given view

---
+++ ux/client/views/viz/useSplitView.js
@@ -0,0 +1,40 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// access to the registered views
+export const useSplitView = view => {
+    // grab the list of {views} from context
+    const { setViews, setActiveView } = React.useContext(Context)
+    // make a handler that adds a new blank view after a given on
+    const splitView = () => {
+        // adjust the view
+        setViews(old => {
+            // make a copy of the old state
+            const clone = [...old]
+            // make a copy of the current view
+            clone.splice(view + 1, 0, old[view])
+            // and make it the active one
+            setActiveView(view + 1)
+            // and hand off the new state
+            return clone
+        })
+        // all done
+        return
+    }
+    // and return it
+    return splitView
+}
+
+
+// end of file
