00b11364f46ec72018b7f66ecabb1550f2823005
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-29 17:24:00 -0800

    qed.readers: the {nisar.rslc} reader now autogenerates {datasets} with all possible {selector} bindings

--- pkg/readers/nisar/RSLC.py
+++ pkg/readers/nisar/RSLC.py
@@ -4,6 +4,8 @@
 # (c) 1998-2021 all rights reserved
 
 
+# external
+import itertools
 # support
 import qed
 
@@ -31,6 +33,33 @@ class RSLC(qed.flow.factory, family="qed.readers.nisar.rslc", implements=qed.pro
 
     datasets = qed.properties.list(schema=qed.protocols.dataset.output())
     datasets.doc = "the list of data sets provided by the reader"
+
+
+    # metamethods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+
+        # grab my selectors
+        selectors = self.selectors
+        # autogenerate all possible selector bindings
+        bindings = (
+            dict(zip(selectors.keys(), values))
+            for values in itertools.product(*selectors.values())
+        )
+        # NISAR RSLC files contain up to eight datasets; autogenerate them, for now
+        for selector in bindings:
+            # make a dataset
+            dataset = qed.datasets.raw()
+            # decorate it
+            dataset.shape = self.shape
+            dataset.cell = qed.datatypes.complex4()
+            dataset.selector = selector
+            # and attach it
+            self.datasets.append(dataset)
+
+        # all done
+        return
 
 
 # end of file
