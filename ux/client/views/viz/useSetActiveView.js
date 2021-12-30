79a5f40c5a71bfe7073629db77717052e94090de
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-12-29 23:34:23 -0800

    ux/views: {viz} now has a {useSetActiveView} hook that builds a handler that sets the active view

---
+++ ux/client/views/viz/useSetActiveView.js
@@ -0,0 +1,31 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2021 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// access to the registered views
+export const useSetActiveView = view => {
+    // grab the list of {views} from context
+    const { setActiveView } = React.useContext(Context)
+    // make a handler that makes {view} the acive one
+    const activate = () => {
+        // easy enough
+        setActiveView(view)
+        // all done
+        return
+    }
+    // and return it
+    return activate
+}
+
+
+// end of file
