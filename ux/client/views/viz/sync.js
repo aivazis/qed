3d402e837ef19f4cb007ed163f1a148db4fe4ba0
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-13 13:17:41 +0200

    ux/views: the {sync} button on the {tab} of a {viz} data viewer no longer requires a ref to the {viewport}, now that that {synced} is back to holding flags

--- ux/client/views/viz/sync.js
+++ ux/client/views/viz/sync.js
@@ -22,11 +22,11 @@ import styles from './styles'
 
 
 // display the datasets associated with this reader
-export const Sync = ({ idx, viewport }) => {
+export const Sync = ({ idx }) => {
     // look up the current sync state of the {viewport}
     const isSynced = useGetViewportSync(idx)
     // build the sync toggle
-    const toggle = useToggleViewportSync(idx, viewport)
+    const toggle = useToggleViewportSync(idx)
 
     // my event handlers
     const behaviors = {
--- ux/client/views/viz/useToggleViewportSync.js
+++ ux/client/views/viz/useToggleViewportSync.js
@@ -12,12 +12,12 @@ import React from 'react'
 import { VizContext } from './vizContext'
 
 
-// viewport registration
-export const useToggleViewportSync = (idx, viewport) => {
+// toggle the sync status of this viewport
+export const useToggleViewportSync = idx => {
     // grab the sync table mutator from the context
     const { setSynced } = React.useContext(VizContext)
 
-    // a handler that toggles whether this {viewport} is synced to the global camera
+    // a handler that toggles whether this viewport is synced to the global camera
     const toggle = () => {
         // update the sync table
         setSynced(old => {
