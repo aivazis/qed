3fd7355e81a6e9fe73a4c87503f4a287862a56e1
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-14 14:02:15 +0200

    ux/views: viewport initialization is now in its own hook that should be invoked only by {viz} to get things rolling on every render

---
+++ ux/client/views/viz/useInitializeViewports.js
@@ -0,0 +1,30 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { VizContext } from './vizContext'
+
+
+// storage for the refs to the {mosaic} placemats that frame the visible part of the data
+export const useInitializeViewports = () => {
+    // grab the list of {views} and the array of refs from my context
+    const { views, viewports } = React.useContext(VizContext)
+
+    // initialize the array
+    viewports.current = new Array(views.length).fill(null)
+    // build the ref registrar
+    const registrar = idx => ref => viewports.current[idx] = ref
+
+    // and return them
+    return { viewports: viewports.current, registrar }
+}
+
+
+// end of file
