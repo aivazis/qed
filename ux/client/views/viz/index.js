441ab66577def4d30357b2ded12fac97d80c371e
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-17 14:14:48 +0200

    ux/views: exported the main {viz} components

---
+++ ux/client/views/viz/index.js
@@ -0,0 +1,14 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// export the container
+export { Viz } from './viz'
+// and the activity specific panels so the main page can use them as outlets
+export { Controls } from './controls'
+export { Datasets } from './datasets'
+
+
+// end of file
