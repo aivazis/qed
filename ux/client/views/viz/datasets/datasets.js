355867db0315a4471aaf05c999d3088288f6cc83
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-17 14:12:32 +0200

    ux/views: primed the {viz} panel that displays the available {datasets}

---
+++ ux/client/views/viz/datasets/datasets.js
@@ -0,0 +1,41 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// project
+// widgets
+import { Header } from '~/widgets'
+
+// locals
+// hooks
+import { useReaders } from '../viz/useReaders'
+// components
+import { Reader } from '../reader'
+// styles
+import styles from './styles'
+
+
+// export the activity panel
+export const Datasets = () => {
+    // get the readers
+    const readers = useReaders()
+    // render
+    return (
+        <>
+            {/* the title of the panel */}
+            <Header title="datasets" style={styles.header} />
+            {/* go through the readers and render them */}
+            {readers.edges.map(edge => (
+                <Reader key={edge.node.uuid} reader={edge.node} />
+            ))}
+        </>
+    )
+}
+
+
+// end of file
