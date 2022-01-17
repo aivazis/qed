f2ee5ea81ef9ffd1eb3b5c1ac68738f1ea410b78
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-17 14:14:27 +0200

    ux/views: primed the {viz} dataset {viewport}

---
+++ ux/client/views/viz/viewport/viewport.js
@@ -0,0 +1,14 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// export the data viewport
+export const Viewport = () => {
+    // nothing yet
+    return null
+}
+
+
+// end of file
