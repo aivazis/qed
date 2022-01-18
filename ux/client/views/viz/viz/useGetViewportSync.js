f7de5254f445b8484d83d67cd15f1f49d22fd6df
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-18 12:22:10 +0200

    ux/views: added the {viz} hooks that enabled syncing the registered viewports

---
+++ ux/client/views/viz/viz/useGetViewportSync.js
@@ -0,0 +1,24 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// viewport registration
+export const useGetViewportSync = (viewport) => {
+    // grab the sync table
+    const { synced } = React.useContext(Context)
+    // and return the state of the {viewport}
+    return synced[viewport]
+}
+
+
+// end of file
---
+++ ux/client/views/viz/viz/useToggleViewportSync.js
@@ -0,0 +1,40 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// toggle the sync status of this viewport
+export const useToggleViewportSync = idx => {
+    // grab the sync table mutator from the context
+    const { setSynced } = React.useContext(Context)
+
+    // a handler that toggles whether this viewport is synced to the global camera
+    const toggle = () => {
+        // update the sync table
+        setSynced(old => {
+            // make a copy of the old table
+            const table = [...old]
+            // add flip the viewport status
+            table[idx] = !old[idx]
+            // return the new table
+            return table
+        })
+        // all done
+        return
+    }
+
+    // return the toggle
+    return toggle
+}
+
+
+// end of file
