e827851d7c0ab2bd580af4fd73a13621a668c6aa
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-18 14:01:46 +0200

    ux/views: activated the new {viz} implementation

--- ux/client/views/index.js
+++ ux/client/views/index.js
@@ -15,7 +15,7 @@ export { Main, useActivityPanel } from './main'
 
 // sandboxes
 export { GiQL } from './graphiql'
-export { Viz, Controls, Datasets } from './oldviz'
+export { Viz, Controls, Datasets } from './viz'
 
 // a blank panel with size information
 export Blank from './blank'
