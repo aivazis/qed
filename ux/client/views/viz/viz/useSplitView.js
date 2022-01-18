162320ec5124b3eb91c8fecfdbac87f50ab14955
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-18 10:56:11 +0200

    ux/views: added {useSplitView}, a {viz} hook that splits a given {flex} panel in two

---
+++ ux/client/views/viz/viz/useSplitView.js
@@ -0,0 +1,50 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// access to the registered views
+export const useSplitView = view => {
+    // grab the list of {views} from context
+    const { setViews, setSynced, setActiveViewport } = React.useContext(Context)
+    // make a handler that adds a new blank view after a given on
+    const splitView = () => {
+        // add a new view to the pile
+        setViews(old => {
+            // make a copy of the old state
+            const clone = [...old]
+            // the new view is a copy of the view being split
+            clone.splice(view + 1, 0, old[view])
+            // all done
+            return clone
+        })
+        // initialize its sync status
+        setSynced(old => {
+            // make a copy of the old table
+            const table = [...old]
+            // add the new viewport at {idx} with a default state
+            table.splice(view + 1, 0, false)
+            // return the new table
+            return table
+        })
+        // activate the new view
+        setActiveViewport(view + 1)
+
+        // all done
+        return
+    }
+    // and return it
+    return splitView
+}
+
+
+// end of file
