4a9e00fa0a79701b0a42299fe5f15254e69ba4b8
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-18 13:43:40 +0200

    ux/views: primed the {viz} dataset {view} styles

---
+++ ux/client/views/viz/viewport/styles.js
@@ -0,0 +1,23 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// get colors
+import { theme, wheel } from '~/palette'
+// and the base styles
+import styles from '../styles'
+
+
+const viewport = {
+
+}
+
+// publish
+export default {
+    viewport,
+}
+
+
+// end of file
