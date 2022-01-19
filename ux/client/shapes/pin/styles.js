4439647e16b481ebe2b0997f8587de671b588823
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-19 14:05:12 +0200

    ux/shapes: added {pin}

---
+++ ux/client/shapes/pin/styles.js
@@ -0,0 +1,54 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// get colors
+import { theme } from '~/palette'
+
+// the base style
+import style from '~/shapes/styles'
+
+
+// the shape color
+const ink = "hsl(0deg, 0%, 90%)"
+const paint = "hsl(0deg, 0%, 70%)"
+
+// paint mixer
+const pin = (client) => ({
+    // the main shape
+    icon: {
+        // inherit
+        ...style.icon,
+        // stroke
+        stroke: ink,
+        strokeWidth: "1px",
+        // fill
+        fill: "none",
+        // plus whatever the {client} said
+        ...client?.icon,
+    },
+
+    // decorative touches
+    decoration: {
+        // inherit
+        ...style.decoration,
+        // stroke
+        stroke: ink,
+        strokeWidth: "0.5px",
+        // fill
+        fill: "none",
+        // plus whatever the {client} said
+        ...client?.decoration,
+    },
+})
+
+
+// publish
+export default {
+    pin,
+}
+
+
+// end of file
