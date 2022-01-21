cd16b24ff795671e24cee7a28d7604aee23764d2
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-21 10:16:48 +0200

    qed.nisar: adjusted the {rslc} family name to refelct its new home

--- pkg/nisar/readers/RSLC.py
+++ pkg/nisar/readers/RSLC.py
@@ -11,7 +11,7 @@ import qed
 
 
 # the RSLC reader
-class RSLC(qed.flow.factory, family="qed.readers.nisar.rslc", implements=qed.protocols.reader):
+class RSLC(qed.flow.factory, family="qed.nisar.readers.rslc", implements=qed.protocols.reader):
     """
     The reader of RSLC files
     """
