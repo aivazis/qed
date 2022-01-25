120540e474bfc7ce59da37e0903c7fec38a4861f
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-25 12:24:39 +0200

    ux/widgets: added {meta} hooks to provide access to the detail {range} and the current value

---
+++ ux/client/widgets/meta/useDetail.js
@@ -0,0 +1,24 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// access to the current detail level
+export const useDetail = () => {
+    // grab the current detail level
+    const { detail, setDetail } = React.useContext(Context)
+    // and return it
+    return { detail, setDetail }
+}
+
+
+// end of file
---
+++ ux/client/widgets/meta/useRange.js
@@ -0,0 +1,26 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// access to the detail range
+export const useRange = () => {
+    // grab the detail range
+    const { range } = React.useContext(Context)
+    // unpack
+    const [min, max] = range
+    // and return it
+    return { min, max }
+}
+
+
+// end of file
