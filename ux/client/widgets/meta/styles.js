3cbca59733adea3c69c1f945ae9071d20b9a6ac3
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-01-25 12:27:54 +0200

    ux/widgets: the {meta} styling now has a state dependent mixer for the paint of its controls

--- ux/client/widgets/meta/styles.js
+++ ux/client/widgets/meta/styles.js
@@ -8,41 +8,91 @@
 import { wheel, theme } from '~/palette'
 
 
-// publish
-export default {
-    box: {},
+// paint
+const box = {}
 
-    entry: {
-        cursor: "default",
-        lineHeight: "150%",
-        verticalAlign: "baseline",
-    },
-
-    attribute: {
-        textTransform: "uppercase",
-        textAlign: "right",
-        padding: "0.0em 0.0em 0.0em 0.5em",
-        width: "30%",
-    },
-
-    separator: {
-        width: "1.0em",
-        padding: "0.0em 0.25em 0.0em 0.25em",
-        textAlign: "center",
-    },
-
-    value: {
-        // layout
-        padding: "0.0em 0.5em 0.0em 0.0em",
-        // for my children
-        textAlign: "left",
-        textOverflow: "ellipsis",
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-    },
-
+const entry = {
+    cursor: "default",
+    lineHeight: "150%",
+    verticalAlign: "baseline",
 }
 
+const attribute = {
+    textTransform: "uppercase",
+    textAlign: "right",
+    padding: "0.0em 0.0em 0.0em 0.5em",
+    width: "30%",
+}
+
+const separator = {
+    width: "1.0em",
+    padding: "0.0em 0.25em 0.0em 0.25em",
+    textAlign: "center",
+}
+
+const value = {
+    // layout
+    padding: "0.0em 0.5em 0.0em 0.0em",
+    // for my children
+    textAlign: "left",
+    textOverflow: "ellipsis",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+}
+
+const detail = {
+    box: {
+        fontSize: "75%",
+        fontWeight: "normal",
+        textAlign: "right",
+        textTransform: "lowercase",
+        padding: "0.25em 0.0em",
+        cursor: "default",
+    },
+
+    control: {
+        // basic style
+        base: {
+            fontFamily: "rubik-light",
+            textTransform: "uppercase",
+            display: "inline-block",
+            margin: "0.0em 0.5em",
+            color: wheel.gray.aluminum,
+        },
+        // when disabled
+        disabled: {
+            opacity: 0.25,
+        },
+        // when enabled
+        enabled: {
+            cursor: "pointer",
+        },
+        // when highlighted
+        available: {
+            color: theme.page.name,
+        },
+    },
+}
+
+
+// mixers
+const detailControl = (state, polish) => ({
+    ...detail.control.base,
+    ...detail.control[state],
+    ...(polish ? detail.control.available : {})
+})
+
+
+// publish
+export default {
+    attribute,
+    box,
+    detail,
+    detailControl,
+    entry,
+    separator,
+    value,
+}
 
 // end of file
