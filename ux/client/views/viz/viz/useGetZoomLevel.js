f221408c63e54ac11a83ad9ecdcc36ee66e60ce1
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-07 11:55:07 -0800

    ux/views: the {viz} context now has hooks to get and set the zoom level of the active viewport

---
+++ ux/client/views/viz/viz/useGetZoomLevel.js
@@ -0,0 +1,25 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// toggle the sync status of this viewport
+export const useGetZoomLevel = () => {
+    // grab the active viewport and the zoom table
+    const { activeViewport, zoom } = React.useContext(Context)
+
+    // return the zoom level of the active viewport
+    return zoom[activeViewport]
+}
+
+
+// end of file
---
+++ ux/client/views/viz/viz/useSetZoomLevel.js
@@ -0,0 +1,40 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// set the zoom level of the active viewport
+export const useSetZoomLevel = () => {
+    // grab the active viewport and the zoom level table mutator
+    const { activeViewport, setZoom } = React.useContext(Context)
+
+    // a handler that sets the zoom level of the active viewport
+    const set = (value) => {
+        // update the sync table
+        setZoom(old => {
+            // make a copy of the old table
+            const table = [...old]
+            // adjust the entry that corresponds to the active view
+            table[activeViewport] = value
+            // return the new table
+            return table
+        })
+        // all done
+        return
+    }
+
+    // return the handler
+    return set
+}
+
+
+// end of file
