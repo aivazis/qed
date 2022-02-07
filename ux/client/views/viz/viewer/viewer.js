4d4676bc35260b9ab6409b9efcd3f46f23749fdf
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-07 08:29:54 -0800

    ux/views: the {viz} dataset {viewer} now threads the {zoom} level down to its {viewport}

--- ux/client/views/viz/viewer/viewer.js
+++ ux/client/views/viz/viewer/viewer.js
@@ -21,7 +21,7 @@ import styles from './styles'
 
 
 // export the data viewer
-export const Viewer = ({ viewport, view, registrar }) => {
+export const Viewer = ({ viewport, view, zoom, registrar }) => {
     // unpack the view
     const { reader, dataset, channel } = view
     // check for the trivial cases
@@ -80,7 +80,8 @@ export const Viewer = ({ viewport, view, registrar }) => {
             </Meta.Table>
 
             {/* the viewport */}
-            <Viewport viewport={viewport} view={view} uri={base} registrar={registrar} />
+            <Viewport viewport={viewport} view={view} zoom={zoom}
+                uri={base} registrar={registrar} />
         </>
     )
 }
