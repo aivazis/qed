fe4027bb66bb2bf63bdb4870db0d9a32f9b821f2
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-08 11:55:46 -0800

    ux/views: simplified the {zoom} {controller} by flattening its parts and migrating the design to {dimensions}

--- ux/client/views/viz/zoom/controller.js
+++ ux/client/views/viz/zoom/controller.js
@@ -8,48 +8,25 @@
 import React from "react"
 
 // locals
-// hooks
-import { useGetZoomLevel } from "../viz/useGetZoomLevel"
 // components
-import { Indicator } from "./indicator"
-import { Legend } from "./legend"
-// styles
-import styles from './styles'
+import { Axis } from "./axis"
+import { Indicator } from './indicator'
+import { Labels } from './labels'
+import { MajorTickmarks } from './majorTickmarks'
 
 
 // the zoom controller
-export const Controller = ({ state }) => {
-    // the zoom level of the active viewport
-    const zoom = useGetZoomLevel()
-
-    // pick a number of ticks
-    const ticks = 5
-
-    // set up a scale
-    const width = 1000
-    const height = width / 2
-
-    // set a margin
-    const margin = width / 10
-    // figure out the spacing among tick marks
-    const spacing = (width - 2 * margin) / (ticks - 1)
-
-    // the indicator {x} coordinate is tied to the {zoom} level
-    const x = margin + zoom * spacing
-    // the indicator {y} coordinate is fixed
-    const y = height / 2 - height / 8
-
-    // render
+export const Controller = ({ state, geometry }) => {
+    // get my intrinsic length scale out of the control {geometry}
+    const { ils } = geometry
+    // assemble the parts and render them
     return (
-        <>
-            {/* the static background */}
-            <Legend state={state} ticks={ticks}
-                width={width} height={height} margin={margin} spacing={spacing} />
-            {/* the movable value indicator */}
-            <g transform={`translate(${x} ${y})`}>
-                <Indicator state={state} width={75} height={60} />
-            </g>
-        </>
+        <g transform={`scale(${ils}) translate(0 50)`}>
+            <MajorTickmarks geometry={geometry} />
+            <Axis geometry={geometry} />
+            <Indicator geometry={geometry} state={state} />
+            <Labels geometry={geometry} state={state} />
+        </g>
     )
 }
 
