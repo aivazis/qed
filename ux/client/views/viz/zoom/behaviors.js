277d315294f6f89189d1180c334b4111297bce71
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-08 21:28:56 -0800

    ux/views: added {behaviors} to the {zoom} controller with the event listeners necessary to activate dragging the {indicator} to change the zoom level

---
+++ ux/client/views/viz/zoom/behaviors.js
@@ -0,0 +1,72 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// project
+// hooks
+import { useEvent } from '~/hooks'
+
+// local
+// hooks
+import { useZooming } from './useZooming'
+import { useEndZooming } from './useEndZooming'
+import { useSetZoomLevel } from '../viz/useSetZoomLevel'
+
+
+// null component that installs event listeners that handle the movement of the indicator
+export const Behaviors = ({ geometry, client }) => {
+    // get the zooming flag
+    const zooming = useZooming()
+    // make the handler that clears the zooming flag
+    const endZooming = useEndZooming()
+    // make a handler that sets the zoom level
+    const setZoom = useSetZoomLevel()
+
+    // unpack the geometry
+    const { viewportToUser } = geometry
+
+    // when the mouse moves
+    const move = (evt) => {
+        // if the indicator is not being dragged
+        if (!zooming) {
+            // bail
+            return
+        }
+        // get the controller bounding box
+        const { left } = client.current.getBoundingClientRect()
+        // transform the mouse coordinates into controller coordinates
+        const zoom = viewportToUser(evt.clientX - left)
+        // adjust the zoom level
+        setZoom(zoom)
+        // all done
+        return
+    }
+
+    // install the listeners
+    useEvent({
+        name: "mouseup", listener: endZooming, client,
+        triggers: []
+    })
+
+    useEvent({
+        name: "mousemove", listener: move, client,
+        triggers: [zooming, setZoom],
+    })
+
+    useEvent({
+        name: "mouseleave", listener: endZooming, client,
+        triggers: [],
+    })
+
+
+    // leave nothing behind
+    return null
+}
+
+
+// end of file
