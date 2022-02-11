f1afed6692bbedd017933f1a0f7c086281da3047
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-11 09:14:31 -0800

    qed.protocols: added {channel}, a visualization workflow for a particular aspect of a dataset

---
+++ pkg/protocols/Channel.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# support
+import qed
+# my superclass
+from .Producer import Producer
+
+
+# the workflow
+class Channel(Producer, family="qed.channels"):
+    """
+    A channel is a visualization workflow
+    """
+
+
+# end of file
