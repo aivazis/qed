1a760a139f8e2af6d6fbe3417327d6f7f45721ce
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-12 10:45:28 -0800

    ux/views: the {zoom} level of a {viz} dataset {viewport} is now a {synced} attribute

--- ux/client/views/viz/viz/useSetZoomLevel.js
+++ ux/client/views/viz/viz/useSetZoomLevel.js
@@ -14,17 +14,32 @@ import { Context } from './context'
 
 // set the zoom level of the active viewport
 export const useSetZoomLevel = () => {
-    // grab the active viewport and the zoom level table mutator
-    const { activeViewport, setZoom } = React.useContext(Context)
+    // grab the active viewport, the table of synced views,  and the zoom level table mutator
+    const { activeViewport, synced, setZoom } = React.useContext(Context)
 
     // a handler that sets the zoom level of the active viewport
     const set = (value) => {
         // update the zoom table
         setZoom(old => {
+            // preprocess, until continuous zooming is supported
+            value = Math.round(value)
             // make a copy of the old table
             const table = [...old]
             // adjust the entry that corresponds to the active view
-            table[activeViewport] = Math.round(value)
+            table[activeViewport] = value
+
+            // if the active port is synced
+            if (synced[activeViewport]) {
+                // go through all the views in the sync table
+                synced.forEach((status, viewport) => {
+                    // find the synced ones
+                    if (status) {
+                        // and adjust their zoom level as well
+                        table[viewport] = value
+                    }
+                })
+            }
+
             // return the new table
             return table
         })
