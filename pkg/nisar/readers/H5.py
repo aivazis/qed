e466c44304d19666016811326f8f952d4ff27b92
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-15 20:12:39 -0800

    qed.nisar: added a base {reader} that handles opening the HDF5 file with the product

---
+++ pkg/nisar/readers/H5.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# support
+import qed
+
+
+# the basic reader for NISAR products in HDF5 format
+class H5(qed.flow.factory, family="qed.nisar.readers.h5", implements=qed.protocols.reader):
+    """
+    The reader of HDF5 files
+    """
+
+
+    # public data
+    uri = qed.properties.path()
+    uri.doc = "the uri of the data source"
+
+    datasets = qed.properties.list(schema=qed.protocols.dataset.output())
+    datasets.doc = "the list of data sets provided by the reader"
+
+    selectors = qed.protocols.selectors()
+    selectors.doc = "a map of selector names to their allowed values"
+
+
+    # metamethods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+
+        # open my file
+        self.h5 = qed.libh5.File(path=str(self.uri))
+
+        # all done
+        return
+
+
+
+# end of file
