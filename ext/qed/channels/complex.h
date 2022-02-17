977b45cbbf93222a754b05c855d0a9300dbebb60
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-16 17:02:59 -0800

    qed::channels: added visualization of complex HDF5 tiles

--- ext/qed/channels/complex.h
+++ ext/qed/channels/complex.h
@@ -10,9 +10,9 @@
 
 // decorators
 namespace qed::py::channels {
-    // the tile generator for the complex of a complex source
+    // the tile generator for the complex of a complex grid source
     template <typename sourceT>
-    auto complexTile(
+    auto complexGridTile(
         // the source
         const sourceT & source,
         // the zoom level
@@ -20,7 +20,21 @@ namespace qed::py::channels {
         // the origin of the tile
         typename sourceT::index_type origin,
         // the tile shape
-        typename sourceT::shape_type shape,
+        typename sourceT::shape_type tile,
+        // the range of values to render
+        double min, double max, double saturation) -> bmp_t;
+
+    // the tile generator for the complex of a complex HDF5 source
+    template <typename sourceT>
+    auto complexHDF5Tile(
+        // the source
+        const dataset_t & dataset,
+        // the zoom level
+        int zoom,
+        // the origin of the tile
+        typename sourceT::index_type origin,
+        // the tile shape
+        typename sourceT::shape_type tile,
         // the range of values to render
         double min, double max, double saturation) -> bmp_t;
 }
