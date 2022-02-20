ec980b59f15f4c492aa97808c746737f3bcd2cd0
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-19 21:49:31 -0800

    ux/widgets: added rudimentary {label} rendering to the new {slider}

---
+++ ux/client/widgets/slider/label.js
@@ -0,0 +1,73 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// external
+import React from "react"
+import styled from 'styled-components'
+
+
+// render a single label
+export const Label = ({ tick, value, geometry, enabled }) => {
+    // unpack the geometry
+    const { arrow, labels, labelPosition } = geometry
+
+    // check whether my value is the currently chosen one
+    const selected = tick == value
+    // if so, and the marker is on the same side of the axis as the labels
+    if (selected && arrow === labels) {
+        // the marker gets drawn in my place; bail
+        return null
+    }
+
+    // pick an implementation based on my state
+    const Tick = enabled ? (selected ? Selected : Enabled) : Disabled
+
+    // render
+    return (
+        <Tick {...labelPosition(tick)} >
+            {tick}
+        </Tick>
+    )
+}
+
+
+// styling
+// the base
+const Base = styled.text`
+    font-family: inconsolata;
+    font-size: 36px;
+    text-anchor: middle;
+    cursor: default;
+`
+
+
+const Disabled = styled(Base)`
+    fill: hsl(0deg, 0%, 20%);
+`
+
+
+const Enabled = styled(Base)`
+    & {
+        fill: hsl(0deg, 0%, 40%);
+        cursor: pointer;
+    }
+
+    &:hover {
+        fill: hsl(0deg, 0%, 75%);
+    }
+
+    &:active {
+        fill: hsl(28deg, 90%, 55%);
+    }
+`
+
+
+const Selected = styled(Base)`
+    fill: hsl(28deg, 90%, 55%);
+`
+
+
+// end of file
---
+++ ux/client/widgets/slider/labels.js
@@ -0,0 +1,33 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// external
+import React from "react"
+import styled from 'styled-components'
+
+
+// locals
+// components
+import { Label } from './label'
+
+
+// render the labels
+export const Labels = ({ value, geometry, enabled }) => {
+    // get the major tick marks
+    const { major } = geometry
+
+    // render
+    return (
+        <>
+            {major.map(tick => (
+                <Label key={tick} tick={tick} value={value} geometry={geometry} enabled={enabled} />
+            ))}
+        </>
+    )
+}
+
+
+// end of file
