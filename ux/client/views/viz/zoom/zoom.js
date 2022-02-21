c65c37ce5a9b072cbd911f3f0146043e3597d85d
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-21 10:04:10 -0800

    ux/views: installed the new {slider} widget in the {zoom} tray of {viz}

--- ux/client/views/viz/zoom/zoom.js
+++ ux/client/views/viz/zoom/zoom.js
@@ -9,75 +9,52 @@ import React from 'react'
 
 // project
 // widgets
-import { Tray } from '~/widgets'
+import { SVG, Slider, Tray } from '~/widgets'
 
 // locals
-// support
-import { dimensions } from './dimensions'
-// context
-import { Provider } from './context'
 // hooks
 import { useGetView } from '../viz/useGetView'
-// components
-import { Behaviors } from './behaviors'
-import { Controller } from './controller'
+import { useGetZoomLevel } from '../viz/useGetZoomLevel'
+import { useSetZoomLevel } from '../viz/useSetZoomLevel'
 // styles
 import styles from './styles'
 
 
-// export the tray
-export const Zoom = () => {
-    // set up my context and embed my panel
-    return (
-        <Provider>
-            <Panel />
-        </Provider>
-    )
-}
-
-
 //  display the zoom control
-const Panel = () => {
-    // build a ref for my container
-    const ref = React.useRef()
+export const Zoom = () => {
+    // look up the zoom level of the active viewport
+    const zoom = useGetZoomLevel()
+    // make a handler that sets the zoom level
+    const setZoom = useSetZoomLevel()
     // get the active view and unpack it
     const { reader, dataset, channel } = useGetView()
 
     // inspect the view components to initialize my state
-    const state = (!reader || !dataset || !channel) ? "disabled" : "enabled"
+    const enabled = reader && dataset && channel
 
-
-    // build the control geometry
-    const geometry = dimensions({
-        // the minimum zoom level
-        min: 0,
-        // the maximum zoom level
-        max: 4,
-        // dimensions
-        height: 50,
-        width: 200,
-    })
+    // level range
+    const [min, max] = [0, 4]
+    // slider configuration
+    const slideConf = {
+        value: zoom, setValue: setZoom,
+        direction: "row", labels: "bottom", arrow: "top",
+        height: 50, width: 200,
+        min, max, major: [...Array(max - min + 1).keys()].map((_, idx) => min + idx)
+    }
 
     // mix my paint
-    const zoomPaint = styles.zoom(state)
+    const zoomPaint = styles.zoom(enabled ? "enabled" : "disabled")
     const controllerPaint = styles.controller
 
     // and render
     return (
         <Tray title="zoom" initially={true} style={zoomPaint.tray}>
             {/* the control housing */}
-            <svg ref={ref} version="1.1" xmlns="http://www.w3.org/2000/svg"
-                width={geometry.width} height={geometry.height}
-                style={controllerPaint}>
-
-                {/* the behaviors */}
-                <Behaviors geometry={geometry} client={ref} />
-                {/* the controller engine */}
-                <Controller geometry={geometry} state={state} />
-
-            </svg>
-
-        </Tray >
+            <SVG height={50} width={200} style={controllerPaint}>
+                {/* the slider */}
+                <Slider enabled={enabled} {...slideConf} />
+            </SVG>
+        </Tray>
     )
 }
 
