2325cb85b26e8ee91d5c36a4b66fc36f22d5f7cc
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-24 21:20:08 -0800

    ux/widgets: {slider} got a {rangemat}, a new placemat that is tuned to supporting selecting the interval between two values

---
+++ ux/client/widgets/slider/rangemat.js
@@ -0,0 +1,125 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// external
+import React from 'react'
+import styled from 'styled-components'
+
+// project
+// hooks
+import { useEvent } from '~/hooks'
+
+// locals
+// hooks
+import { useClient } from './useClient'
+import { useMine } from './useMine'
+import { useUser } from './useUser'
+import { useSliding } from './useSliding'
+import { useStopSliding } from './useStopSliding'
+
+
+// a workaround for capturing events in the controller client area
+// needed only because {pointer-events: bounding-box} doesn't work yet
+export const Rangemat = ({ setValue, geometry, enabled, children, ...rest }) => {
+    // make a ref to attach to the placemat so we can measure its extent
+    const placemat = React.useRef(null)
+    // get the {sliding} indicator
+    const sliding = useSliding()
+    // make a handler that clears the {sliding} flag
+    const stopSliding = useStopSliding()
+
+    // unpack the geometry
+    const { emplace } = useClient()
+    const { bboxMine } = useMine()
+    const { mouseToUser } = useUser()
+
+    // if the slider is {enabled}
+    if (enabled) {
+        // handler that converts mouse coordinates to user space and invokes {setValue} to inform
+        // the client
+        const pick = evt => {
+            // measure the placemat bounding box
+            const box = placemat.current.getBoundingClientRect()
+            // transform the mouse coordinates into a user value
+            const value = mouseToUser(box, evt)
+            // notify the client
+            setValue(old => {
+                // make a copy
+                const copy = [...old]
+                // use the selector id to modify to correct entry
+                copy[sliding] = value
+                // validate and return the correct value
+                return copy[0] < copy[1] ? copy : old
+            })
+            // and done
+            return
+        }
+
+        // handler that checks whether the marker is being dragged before converting
+        // mouse coordinates to user space
+        const drag = evt => {
+            // if the indicator is not being dragged
+            if (sliding === null) {
+                // do nothing
+                return
+            }
+            // otherwise, {pick} a value
+            pick(evt)
+            // all done
+            return
+        }
+
+        // install the mouse event listeners
+        // when the use clicks anywhere inside the {placemat}
+        useEvent({
+            name: "click", listener: pick, client: placemat,
+            triggers: [setValue]
+        })
+
+        // the mouse down is attached to the indicator
+        // when the mouse drags
+        useEvent({
+            name: "mousemove", listener: drag, client: placemat,
+            triggers: [sliding, setValue]
+        })
+
+        // dragging ends when the user lets go of the mouse button
+        useEvent({
+            name: "mouseup", listener: stopSliding, client: placemat,
+            triggers: []
+        })
+        // or when the mouse leaves the placemat
+        useEvent({
+            name: "mouseleave", listener: stopSliding, client: placemat,
+            triggers: []
+        })
+    }
+
+    // render
+    // N.B.: attach the {ref} to the group and not the {rect} that fills the bounding box;
+    // otherwise, the {mouseleave} will trigger when the mouse enters any of the other
+    // {children} of the slider
+    return (
+        <g ref={placemat} transform={emplace} >
+            <Rect {...bboxMine} {...rest} />
+            {children}
+        </g>
+    )
+}
+
+
+// styling
+const Rect = styled.rect`
+    fill: hsl(0deg, 0%, 7%);
+
+    &:active {
+        stroke: hsl(0deg, 0%, 20%);
+        stroke-width: 1;
+        vector-effect: non-scaling-stroke;
+    }
+`
+
+// end of file
