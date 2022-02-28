1a2c42ac89c1ea55ae4c0384ebe08a56257b899b
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-02-28 12:32:39 -0800

    ux/views: added a button that toggles the presence of the measuring layer of the {viz} dataset {viewer}

---
+++ ux/client/views/viz/viewer/measure.js
@@ -0,0 +1,56 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// project
+// shapes
+import { Target as Icon } from '~/shapes'
+// widgets
+import { Badge } from '~/widgets'
+
+// locals
+// hooks
+import { useMeasureLayer } from './useMeasureLayer'
+import { useToggleMeasureLayer } from './useToggleMeasureLayer'
+// styles
+import styles from './styles'
+
+
+// split a {view} into two
+export const Measure = ({ viewport }) => {
+    // get the current measure layer state
+    const selected = useMeasureLayer()
+    // grab the measure layer toggle from context
+    const toggle = useToggleMeasureLayer()
+    // turn it into a handler
+    const measure = evt => {
+        // stop this event from bubbling up
+        evt.stopPropagation()
+        // toggle
+        toggle()
+        // all done
+        return
+    }
+    // assemble my controllers
+    const behaviors = {
+        onClick: measure
+    }
+    // deduce my state
+    const state = selected ? "selected" : "enabled"
+    // mix my paint
+    const paint = styles.split
+    // and render
+    return (
+        <Badge size={14} state={state} behaviors={behaviors} style={paint} >
+            <Icon />
+        </Badge>
+    )
+}
+
+
+// end of file
