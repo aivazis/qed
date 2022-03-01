f1fb842b781b978294b762d33e4c34eb50724e51
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-01 14:22:18 -0800

    ux/views: moved the {crosshairs} of the {measure} layer in {viz} out of the body of the renderer

--- ux/client/views/viz/measure/mark.js
+++ ux/client/views/viz/measure/mark.js
@@ -11,15 +11,6 @@ import styled from 'styled-components'
 
 // mark a point
 export const Mark = ({ idx, at }) => {
-
-    // the cross hairs
-    const crosshairs = `
-        M -9 0 l 6 0
-        M 0 -9 l 0 6
-        M 9 0 l -6 0
-        M 0 9 l 0 -6
-    `
-
     // make a handler that marks me as the selected one when i'm clicked
     const select = evt => {
         // don't let this bubble up; the parent's handler adds points...
@@ -60,7 +51,14 @@ const Ring = styled.circle`
 `
 
 
-// the crosshairs
+// the path data for the crosshairs
+const crosshairs = `
+        M -9 0 l 6 0
+        M 0 -9 l 0 6
+        M 9 0 l -6 0
+        M 0 9 l 0 -6
+    `
+
 const Crosshairs = styled.path`
     fill: none;
     stroke: hsl(28deg, 90%, 55%);
