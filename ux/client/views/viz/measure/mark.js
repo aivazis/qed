311bdeeca7eec59b2481218bbdd1a8b3330e187b
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-01 13:51:25 -0800

    ux/views: added a {mark} for the {measure} layer of {viz} to identify the selected points

---
+++ ux/client/views/viz/measure/mark.js
@@ -0,0 +1,72 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+import styled from 'styled-components'
+
+
+// mark a point
+export const Mark = ({ idx, at }) => {
+
+    // the cross hairs
+    const crosshairs = `
+        M -9 0 l 6 0
+        M 0 -9 l 0 6
+        M 9 0 l -6 0
+        M 0 9 l 0 -6
+    `
+
+    // make a handler that marks me as the selected one when i'm clicked
+    const select = evt => {
+        // don't let this bubble up; the parent's handler adds points...
+        evt.stopPropagation()
+        // all done
+        return
+    }
+
+    // build my controller
+    const behaviors = {
+        onClick: select,
+    }
+
+    // render
+    return (
+        <g transform={`translate(${at[0]} ${at[1]})`} {...behaviors}>
+            <Mat cx={0} cy={0} r="15" />
+            <Ring cx={0} cy={0} r="7" />
+            <Crosshairs d={crosshairs} />
+        </g>
+    )
+}
+
+
+// placemat
+const Mat = styled.circle`
+    fill: hsl(0deg, 0%, 10%, 0.75);
+    stroke: none;
+`
+
+
+// node
+const Ring = styled.circle`
+    fill: none;
+    stroke: hsl(28deg, 90%, 55%);
+    stroke-width: 1;
+    vector-effect: non-scaling-stroke;
+`
+
+
+// the crosshairs
+const Crosshairs = styled.path`
+    fill: none;
+    stroke: hsl(28deg, 90%, 55%);
+    stroke-width: 1;
+    vector-effect: non-scaling-stroke;
+`
+
+
+// end of file
