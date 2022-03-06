1fbf183274c6591db9fa0373fd2eff59a348d27c
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-05 18:52:52 -0800

    ux/widgets: {tray} is now {styled}

--- ux/client/widgets/tray/tray.js
+++ ux/client/widgets/tray/tray.js
@@ -6,44 +6,111 @@
 
 // externals
 import React from 'react'
+import styled from 'styled-components'
+
+// project
+// theme
+import { theme } from '~/palette'
 
 // locals
+// components
 import { Indicator } from './indicator'
-// styles
-import styles from './styles'
 
 
 // a tray with a header and some items
-export const Tray = ({ title, style, initially = false, children }) => {
+export const Tray = ({ title, state, initially = false, children }) => {
     // storage for my state
     const [expanded, setExpanded] = React.useState(initially)
-
     // handler for flipping my state
     const toggle = () => setExpanded(!expanded)
 
-    // mix my paint
-    const boxStyle = { ...styles.box, ...style?.box }
-    const headerStyle = { ...styles.header, ...style?.header }
-    const titleStyle = { ...styles.title, ...style?.title }
-    const itemsStyle = { ...styles.items, ...style?.items }
+    // pick my parts based on my state
+    const { Header, Title } = components(state)
 
     // paint me
     return (
-        <section style={boxStyle} >
-            <div style={headerStyle} onClick={toggle}>
+        <Section>
+            <Header onClick={toggle}>
                 <Indicator expanded={expanded} />
-                <span style={titleStyle} >
-                    {title}
-                </span>
-            </div>
-            {expanded &&
-                <div style={itemsStyle} >
-                    {children}
-                </div>
-            }
-        </section>
+                <Title>{title}</Title>
+            </Header>
+            {expanded && <Items>{children}</Items>}
+        </Section>
     )
 }
+
+
+// state dependent selection
+const components = state => {
+    // when i'm selected
+    if (state === "selected") {
+        return { Header: SelectedHeader, Title: SelectedTitle }
+    }
+
+    // otherwise
+    return { Header, Title }
+}
+
+
+// parts
+// the box
+const Section = styled.section`
+    /* for me */
+    min-height: 1.3rem;
+    /* for my children */
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+`
+
+
+// the header
+const Header = styled.div`
+    /* style */
+    font-size: 60%;
+    padding: 0.25rem 0.0rem 0.25rem 0.6rem;
+    cursor: pointer;
+    /* colors */
+    color: hsl(0deg, 0%, 60%, 1);
+    background-color: hsl(0deg, 0%, 12%, 1);
+    /* don't stretch me */
+    flex: 0;
+
+    /* for my children */
+    display: flex;
+    align-items: center;
+`
+
+const SelectedHeader = styled(Header)`
+    background-color: hsl(0deg, 0%, 17%, 1);
+`
+
+
+// the title
+const Title = styled.span`
+    /* style */
+    font-family: rubik-medium;
+    padding: 0.0rem 0.0rem 0.0rem 0.5rem;
+    /* disable text selection */
+    user-select: none;
+`
+
+const SelectedTitle = styled(Title)`
+    color: ${theme.page.name};
+`
+
+
+// the items
+const Items = styled.div`
+    /* i stretch */
+    flex: 1;
+    /* gimme some room */
+    padding: 0.25rem 0.0rem;
+    /* for my children */
+    display: flex;
+    flex-direction: column;
+    overflow: auto;
+`
 
 
 // end of file
