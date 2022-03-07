2b4a9a85da7a8d3875bc3df8e335a73d83142072
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-07 09:03:56 -0800

    ux/views: the {useToggleMeasureLayer} hook of the {viz} context now clears the {pixelPath} every time the {measure} layer of a {viewport} is toggled

--- ux/client/views/viz/viz/useToggleMeasureLayer.js
+++ ux/client/views/viz/viz/useToggleMeasureLayer.js
@@ -15,18 +15,27 @@ import { Context } from './context'
 // toggle the measure layer over this viewport
 export const useToggleMeasureLayer = viewport => {
     // grab the measure layer mutator from context
-    const { setMeasureLayer } = React.useContext(Context)
+    const { setMeasureLayer, setPixelPath } = React.useContext(Context)
 
     // a handler that toggles whether the measure layer is visible
     const toggle = () => {
         // toggle
         setMeasureLayer(old => {
-            // make a copy
+            // make a copy of the measure layer status table
             const table = [...old]
             // toggle the entry that corresponds to this viewport
             table[viewport] = !table[viewport]
             // and return the new table
             return table
+        })
+        // on toggle, clear the pixel path as well
+        setPixelPath(old => {
+            // make a copy of the paths
+            const paths = [...old]
+            // clear out the one that corresponds to {viewport}
+            paths[viewport] = []
+            // and return the new pile
+            return paths
         })
         // all done
         return
