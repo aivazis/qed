cdb9e3b133fb52020295d697a02bde891d7b5272
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-08 08:16:03 -0800

    ux/views: added a {useCenterViewport} hook so clients with access to the {viz} dataset {viewports} can ask them to center themselves

---
+++ ux/client/views/viz/viz/useCenterViewport.js
@@ -0,0 +1,43 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// local
+// hooks
+import { useViewports } from './useViewports'
+import { useViews } from './useViews'
+
+
+// make a handler that centers a viewport at a given point
+export const useCenterViewport = (viewport = null) => {
+    // get the table of viewports
+    const viewports = useViewports()
+    // and the active viewport so we can use it as a fall back
+    const { activeViewport } = useViews()
+
+    // make a handler that scrolls the viewport so that the given point is at the center
+    const center = ({ x, y }) => {
+        // get the scrolling container
+        const placemat = viewports[viewport ?? activeViewport]
+        // measure it
+        const box = placemat.getBoundingClientRect()
+        // extract the viewport size
+        const width = box.width
+        const height = box.height
+        // compute the new position of the upper left hand corner
+        const left = x - width / 2
+        const top = y - height / 2
+        // scroll there
+        placemat.scroll({ left, top, behavior: "smooth" })
+        // all done
+        return
+    }
+
+    // and return it
+    return center
+}
+
+
+// end of file
