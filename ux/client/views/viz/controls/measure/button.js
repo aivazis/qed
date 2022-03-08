e6e3b8d3734a6ac55d8cdaa863fdcb05d3ecd6af
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-07 20:37:36 -0800

    ux/views: factored the wrapping of shapes into clickable content into its own component in the {measure} tray of the {viz} viewport {controls}

---
+++ ux/client/views/viz/controls/measure/button.js
@@ -0,0 +1,45 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+import styled from 'styled-components'
+
+// project
+// widgets
+import { SVG } from '~/widgets'
+
+
+// a line in the table of the points on the {measure} layer of the active viewport
+export const Button = ({ children }) => {
+    // make a mark
+    return (
+        <Mark>
+            <Icon>
+                <g transform={`scale(${14 / 1000})`} >
+                    {children}
+                </g>
+            </Icon>
+        </Mark>
+
+    )
+}
+
+
+const Mark = styled.span`
+    display: inline-block;
+    width: 2.0rem;
+    text-align: end;
+`
+
+const Icon = styled(SVG)`
+    width: 0.7rem;
+    height: 0.7rem;
+    vertical-align: bottom;
+`
+
+
+// end of file
