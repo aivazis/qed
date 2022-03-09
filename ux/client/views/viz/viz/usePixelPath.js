a526f6d0f6d276ea27f9dab98405ef59410845e4
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-09 07:31:02 -0800

    ux/views: the {usePixelPath} hook of {viz} now defaults to the {activeViewport}

--- ux/client/views/viz/viz/usePixelPath.js
+++ ux/client/views/viz/viz/usePixelPath.js
@@ -13,11 +13,13 @@ import { Context } from './context'
 
 
 // access to the list of points in the profile
-export const usePixelPath = (viewport) => {
+export const usePixelPath = (viewport = null) => {
     // get the list and the active viewport
     const { activeViewport, pixelPath } = React.useContext(Context)
+    // normalize the viewport
+    viewport ??= activeViewport
     // and return it
-    return pixelPath[viewport ?? activeViewport]
+    return pixelPath[viewport]
 }
 
 
