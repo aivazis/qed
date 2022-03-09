5e336e26b664f6581f83524eb346267a60a13ed2
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-09 15:22:31 -0800

    ux/views: the {focus} button on the {measure} tray of the {viz} {controls} now takes the {zoom} level into account before sending the coordinates off to center the {viewport}

--- ux/client/views/viz/controls/measure/focus.js
+++ ux/client/views/viz/controls/measure/focus.js
@@ -14,19 +14,25 @@ import { Target } from '~/shapes'
 // local
 // hooks
 import { useCenterViewport } from '../../viz/useCenterViewport'
+import { useGetZoomLevel } from '../../viz/useGetZoomLevel'
 // components
 import { Button } from './button'
 
 
 // locate the given point on the viewport
 export const Focus = ({ idx, point }) => {
+    // get the zoom level of the active viewport
+    const zoom = useGetZoomLevel()
     // get the handler that centers the active viewport
     const center = useCenterViewport()
+
+    // turn the zoom level into a scale
+    const scale = 2 ** zoom
 
     // wrap it in a control
     const centerAt = () => {
         // center the viewport on my point
-        center({ x: point[0], y: point[1] })
+        center({ x: point[0] / scale, y: point[1] / scale })
         // and done
         return
     }
