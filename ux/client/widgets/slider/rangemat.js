e2b0c2495f123825a1c06a5ea59065610b733d67
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-11 10:39:45 -0800

    ux/widgets: the {slider} mats now respect the rules of hooks by sending {null} listeners when they are disabled

--- ux/client/widgets/slider/rangemat.js
+++ ux/client/widgets/slider/rangemat.js
@@ -41,82 +41,79 @@ export const Rangemat = ({ setValue, children, ...rest }) => {
     const { bboxMine } = useMine()
     const { mouseDeltaToUser } = useUser()
 
-    // if the slider is {enabled}
-    if (enabled) {
-        // handler that converts mouse coordinates to user space and invokes {setValue} to inform
-        // the client
-        const pick = evt => {
-            // get the mouse movement along my main axis
-            const dMain = evt[mainMovementName]
-            // if there is no change
-            if (Math.trunc(dMain) === 0) {
-                // bail
-                return
-            }
-            // transform the mouse coordinates into a user value
-            const delta = mouseDeltaToUser(dMain)
-            // notify the client
-            setValue(old => {
-                // make a copy
-                const range = [...old]
-                // if both are sliding
-                if (sliding < 0) {
-                    range[0] += delta
-                    range[1] += delta
-                } else {
-                    // use the selector id to modify to correct entry
-                    range[sliding] += delta
-                }
-
-                // clip
-                range[0] = Math.max(range[0], min)
-                range[1] = Math.max(Math.min(range[1], max), range[0])
-                // and return the new range
-                return range
-            })
-            // and done
+    // handler that converts mouse coordinates to user space and invokes {setValue} to inform
+    // the client
+    const pick = evt => {
+        // get the mouse movement along my main axis
+        const dMain = evt[mainMovementName]
+        // if there is no change
+        if (Math.trunc(dMain) === 0) {
+            // bail
             return
         }
-
-        // handler that checks whether the marker is being dragged before converting
-        // mouse coordinates to user space
-        const drag = evt => {
-            // if the indicator is not being dragged
-            if (sliding === null) {
-                // do nothing
-                return
+        // transform the mouse coordinates into a user value
+        const delta = mouseDeltaToUser(dMain)
+        // notify the client
+        setValue(old => {
+            // make a copy
+            const range = [...old]
+            // if both are sliding
+            if (sliding < 0) {
+                range[0] += delta
+                range[1] += delta
+            } else {
+                // use the selector id to modify to correct entry
+                range[sliding] += delta
             }
-            // otherwise, {pick} a value
-            pick(evt)
-            // all done
-            return
-        }
 
-        // install the mouse event listeners
-        // when the use clicks anywhere inside the {placemat}
-        useEvent({
-            name: "click", listener: pick, client: placemat,
-            triggers: [setValue]
+            // clip
+            range[0] = Math.max(range[0], min)
+            range[1] = Math.max(Math.min(range[1], max), range[0])
+            // and return the new range
+            return range
         })
-
-        // the mouse down is attached to the indicator
-        // when the mouse drags
-        useEvent({
-            name: "mousemove", listener: drag, client: placemat,
-            triggers: [sliding, setValue]
-        })
-
-        // dragging ends when the user lets go of the mouse button
-        useEvent({
-            name: "mouseup", listener: stopSliding, client: placemat,
-            triggers: []
-        })
-        // or when the mouse leaves the placemat
-        useEvent({
-            name: "mouseleave", listener: stopSliding, client: placemat,
-            triggers: []
-        })
+        // and done
+        return
     }
+
+    // handler that checks whether the marker is being dragged before converting
+    // mouse coordinates to user space
+    const drag = evt => {
+        // if the indicator is not being dragged
+        if (sliding === null) {
+            // do nothing
+            return
+        }
+        // otherwise, {pick} a value
+        pick(evt)
+        // all done
+        return
+    }
+
+    // install the mouse event listeners
+    // when the use clicks anywhere inside the {placemat}
+    useEvent({
+        name: "click", listener: enabled ? pick : null, client: placemat,
+        triggers: [setValue]
+    })
+
+    // the mouse down is attached to the indicator
+    // when the mouse drags
+    useEvent({
+        name: "mousemove", listener: enabled ? drag : null, client: placemat,
+        triggers: [sliding, setValue]
+    })
+
+    // dragging ends when the user lets go of the mouse button
+    useEvent({
+        name: "mouseup", listener: enabled ? stopSliding : null, client: placemat,
+        triggers: []
+    })
+    // or when the mouse leaves the placemat
+    useEvent({
+        name: "mouseleave", listener: enabled ? stopSliding : null, client: placemat,
+        triggers: []
+    })
 
     // render
     // N.B.: attach the {ref} to the group and not the {rect} that fills the bounding box;
--- ux/client/widgets/slider/simplemat.js
+++ ux/client/widgets/slider/simplemat.js
@@ -42,67 +42,64 @@ export const Simplemat = ({ setValue, children, ...rest }) => {
     const { bboxMine } = useMine()
     const { mouseToUser } = useUser()
 
-    // if the slider is {enabled}
-    if (enabled) {
-        // handler that converts mouse coordinates to user space and invokes {setValue} to inform
-        // the client
-        const pick = evt => {
-            // get the position of the mouse relative to the main edge
-            const pixels = evt[mainOffsetName]
-            // transform the mouse coordinates into a user value
-            const value = mouseToUser(pixels)
-            // notify the client
-            setValue(value)
-            // and done
-            return
-        }
-
-        // handler that checks whether the marker is being dragged before converting
-        // mouse coordinates to user space
-        const drag = evt => {
-            // if the indicator is not being dragged
-            if (sliding === null) {
-                // do nothing
-                return
-            }
-            // get the mouse movement along my main axis
-            const dMain = evt[mainMovementName]
-            // if there is no change
-            if (Math.trunc(dMain) === 0) {
-                // bail
-                return
-            }
-            // otherwise, {pick} a value
-            pick(evt)
-            // all done
-            return
-        }
-
-        // install the mouse event listeners
-        // when the use clicks anywhere inside the {placemat}
-        useEvent({
-            name: "click", listener: pick, client: placemat,
-            triggers: [setValue]
-        })
-
-        // the mouse down is attached to the indicator
-        // when the mouse drags
-        useEvent({
-            name: "mousemove", listener: drag, client: placemat,
-            triggers: [sliding, setValue]
-        })
-
-        // dragging ends when the user lets go of the mouse button
-        useEvent({
-            name: "mouseup", listener: stopSliding, client: placemat,
-            triggers: []
-        })
-        // or when the mouse leaves the placemat
-        useEvent({
-            name: "mouseleave", listener: stopSliding, client: placemat,
-            triggers: []
-        })
+    // handler that converts mouse coordinates to user space and invokes {setValue} to inform
+    // the client
+    const pick = evt => {
+        // get the position of the mouse relative to the main edge
+        const pixels = evt[mainOffsetName]
+        // transform the mouse coordinates into a user value
+        const value = mouseToUser(pixels)
+        // notify the client
+        setValue(value)
+        // and done
+        return
     }
+
+    // handler that checks whether the marker is being dragged before converting
+    // mouse coordinates to user space
+    const drag = evt => {
+        // if the indicator is not being dragged
+        if (sliding === null) {
+            // do nothing
+            return
+        }
+        // get the mouse movement along my main axis
+        const dMain = evt[mainMovementName]
+        // if there is no change
+        if (Math.trunc(dMain) === 0) {
+            // bail
+            return
+        }
+        // otherwise, {pick} a value
+        pick(evt)
+        // all done
+        return
+    }
+
+    // install the mouse event listeners
+    // when the use clicks anywhere inside the {placemat}
+    useEvent({
+        name: "click", listener: enabled ? pick : null, client: placemat,
+        triggers: [setValue]
+    })
+
+    // the mouse down is attached to the indicator
+    // when the mouse drags
+    useEvent({
+        name: "mousemove", listener: enabled ? drag : null, client: placemat,
+        triggers: [sliding, setValue]
+    })
+
+    // dragging ends when the user lets go of the mouse button
+    useEvent({
+        name: "mouseup", listener: enabled ? stopSliding : null, client: placemat,
+        triggers: []
+    })
+    // or when the mouse leaves the placemat
+    useEvent({
+        name: "mouseleave", listener: enabled ? stopSliding : null, client: placemat,
+        triggers: []
+    })
 
     // render
     // N.B.: attach the {ref} to the group and not the {rect} that fills the bounding box;
