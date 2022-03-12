be808a321b2ad02f8087eff349ac256a6785742f
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-11 21:51:59 -0800

    ux/views: removed the {usePixelPathSelection} state from the {measure} layer context in {viz}

--- ux/client/views/viz/measure/context.js
+++ ux/client/views/viz/measure/context.js
@@ -10,15 +10,11 @@ import React from 'react'
 
 // the provider factory
 export const Provider = ({ children }) => {
-    // the set of selected nodes
-    const [selection, setSelection] = React.useState(new Set())
     // a flag that indicates that the user has started dragging a {mark}
     const [moving, setMoving] = React.useState(null)
 
     // build the initial context value
     const context = {
-        // selected nodes
-        selection, setSelection,
         // movement indicator
         moving, setMoving,
     }
@@ -36,9 +32,6 @@ export const Provider = ({ children }) => {
 export const Context = React.createContext(
     // the default value clients see when accessing the context outside a provider
     {
-        // selected nodes
-        selection: null,
-        setSelection: () => { throw new Error(complaint) },
         // movement
         moving: null,
         setMoving: () => { throw new Error(complaint) },
