6f4a308bdee26bc637ef291bf4e7d0a41b74d742
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-03-21 14:32:18 -0700

    ux/views: added {useClearPixelPath} hook to remove the {measure} layer {path} associated with a {viz} dataset {viewer}

---
+++ ux/client/views/viz/viz/useClearPixelPath.js
@@ -0,0 +1,42 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// clear the current selection
+export const useClearPixelPath = viewport => {
+    // get the selection mutator
+    const { activeViewport, setPixelPath } = React.useContext(Context)
+    // normalize the viewport
+    viewport ??= activeViewport
+
+    // make a handler that clears the current selection
+    const clear = () => {
+        // reset the selection to an empty set
+        setPixelPath(old => {
+            // make a copy
+            const paths = [...old]
+            // clear out the one that corresponds to {viewport}
+            paths[viewport] = []
+            // and return the new pile
+            return paths
+        })
+        // all done
+        return
+    }
+
+    // and return the handler
+    return clear
+}
+
+
+// end of file
