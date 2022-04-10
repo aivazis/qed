bd61f7350c07262b3edfc78ea3d1731b59790eb5
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-04-10 05:57:59 -0700

    qed.channels: reimplemented {real} amd {imaginary} in terms of {ranged}

--- pkg/channels/Imaginary.py
+++ pkg/channels/Imaginary.py
@@ -7,11 +7,11 @@
 # support
 import qed
 # superclass
-from .Channel import Channel
+from .Ranged import Ranged
 
 
 # a channel for displaying the imaginary part of complex values
-class Imaginary(Channel, family="qed.channels.imaginary"):
+class Imaginary(Ranged, family="qed.channels.imaginary"):
    """
    Make a visualization pipeline to display the imaginary part of complex values
    """
@@ -22,36 +22,14 @@ class Imaginary(Channel, family="qed.channels.imaginary"):
 
 
    # user configurable state
-   min = qed.properties.float(default=-1000.0)
+   min = qed.properties.float(default=-1.0e3)
    min.doc = "the minimum value; anything below is underflow"
 
-   max = qed.properties.float(default=1000.0)
+   max = qed.properties.float(default=1.0e3)
    max.doc = "the maximum value; anything above is overflow"
 
 
    # interface
-   def tile(self, source, **kwds):
-      """
-      Generate a tile of the given characteristics
-      """
-      # get my range
-      low = self.min
-      high = self.max
-      # if either is uninitialized
-      if low is None or high is None:
-         # extract from the dataset
-         low, mean, high = source.stats()
-         # adjust
-         high = min(high, 4*mean)
-         low = -high
-         # and remember for next time
-         self.min = low
-         self.max = high
-
-      # add my configuration and chain up
-      return super().tile(source=source, min=low, max=high, **kwds)
-
-
    def project(self, pixel):
       """
       Compute the imaginary part of a {pixel}
