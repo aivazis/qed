81cfaf1d5280ced4027ea2c829bff4ac970f84c9
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-04-11 10:12:57 -0700

    ux/views: installed a {range} controller in the {viz} tray in {controls}

--- ux/client/views/viz/controls/viz/range.js
+++ ux/client/views/viz/controls/viz/range.js
@@ -11,7 +11,7 @@ import styled from 'styled-components'
 
 // project
 // widgets
-import { Range } from '~/widgets'
+import { Range, SVG } from '~/widgets'
 
 
 // a ranged controller
@@ -28,12 +28,69 @@ export const RangeController = props => {
         }
     `, props.configuration)
 
-    // show me
-    console.log(configuration)
+    // unpack
+    const { slot, min, max, low, high } = configuration
 
-    // all done
-    return null
+    // switch to log scale
+    const logMin = Math.round(Math.log10(min))
+    const logMax = Math.round(Math.log10(max))
+    const logLow = Math.log10(Math.max(min, low))
+    const logHigh = Math.log10(Math.min(max, high))
+    // set up the tick marks
+    const major = [...Array(logMax - logMin + 1).keys()].map((_, idx) => logMin + idx)
+
+    // the value updated
+    const setValue = v => console.log(v)
+    // controller configuration
+    const range = {
+        value: [logLow, logHigh], setValue,
+        min: logMin, max: logMax, major,
+        direction: "row", labels: "bottom", arrows: "top",
+        height: 100, width: 250,
+    }
+
+    // show me
+    console.log(range)
+
+    // render
+    return (
+        <>
+            <Header>
+                <Title>{slot}</Title>
+            </Header>
+            <Housing height={range.height} width={range.width}>
+                <Controller enabled={true} {...range} />
+            </Housing>
+        </>
+    )
 }
+
+
+// the section header
+const Header = styled.div`
+    font-size: 65%;
+    margin: 0.5rem 1.0rem 0.25rem 1.0rem;
+`
+
+// the title
+const Title = styled.span`
+    display: inline-block;
+    font-family: rubik-light;
+    width: 2.5rem;
+    padding: 0.0rem 0.0rem 0.25rem 0.0rem;
+    cursor: default;
+    color: hsl(0deg, 0%, 75%);
+`
+
+// the controller housing
+const Housing = styled(SVG)`
+    margin: 0.25rem auto;
+    border: 1px solid hsl(0deg, 0%, 10%);
+`
+
+// the controller
+const Controller = styled(Range)`
+`
 
 
 // end of file
