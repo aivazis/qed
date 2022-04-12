1cf9610a8f2aa03f1a946408eb5a50c5791c6b73
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-04-12 10:14:38 -0700

    qed.gql: added the definition of the input payload for the range controller mutation

---
+++ pkg/gql/RangeControllerInput.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# externals
+import graphene
+
+
+# the payload for an update to the range of a controller
+class RangeControllerInput(graphene.InputObjectType):
+    """
+    The payload for a ranged controller update
+    """
+
+
+    # the fields
+    dataset = graphene.ID()
+    channel = graphene.ID()
+
+    slot = graphene.String(required=True)
+
+    low = graphene.Float(required=True)
+    high = graphene.Float(required=True)
+
+
+# end of file
