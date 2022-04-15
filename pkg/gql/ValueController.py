f33adc21da2486d887fe793cef0e5c68b897980a
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-04-14 20:53:14 -0700

    qed.gql: fleshed out the {value} controller resolver

--- pkg/gql/ValueController.py
+++ pkg/gql/ValueController.py
@@ -22,14 +22,62 @@ class ValueController(graphene.ObjectType):
         # register my interface
         interfaces = Node,
 
+
     # my fields
     id = graphene.ID()
-    uuid = graphene.ID()
     # payload
     slot = graphene.String()
     min = graphene.Float()
     max = graphene.Float()
     value = graphene.Float()
+
+
+    # resolvers
+    def resolve_id(context, *_):
+        # extract the controller
+        controller = context["controller"]
+        # form the controller id
+        return controller.pyre_name
+
+
+    def resolve_slot(context, *_):
+        """
+        Resolve the slot managed by this controller
+        """
+        # extract the controller metadata
+        trait = context["trait"]
+        # easy enough
+        return trait.name
+
+
+    def resolve_min(context, *_):
+        """
+        Resolve the minimum value of the range
+        """
+        # extract the controller
+        controller = context["controller"]
+        # easy enough
+        return controller.min
+
+
+    def resolve_max(context, *_):
+        """
+        Resolve the maximum value of the range
+        """
+        # extract the controller
+        controller = context["controller"]
+        # easy enough
+        return controller.max
+
+
+    def resolve_value(context, *_):
+        """
+        Resolve the value
+        """
+        # extract the controller
+        controller = context["controller"]
+        # easy enough
+        return controller.value
 
 
 # end of file
