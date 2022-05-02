2462480726c452f8311962edf994f99d8787bc6d
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-02 07:51:00 -0700

    qed.channels: exported the base {channel} and the controllers

--- pkg/channels/__init__.py
+++ pkg/channels/__init__.py
@@ -4,11 +4,20 @@
 # (c) 1998-2022 all rights reserved
 
 
+# the base class
+from .Channel import Channel as channel
+
+# generic channels
 from .Amplitude import Amplitude as amplitude
 from .Complex import Complex as complex
 from .Imaginary import Imaginary as imaginary
 from .Phase import Phase as phase
 from .Real import Real as real
+
+# the controllers
+from .LinearRange import LinearRange as linearRange
+from .LogRange import LogRange as logRange
+from .Value import Value as value
 
 
 # end of file
