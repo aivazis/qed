f07cf11acd1853b74340a37313a86b9a1013882c
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-03 09:09:46 -0700

    qed::native: added a namespace for the support for {native} readers and their infrastructure

---
+++ lib/qed/native/forward.h
@@ -0,0 +1,14 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_native_forward_h)
+#define qed_native_forward_h
+
+
+#endif
+
+// end of file
---
+++ lib/qed/native/public.h
@@ -0,0 +1,24 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_native_public_h)
+#define qed_native_public_h
+
+// external packages
+#include "externals.h"
+// get the forward declarations
+#include "forward.h"
+
+// published type aliases; this is the file you are looking for...
+#include "api.h"
+
+
+// implementation
+
+#endif
+
+// end of file
