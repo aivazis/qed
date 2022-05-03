66ba68a363c8fd84f0721ce5de9a3f2712a0bdb0
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-03 09:06:29 -0700

    qed: made room for collecting the bindings of the {native} readers

---
+++ ext/qed/native/native.cc
@@ -0,0 +1,29 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+
+// external
+#include "external.h"
+// namespace setup
+#include "forward.h"
+
+
+// build the submodule
+void
+qed::py::native::native(py::module & m)
+{
+    // create the {native} submodule
+    auto native = m.def_submodule(
+        // the name of the module
+        "native",
+        // its docstring
+        "support for native readers and their infrastructure");
+
+    // all done
+    return;
+}
+
+
+// end of file
