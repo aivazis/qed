1950aa4d40aca980719823ba3519d4cbb3e6d1c1
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-03 09:09:46 -0700

    qed::native: added a namespace for the support for {native} readers and their infrastructure

---
+++ lib/qed/native.h
@@ -0,0 +1,18 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_native_h)
+#define qed_native_h
+
+// publish
+// the api is in "native.api.h"
+#include "native/public.h"
+
+
+#endif
+
+// end of file
---
+++ lib/qed/native/externals.h
@@ -0,0 +1,14 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_native_externals_h)
+#define qed_native_externals_h
+
+
+#endif
+
+// end of file
