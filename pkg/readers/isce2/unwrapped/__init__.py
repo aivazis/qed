a9b5e10345f56dd7f8c86049729f9603e8ac7841
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-03 08:36:09 -0700

    qed.isce2: primed the {unwrapped} interferogram reader

---
+++ pkg/readers/isce2/unwrapped/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# publish the reader
+from .Reader import Reader as unw
+
+
+# end of file
