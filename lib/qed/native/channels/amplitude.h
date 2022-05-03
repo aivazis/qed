c31056959f386295040b0b4dc225065d174418b9
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-03 10:27:53 -0700

    qed::native:: added the complex {channels}

---
+++ lib/qed/native/channels/amplitude.h
@@ -0,0 +1,36 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_native_channels_amplitude_h)
+#define qed_native_channels_amplitude_h
+
+
+// the amplitude tile generator
+namespace qed::native::channels {
+    // the tile generator for the amplitude of a complex grid source
+    template <typename sourceT>
+    auto amplitude(
+        // the source
+        const sourceT & source,
+        // the zoom level
+        int zoom,
+        // the origin of the tile
+        typename sourceT::index_type origin,
+        // the tile shape
+        typename sourceT::shape_type tile,
+        // the range of values to render
+        double min, double max) -> bmp_t;
+}
+
+
+// pull in the implementations
+#define qed_native_channels_amplitude_icc
+#include "amplitude.icc"
+#undef qed_native_channels_amplitude_icc
+
+#endif
+
+// end of file
