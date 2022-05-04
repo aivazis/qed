cb4a98b2178e69aa99ef4c001535082e2ddc57d5
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-03 19:36:14 -0700

    qed.readers: moved the {native} {channels} to their own package

---
+++ pkg/readers/native/channels/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2022 all rights reserved
+
+
+# the base channel
+from .Channel import Channel as channel
+
+# generic channels
+from .Amplitude import Amplitude as amplitude
+from .Complex import Complex as complex
+from .Imaginary import Imaginary as imaginary
+from .Phase import Phase as phase
+from .Real import Real as real
+
+
+# end of file
