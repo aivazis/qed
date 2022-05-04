b9fdae496dc33e67fa6960037a2a8205be557e70
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-03 21:11:50 -0700

    qed::nisar: the {slc} channels are now in their own package

--- ext/qed/nisar/slc.cc
+++ ext/qed/nisar/slc.cc
@@ -12,53 +12,53 @@
 
 // submodule with the bindings for the nisar tile generators
 void
-qed::py::nisar::channels(py::module & m)
+qed::py::nisar::slc(py::module & m)
 {
-    // create a {channels} submodule
-    auto channels = m.def_submodule(
+    // create the {slc} submodule
+    auto slc = m.def_submodule(
         // the name of the module
-        "channels",
+        "slc",
         // its docstring
-        "support for nisar {channels}");
+        "support for nisar {slc} datasets");
 
     // {c8} amplitude
-    channels.def(
+    slc.def(
         // the name of the function
         "amplitude",
         // the handler
-        &qed::nisar::channels::amplitude<heapgrid_t<std::complex<float>>>,
+        &qed::nisar::slc::amplitude<heapgrid_t<std::complex<float>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "min"_a, "max"_a,
         // the docstring
         "render the amplitude of a complex float tile");
     // {c16} amplitude
-    channels.def(
+    slc.def(
         // the name of the function
         "amplitude",
         // the handler
-        &qed::nisar::channels::amplitude<heapgrid_t<std::complex<double>>>,
+        &qed::nisar::slc::amplitude<heapgrid_t<std::complex<double>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "min"_a, "max"_a,
         // the docstring
         "render the amplitude of a complex double tile");
 
     // {c8}
-    channels.def(
+    slc.def(
         // the name of the function
         "complex",
         // the handler
-        &qed::nisar::channels::complex<heapgrid_t<std::complex<float>>>,
+        &qed::nisar::slc::complex<heapgrid_t<std::complex<float>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "min"_a, "max"_a, "minPhase"_a,
         "maxPhase"_a, "saturation"_a,
         // the docstring
         "render the value of a complex float tile");
     // {c16}
-    channels.def(
+    slc.def(
         // the name of the function
         "complex",
         // the handler
-        &qed::nisar::channels::complex<heapgrid_t<std::complex<double>>>,
+        &qed::nisar::slc::complex<heapgrid_t<std::complex<double>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "min"_a, "max"_a, "minPhase"_a,
         "maxPhase"_a, "saturation"_a,
@@ -66,43 +66,43 @@ qed::py::nisar::channels(py::module & m)
         "render the value of a complex double tile");
 
     // imaginary part of {c8}
-    channels.def(
+    slc.def(
         // the name of the function
         "imaginary",
         // the handler
-        &qed::nisar::channels::imaginary<heapgrid_t<std::complex<float>>>,
+        &qed::nisar::slc::imaginary<heapgrid_t<std::complex<float>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "min"_a, "max"_a,
         // the docstring
         "render the imaginary part a complex float tile");
     // imaginary part of {c16}
-    channels.def(
+    slc.def(
         // the name of the function
         "imaginary",
         // the handler
-        &qed::nisar::channels::imaginary<heapgrid_t<std::complex<double>>>,
+        &qed::nisar::slc::imaginary<heapgrid_t<std::complex<double>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "min"_a, "max"_a,
         // the docstring
         "render the imaginary part of a complex double tile");
 
     // phase of {c8}
-    channels.def(
+    slc.def(
         // the name of the function
         "phase",
         // the handler
-        &qed::nisar::channels::phase<heapgrid_t<std::complex<float>>>,
+        &qed::nisar::slc::phase<heapgrid_t<std::complex<float>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "low"_a, "high"_a,
         "saturation"_a, "brightness"_a,
         // the docstring
         "render the phase of a complex float tile");
     // phase of {c16}
-    channels.def(
+    slc.def(
         // the name of the function
         "phase",
         // the handler
-        &qed::nisar::channels::phase<heapgrid_t<std::complex<double>>>,
+        &qed::nisar::slc::phase<heapgrid_t<std::complex<double>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "low"_a, "high"_a,
         "saturation"_a, "brightness"_a,
@@ -110,21 +110,21 @@ qed::py::nisar::channels(py::module & m)
         "render the phase of a complex double tile");
 
     // real part of {c8}
-    channels.def(
+    slc.def(
         // the name of the function
         "real",
         // the handler
-        &qed::nisar::channels::real<heapgrid_t<std::complex<float>>>,
+        &qed::nisar::slc::real<heapgrid_t<std::complex<float>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "min"_a, "max"_a,
         // the docstring
         "render the real part a complex float tile");
     // real part of {c16}
-    channels.def(
+    slc.def(
         // the name of the function
         "real",
         // the handler
-        &qed::nisar::channels::real<heapgrid_t<std::complex<double>>>,
+        &qed::nisar::slc::real<heapgrid_t<std::complex<double>>>,
         // the signature
         "source"_a, "datatype"_a, "zoom"_a, "origin"_a, "shape"_a, "min"_a, "max"_a,
         // the docstring
