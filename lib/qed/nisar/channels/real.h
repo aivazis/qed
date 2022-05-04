2bbf033fbf9deda4708e59dc35c55f22cbd925e3
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-03 17:54:02 -0700

    qed::nisar: added the {hdf5} tile generators

---
+++ lib/qed/nisar/channels/real.h
@@ -0,0 +1,38 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_nisar_channels_real_h)
+#define qed_nisar_channels_real_h
+
+
+// the real part tile generator
+namespace qed::nisar::channels {
+    // the tile generator for the real part of a complex HDF5 source
+    template <typename sourceT>
+    auto real(
+        // the source
+        const dataset_t & source,
+        // the data layout
+        const datatype_t & datatype,
+        // the zoom level
+        int zoom,
+        // the origin of the tile
+        typename sourceT::index_type origin,
+        // the tile shape
+        typename sourceT::shape_type tile,
+        // the range of values to render
+        double min, double max) -> bmp_t;
+}
+
+
+// pull in the implementations
+#define qed_nisar_channels_real_icc
+#include "real.icc"
+#undef qed_nisar_channels_real_icc
+
+#endif
+
+// end of file
