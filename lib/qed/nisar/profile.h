0d1cd0cb843487c5c69f12b29312f3d2106efabd
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-05 10:49:59 -0600

    qed::nisar: added support for {profile} and {stats}

---
+++ lib/qed/nisar/profile.h
@@ -0,0 +1,32 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_nisar_profile_h)
+#define qed_nisar_profile_h
+
+
+// decorators
+namespace qed::nisar {
+    // profile for a complex HDF5 source
+    template <typename sourceT>
+    auto profile(
+        // the source
+        const dataset_t & source,
+        // the data layout
+        const datatype_t & datatype,
+        // the points
+        const native::points_t &) -> native::values_t<typename sourceT::value_type>;
+}
+
+
+// pull in the implementations
+#define qed_nisar_profile_icc
+#include "profile.icc"
+#undef qed_nisar_profile_icc
+
+#endif
+
+// end of file
---
+++ lib/qed/nisar/stats.h
@@ -0,0 +1,34 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_nisar_stats_h)
+#define qed_nisar_stats_h
+
+
+// decorators
+namespace qed::nisar {
+    // stats for a complex HDF5 source
+    template <typename sourceT>
+    auto stats(
+        // the source
+        const dataset_t & source,
+        // the data layout
+        const datatype_t & datatype,
+        // the origin of the tile
+        typename sourceT::index_type origin,
+        // the tile shape
+        typename sourceT::shape_type tile) -> native::stats_t;
+}
+
+
+// pull in the implementations
+#define qed_nisar_stats_icc
+#include "stats.icc"
+#undef qed_nisar_stats_icc
+
+#endif
+
+// end of file
