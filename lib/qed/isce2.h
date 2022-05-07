beff4d0e223585370e2f7313dfccb8f362e397e1
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-07 16:18:15 -0600

    qed::isce2: exported the namespace

---
+++ lib/qed/isce2.h
@@ -0,0 +1,18 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_isce2_h)
+#define qed_isce2_h
+
+// publish
+// the api is in "isce2.api.h"
+#include "isce2/public.h"
+
+
+#endif
+
+// end of file
