d8ff35df4b25ebe7b1bf2e1b359003eb851bc84f
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-07 16:18:15 -0600

    qed::isce2: exported the namespace

---
+++ lib/qed/isce2/public.h
@@ -0,0 +1,25 @@
+// -*- C++ -*-
+// -*- coding: utf-8 -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_isce2_public_h)
+#define qed_isce2_public_h
+
+// external packages
+#include "externals.h"
+// get the forward declarations
+#include "forward.h"
+
+// published type aliases; this is the file you are looking for...
+#include "api.h"
+
+// implementation
+#include "interferogram/interferogram.h"
+
+
+#endif
+
+// end of file
