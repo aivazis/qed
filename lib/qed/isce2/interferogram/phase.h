ef167cfedd6a3d449661fa010df394bee7fd92e9
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-07 16:17:40 -0600

    qed::isce2: primed {interferogram}; its {channels} are currently the same as a {native} complex dataset

---
+++ lib/qed/isce2/interferogram/phase.h
@@ -0,0 +1,38 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2022 all rights reserved
+
+// code guard
+#if !defined(qed_isce2_interferorgram_channels_phase_h)
+#define qed_isce2_interferorgram_channels_phase_h
+
+
+// the phase tile generator
+namespace qed::isce2::interferogram::channels {
+    // the tile generator for the phase of a complex grid source
+    template <typename sourceT>
+    inline auto phase(
+        // the source
+        const sourceT & source,
+        // the zoom level
+        int zoom,
+        // the origin of the tile
+        typename sourceT::index_type origin,
+        // the tile shape
+        typename sourceT::shape_type tile,
+        // the hue interaval
+        double low, double high,
+        // the range of values to render
+        double saturation, double brightness) -> bmp_t;
+}
+
+
+// pull in the implementations
+#define qed_isce2_interferorgram_channels_phase_icc
+#include "phase.icc"
+#undef qed_isce2_interferorgram_channels_phase_icc
+
+#endif
+
+// end of file
