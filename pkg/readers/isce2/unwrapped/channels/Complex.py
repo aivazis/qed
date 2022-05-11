3fe8f03da16d9c42c643168b11044d72d5dc8781
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-05-10 21:31:31 -0700

    qed.readers: updated the controllers of the {complex} channel for the {unwrapped} data product in {isce2}

--- pkg/readers/isce2/unwrapped/channels/Complex.py
+++ pkg/readers/isce2/unwrapped/channels/Complex.py
@@ -4,6 +4,8 @@
 # (c) 1998-2022 all rights reserved
 
 
+# externals
+import cmath
 # support
 import qed
 # superclass
@@ -18,27 +20,44 @@ class Complex(Channel, family="qed.channels.isce2.int.complex"):
 
 
    # configurable state
-   range = qed.protocols.controller(default=qed.controllers.logRange)
-   range.doc = "the manager of the range of values to render"
+   scale = qed.protocols.controller(default=qed.controllers.value)
+   scale.doc = "the overall amplitude scaling"
+
+   exponent = qed.protocols.controller(default=qed.controllers.value)
+   exponent.doc = "the amplitude exponent"
 
    phase = qed.protocols.controller(default=qed.controllers.linearRange)
    phase.doc = "the manager of the range of values to render"
 
 
    # interface
-   def autotune(self, **kwds):
+   def autotune(self, stats, **kwds):
       """
       Use the {stats} gathered on a data sample to adjust the range configuration
       """
       # chain up
       super().autotune(**kwds)
-      # notify my range
-      self.range.autotune(**kwds)
+
+      # get π
+      π = cmath.pi
+
+      # set my scale
+      self.scale.value = 0.5
+      # and my exponent
+      self.exponent.value = 0.3
+      # record the mean amplitude
+      self.mean = stats[0][1]
+
+      # extract the high phase
+      high = stats[1][2]
+      # round up to the next multiple of 2π
+      max = 2 * π * (int(high / (2*π)) + 1)
       # adjust my range
       self.phase.min = 0
       self.phase.low = 0
-      self.phase.max = 1
-      self.phase.high = 1
+      self.phase.max = max
+      self.phase.high = high
+
       # all done
       return
 
@@ -49,8 +68,10 @@ class Complex(Channel, family="qed.channels.isce2.int.complex"):
       """
       # chain up
       yield from super().controllers(**kwds)
-      # my range
-      yield self.range, self.pyre_trait(alias="range")
+      # my scale
+      yield self.scale, self.pyre_trait(alias="scale")
+      # and my exponent
+      yield self.exponent, self.pyre_trait(alias="exponent")
       # my phase
       yield self.phase, self.pyre_trait(alias="phase")
       # all done
@@ -75,17 +96,37 @@ class Complex(Channel, family="qed.channels.isce2.int.complex"):
       return
 
 
-   def tile(self, **kwds):
+   def tile(self, source, zoom, origin, shape, **kwds):
       """
       Generate a tile of the given characteristics
       """
-      # unpack my configuration
-      low = 10**self.range.low
-      high = 10**self.range.high
-      lowPhase = self.phase.low
-      highPhase = self.phase.high
-      # add my configuration and chain up
-      return super().tile(min=low, max=high, minPhase=lowPhase, maxPhase=highPhase, **kwds)
+      # get my configuration
+      scale = self.scale.value
+      exponent = self.exponent.value
+      minPhase = self.phase.low
+      maxPhase = self.phase.high
+      # and the mean amplitude
+      mean = self.mean
+
+      # unpack the {tile} origin
+      line, sample = origin
+      # and its shape
+      lines, samples = shape
+
+      # turn the shape into a {pyre::grid::shape_t}
+      shape = qed.libpyre.grid.Shape3D(shape=(lines, 1, samples))
+      # and the origin into a {pyre::grid::index_t}
+      origin = qed.libpyre.grid.Index3D(index=(line, 0, sample))
+      # look for the tile maker in {libqed}
+      tileMaker = qed.libqed.isce2.unwrapped.channels.complex
+      # and ask it to make a tile
+      tile = tileMaker(source=source.data,
+                       zoom=zoom, origin=origin, shape=shape,
+                       mean=mean, scale=scale, exponent=exponent,
+                       minPhase=minPhase, maxPhase=maxPhase,
+                       **kwds)
+      # and return it
+      return tile
 
 
    # constants
