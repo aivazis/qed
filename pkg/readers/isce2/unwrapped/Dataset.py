3ac097bd53b0b22b431799969a1bb50d8695ae51
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-10-02 08:48:19 +0300

    qed.readers: the {isce2.unw} reader now understands dataset URIs and accepts local datasets only

--- pkg/readers/isce2/unwrapped/Dataset.py
+++ pkg/readers/isce2/unwrapped/Dataset.py
@@ -4,23 +4,27 @@
 # (c) 1998-2022 all rights reserved
 
 
+# externals
+import journal
+
 # support
 import qed
+
 # get my channels
 from .channels import channels as channelRegistry
 
 
 # raw dataset, i.e. a dataset in binary file with no metadata
-class Dataset(qed.flow.product,
-              family="qed.datasets.isce2.unw", implements=qed.protocols.dataset):
+class Dataset(
+    qed.flow.product, family="qed.datasets.isce2.unw", implements=qed.protocols.dataset
+):
     """
     A line interleaved unwrapped interferogram
     """
 
-
     # public data
     # the source
-    uri = qed.properties.path()
+    uri = qed.properties.uri()
     uri.default = None
     uri.doc = "the path to the data source"
 
@@ -34,7 +38,7 @@ class Dataset(qed.flow.product,
     channels.doc = "the table of channels supported by this dataset"
 
     origin = qed.properties.tuple(schema=qed.properties.int())
-    origin.default = 0,0
+    origin.default = 0, 0
     origin.doc = "the smallest possible index"
 
     shape = qed.properties.tuple(schema=qed.properties.int())
@@ -46,9 +50,8 @@ class Dataset(qed.flow.product,
     selector.doc = "a key/value map that identifies the dataset to its reader"
 
     tile = qed.properties.tuple(schema=qed.properties.int())
-    tile.default = 512,512
+    tile.default = 512, 512
     tile.doc = "the preferred shape of dataset subsets"
-
 
     # interface
     def channel(self, name):
@@ -57,7 +60,6 @@ class Dataset(qed.flow.product,
         """
         # look up the channel and return it
         return self.channels[name]
-
 
     def peek(self, pixel):
         """
@@ -74,7 +76,6 @@ class Dataset(qed.flow.product,
         # all done
         return
 
-
     def profile(self, points):
         """
         Sample my data along the path defined by {points}
@@ -84,7 +85,6 @@ class Dataset(qed.flow.product,
         # and return it
         return profile
 
-
     def render(self, channel, zoom, origin, shape):
         """
         Render a tile of the given specification
@@ -93,7 +93,6 @@ class Dataset(qed.flow.product,
         channel = self.channel(name=channel)
         # render a tile and return it
         return channel.tile(source=self, zoom=zoom, origin=origin, shape=shape)
-
 
     def summary(self):
         """
@@ -109,7 +108,6 @@ class Dataset(qed.flow.product,
 
         # as the only important channels
         return
-
 
     # metamethods
     def __init__(self, **kwds):
@@ -131,18 +129,37 @@ class Dataset(qed.flow.product,
         # all done
         return
 
-
     # implementation details
     def _open(self):
         """
         Initialize my data source
         """
+        # grab my uri
+        uri = self.uri
+        # we only support local datasets; if the {uri} points to anything else
+        if uri.scheme != "file":
+            # make a channel
+            channel = journal.error("qed.readers.native")
+            # complain
+            channel.line(f"while looking for {uri}")
+            channel.line(f"unsupported scheme '{uri.scheme}' in the dataset URI")
+            channel.line(f"the isce2.unw reader supports local datasets only")
+            channel.line(
+                f"please specify 'file:' as the dataset scheme, or drop it altogether"
+            )
+            # flush
+            channel.log()
+            # and bail
+            return
+
+        # grab the path to the dataset
+        path = str(uri.address)
         # build the name of the buffer factory
         memoryType = f"{self.cell.tag}ConstMap"
         # look up the factory in the {pyre::memory} bindings
         bufferFactory = getattr(qed.libpyre.memory, memoryType)
         # make the memory buffer
-        buffer = bufferFactory(str(self.uri))
+        buffer = bufferFactory(path)
 
         # realize the layout
         layout = qed.libpyre.grid.Shape3D(shape=self.layout)
@@ -154,7 +171,6 @@ class Dataset(qed.flow.product,
         data = gridFactory(packing, buffer)
         # and return it
         return data
-
 
     def _collectStatistics(self):
         """
@@ -168,7 +184,7 @@ class Dataset(qed.flow.product,
         # make a tile that fits within my shape
         tile = tuple(min(256, s) for s in shape)
         # center it in my shape
-        center = tuple((s-t)//2 for s,t in zip(shape, tile))
+        center = tuple((s - t) // 2 for s, t in zip(shape, tile))
 
         # make a pile for the channel stats
         stats = []
@@ -179,13 +195,14 @@ class Dataset(qed.flow.product,
             # extend the tile shape
             tile = qed.libpyre.grid.Shape3D(shape=(tile[0], 1, tile[1]))
             # compute the stats
-            channelStats = qed.libqed.isce2.unwrapped.stats(source=data, origin=center, shape=tile)
+            channelStats = qed.libqed.isce2.unwrapped.stats(
+                source=data, origin=center, shape=tile
+            )
             # and add them to the pile
             stats.append(channelStats)
 
         # and return them
         return stats
-
 
     def _registerChannels(self):
         """
