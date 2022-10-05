9ca73a455ce11b9a69e270ab633100805e56c9aa
Michael Aïvázis <michael.aivazis@para-sim.com>
2022-10-05 11:05:06 +0300

    qed.inspect: restructured the profiler; added timings for fetching tiles of various sizes

--- pkg/inspect/Profiler.py
+++ pkg/inspect/Profiler.py
@@ -22,8 +22,47 @@ class Profiler(qed.component, family="qed.inspect.profiler"):
         """
         Run a profile schedule and report performance
         """
+        # grab the profiling store
+        profile = qed.patterns.vivify(levels=2, atom=float)
+        # make a timer
+        tileTimer = qed.timers.wall("qed.profiler.tiles")
+
+        # go through the datasets
+        for dataset, discovery, stats in self.datasets():
+            # get the name of the dataset
+            name = dataset.pyre_name
+            # save the startup costs
+            profile[name]["discovery"] = discovery
+            profile[name]["stats"] = stats
+            # go through its channels
+            for channel in dataset.channels:
+                # make tile exponents
+                for tileExp in range(5, 11):
+                    # make a tile
+                    tile = (2**tileExp,) * 2
+                    # make zoom
+                    for zoom in range(1):
+                        # start the timer
+                        tileTimer.start()
+                        # attempt to
+                        try:
+                            # render a tile
+                            dataset.render(
+                                channel=channel, zoom=zoom, origin=(0, 0), shape=tile
+                            )
+                        # if anything goes wrong
+                        except qed.exceptions.PyreError:
+                            # ignore and move on
+                            continue
+                        # stop the timer
+                        tileTimer.stop()
+                        # record the time
+                        profile[name][channel, tile, zoom] = tileTimer.ms()
+                        # reset the timer
+                        tileTimer.reset()
+
         # send the report to the screen
-        self.screen()
+        self.screen(profile=profile)
         # all done
         return
 
@@ -31,16 +70,23 @@ class Profiler(qed.component, family="qed.inspect.profiler"):
     def __init__(self, readers, **kwds):
         # chain up
         super().__init__(**kwds)
+        # save the readers
+        self.readers = readers
+        # all done
+        return
 
-        # initialize my pile of datasets
-        datasets = {}
-        # and my profiling store
-        profile = qed.patterns.vivify(levels=2, atom=float)
-
-        # each reader can potentially serve more than one dataset; go through them
-        for reader in readers:
+    # implementation details
+    def datasets(self):
+        """
+        Ask all {readers} for their dataset
+        """
+        # go through my readers
+        for reader in self.readers:
             # get the name of the reader
             name = reader.pyre_name
+            # find out how many datasets it knows about
+            available = len(reader.datasets)
+
             # access the reader specific timers
             discoveryTimer = qed.timers.wall(f"qed.profiler.discovery.{name}")
             statsTimer = qed.timers.wall(f"qed.profiler.stats.{name}")
@@ -49,48 +95,32 @@ class Profiler(qed.component, family="qed.inspect.profiler"):
             # and the number of milliseconds it took to collect statistics
             stats = statsTimer.ms()
 
-            # make a pile of the datasets served by this reader
-            available = list(reader.datasets)
-            # set up the bookkeeping
-            for dataset in available:
-                # get the name of this dataset
-                name = dataset.pyre_name
-                # index the dataset by its name
-                datasets[name] = dataset
-                # and charge it its portion of the costs
-                profile["discovery"][name] = discovery / len(available)
-                profile["stats"][name] = stats / len(available)
-
-        # attach my datasets
-        self.datasets = datasets
-        # and my profiling store
-        self.profile = profile
+            # go through each dataset
+            for dataset in reader.datasets:
+                # and report it along with the access times
+                yield dataset, discovery / available, stats / available
 
         # all done
         return
 
-    # implementation details
-    def screen(self):
+    def screen(self, profile):
         """
         Generate a profiling report to the screen
         """
-        # grab my profiling store
-        profile = self.profile
-
         # make a channel
         channel = journal.info("qed.inspect.profiler")
         # sign on
         channel.line(f"profiler:")
-        channel.line(f"  number of datasets: {len(self.datasets)}")
+        channel.line(f"  number of datasets: {len(profile)}")
 
         # go through the datasets
-        for name in self.datasets:
+        for name, times in profile.items():
             # show me the dataset
             channel.line(f"    {name}")
             # and the profiling categories
-            for category in self.profile:
+            for category, cost in times.items():
                 # show me the times
-                channel.line(f"      {category}: {profile[category][name]:.3f} ms")
+                channel.line(f"      {category}: {cost:.3f} ms")
 
         # flush
         channel.log()
