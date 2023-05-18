d562eea022f8c5c751cc5f21adea8066097ca3d6
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-18 21:36:09 +0300

    qed.controllers: {linearRange} is now a bit more robust when picking its range

--- pkg/controllers/LinearRange.py
+++ pkg/controllers/LinearRange.py
@@ -6,6 +6,7 @@
 
 # external
 import math
+
 # support
 import qed
 
@@ -15,64 +16,60 @@ from .Controller import Controller
 
 # a mixin for a channel with rel values in a linear range
 class LinearRange(Controller, family="qed.controllers.linearrange"):
-   """
-   Configuration for a channel with data in a linear range
-   """
+    """
+    Configuration for a channel with data in a linear range
+    """
 
+    # user configurable state
+    low = qed.properties.float(default=None)
+    low.doc = "the lowest value; anything below is underflow"
 
-   # user configurable state
-   low = qed.properties.float(default=None)
-   low.doc = "the lowest value; anything below is underflow"
+    high = qed.properties.float(default=None)
+    high.doc = "the highest value; anything above is overflow"
 
-   high = qed.properties.float(default=None)
-   high.doc = "the highest value; anything above is overflow"
+    min = qed.properties.float(default=-1.0e3)
+    min.doc = "the smallest possible value"
 
-   min = qed.properties.float(default=-1.0e3)
-   min.doc = "the smallest possible value"
+    max = qed.properties.float(default=1.0e3)
+    max.doc = "the largest possible value"
 
-   max = qed.properties.float(default=1.0e+3)
-   max.doc = "the largest possible value"
+    # interface
+    def autotune(self, stats, **kwds):
+        """
+        Use the {stats} gathered on a data sample to adjust the range configuration
+        """
+        # chain up
+        super().autotune(stats=stats, **kwds)
 
+        # unpack the stats
+        _, mean, _ = stats
 
-   # interface
-   def autotune(self, stats, **kwds):
-      """
-      Use the {stats} gathered on a data sample to adjust the range configuration
-      """
-      # chain up
-      super().autotune(stats=stats, **kwds)
+        # we want to be conservative, as this logic is only supposed to make sure that
+        # the initial display is sensible
+        self.high = max(1, abs(4 * mean))
+        # use the opposite for the low end
+        self.low = -self.high
 
-      # unpack the stats
-      _, mean, _ = stats
+        # set the max value to the next higher power of 10
+        self.max = 10 ** (math.ceil(math.log10(self.high)))
+        # and the min to the opposite
+        self.min = -self.max
 
-      # we want to be conservative, as this logic is only supposed to make sure that
-      # the initial display is sensible
-      self.high = 4 * mean
-      # use the opposite for the low end
-      self.low = -self.high
+        # all done
+        return
 
-      # set the max value to the next higher power of 10
-      self.max = 10**(math.ceil(math.log10(self.high)))
-      # and the min to the opposite
-      self.min = -self.max
+    def updateRange(self, low, high):
+        """
+        Update my state with new values for the range
+        """
+        # update my state
+        self.low = low
+        self.high = high
+        # and let the caller know
+        return True
 
-      # all done
-      return
-
-
-   def updateRange(self, low, high):
-      """
-      Update my state with new values for the range
-      """
-      # update my state
-      self.low = low
-      self.high = high
-      # and let the caller know
-      return True
-
-
-   # constants
-   tag = "range"
+    # constants
+    tag = "range"
 
 
 # end of file
