2bf253f1b5dddb3541b14f5f4e2e9ad43f2bf893
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-19 18:49:21 +0300

    qed.readers.nisar: added {category} values to the {slc} channels

--- pkg/readers/nisar/products/channels/Complex.py
+++ pkg/readers/nisar/products/channels/Complex.py
@@ -6,99 +6,100 @@
 
 # support
 import qed
+
 # superclass
 from .Channel import Channel
 
 
 # a channel for displaying complex values
 class Complex(Channel, family="qed.channels.nisar.complex"):
-   """
-   Make a visualization pipeline to display complex values
-   """
+    """
+    Make a visualization pipeline to display complex values
+    """
 
+    # configurable state
+    range = qed.protocols.controller(default=qed.controllers.logRange)
+    range.doc = "the manager of the range of values to render"
 
-   # configurable state
-   range = qed.protocols.controller(default=qed.controllers.logRange)
-   range.doc = "the manager of the range of values to render"
+    phase = qed.protocols.controller(default=qed.controllers.linearRange)
+    phase.doc = "the manager of the range of values to render"
 
-   phase = qed.protocols.controller(default=qed.controllers.linearRange)
-   phase.doc = "the manager of the range of values to render"
+    saturation = qed.protocols.controller(default=qed.controllers.value)
+    saturation.doc = "the saturation"
 
-   saturation = qed.protocols.controller(default=qed.controllers.value)
-   saturation.doc = "the saturation"
+    # interface
+    def autotune(self, **kwds):
+        """
+        Use the {stats} gathered on a data sample to adjust the range configuration
+        """
+        # chain up
+        super().autotune(**kwds)
+        # notify my range
+        self.range.autotune(**kwds)
+        # adjust my range
+        self.phase.min = 0
+        self.phase.low = 0
+        self.phase.max = 1
+        self.phase.high = 1
+        # and my saturation
+        self.saturation.value = 1
+        # all done
+        return
 
+    def controllers(self, **kwds):
+        """
+        Generate the controllers that manipulate my state
+        """
+        # chain up
+        yield from super().controllers(**kwds)
+        # my range
+        yield self.range, self.pyre_trait(alias="range")
+        # my phase
+        yield self.phase, self.pyre_trait(alias="phase")
+        # and my saturation
+        yield self.saturation, self.pyre_trait(alias="saturation")
+        # all done
+        return
 
-   # interface
-   def autotune(self, **kwds):
-      """
-      Use the {stats} gathered on a data sample to adjust the range configuration
-      """
-      # chain up
-      super().autotune(**kwds)
-      # notify my range
-      self.range.autotune(**kwds)
-      # adjust my range
-      self.phase.min = 0
-      self.phase.low = 0
-      self.phase.max = 1
-      self.phase.high = 1
-      # and my saturation
-      self.saturation.value = 1
-      # all done
-      return
+    def eval(self, pixel):
+        """
+        Get the {pixel} value
+        """
+        # easy enough
+        return pixel
 
+    def project(self, pixel):
+        """
+        Represent a {pixel} as a complex number
+        """
+        # only one rep
+        yield pixel, ""
+        # all done
+        return
 
-   def controllers(self, **kwds):
-      """
-      Generate the controllers that manipulate my state
-      """
-      # chain up
-      yield from super().controllers(**kwds)
-      # my range
-      yield self.range, self.pyre_trait(alias="range")
-      # my phase
-      yield self.phase, self.pyre_trait(alias="phase")
-      # and my saturation
-      yield self.saturation, self.pyre_trait(alias="saturation")
-      # all done
-      return
+    def tile(self, **kwds):
+        """
+        Generate a tile of the given characteristics
+        """
+        # unpack my configuration
+        low = 10**self.range.low
+        high = 10**self.range.high
+        lowPhase = self.phase.low
+        highPhase = self.phase.high
+        saturation = self.saturation.value
+        # add my configuration and chain up
+        return super().tile(
+            min=low,
+            max=high,
+            minPhase=lowPhase,
+            maxPhase=highPhase,
+            saturation=saturation,
+            **kwds
+        )
 
-
-   def eval(self, pixel):
-      """
-      Get the {pixel} value
-      """
-      # easy enough
-      return pixel
-
-
-   def project(self, pixel):
-      """
-      Represent a {pixel} as a complex number
-      """
-      # only one rep
-      yield pixel, ""
-      # all done
-      return
-
-
-   def tile(self, **kwds):
-      """
-      Generate a tile of the given characteristics
-      """
-      # unpack my configuration
-      low = 10**self.range.low
-      high = 10**self.range.high
-      lowPhase = self.phase.low
-      highPhase = self.phase.high
-      saturation = self.saturation.value
-      # add my configuration and chain up
-      return super().tile(min=low, max=high,
-         minPhase=lowPhase, maxPhase=highPhase, saturation=saturation, **kwds)
-
-
-   # constants
-   tag = "complex"
+    # constants
+    tag = "complex"
+    category = "slc"
 
 
 # end of file
--- pkg/readers/nisar/products/channels/Phase.py
+++ pkg/readers/nisar/products/channels/Phase.py
@@ -7,110 +7,107 @@
 # externals
 import cmath
 import qed
+
 # superclass
 from .Channel import Channel
 
 
 # a channel for displaying the phase of complex values
 class Phase(Channel, family="qed.channels.nisar.phase"):
-   """
-   Make a visualization pipeline to display the phase of complex values
-   """
+    """
+    Make a visualization pipeline to display the phase of complex values
+    """
 
+    # user configurable state
+    phase = qed.protocols.controller(default=qed.controllers.linearRange)
+    phase.doc = "the manager of the range of values to render"
 
-   # user configurable state
-   phase = qed.protocols.controller(default=qed.controllers.linearRange)
-   phase.doc = "the manager of the range of values to render"
+    brightness = qed.protocols.controller(default=qed.controllers.value)
+    brightness.doc = "the brightness"
 
-   brightness = qed.protocols.controller(default=qed.controllers.value)
-   brightness.doc = "the brightness"
+    saturation = qed.protocols.controller(default=qed.controllers.value)
+    saturation.doc = "the saturation"
 
-   saturation = qed.protocols.controller(default=qed.controllers.value)
-   saturation.doc = "the saturation"
+    # interface
+    def autotune(self, **kwds):
+        # chain up
+        super().autotune(**kwds)
+        # adjust my range
+        self.phase.min = 0
+        self.phase.low = 0
+        self.phase.max = 1
+        self.phase.high = 1
+        # my brightness
+        self.brightness.value = 1
+        # and my saturation
+        self.saturation.value = 1
+        # all done
+        return
 
+    def controllers(self, **kwds):
+        """
+        Generate the controllers that manipulate my state
+        """
+        # chain up
+        yield from super().controllers(**kwds)
+        # my phase
+        yield self.phase, self.pyre_trait(alias="phase")
+        # my brightness
+        yield self.brightness, self.pyre_trait(alias="brightness")
+        # and my saturation
+        yield self.saturation, self.pyre_trait(alias="saturation")
+        # all done
+        return
 
-   # interface
-   def autotune(self, **kwds):
-      # chain up
-      super().autotune(**kwds)
-      # adjust my range
-      self.phase.min = 0
-      self.phase.low = 0
-      self.phase.max = 1
-      self.phase.high = 1
-      # my brightness
-      self.brightness.value = 1
-      # and my saturation
-      self.saturation.value = 1
-      # all done
-      return
+    def eval(self, pixel):
+        """
+        Get the {pixel} value
+        """
+        # easy enough
+        return cmath.phase(pixel)
 
+    def project(self, pixel):
+        """
+        Compute the phase of a {pixel}
+        """
+        # get the value as angle in radians in [-π, π]
+        # N.B.: the range interval is closed thanks to the peculiarities of {atan2}
+        value = cmath.phase(pixel) / cmath.pi
 
-   def controllers(self, **kwds):
-      """
-      Generate the controllers that manipulate my state
-      """
-      # chain up
-      yield from super().controllers(**kwds)
-      # my phase
-      yield self.phase, self.pyre_trait(alias="phase")
-      # my brightness
-      yield self.brightness, self.pyre_trait(alias="brightness")
-      # and my saturation
-      yield self.saturation, self.pyre_trait(alias="saturation")
-      # all done
-      return
+        # project
+        # in π radians
+        yield value, "π radians"
 
+        # transform to [0, 2π]
+        if value < 0:
+            # by adding a whole cycle to negative values
+            value += 2
 
-   def eval(self, pixel):
-      """
-      Get the {pixel} value
-      """
-      # easy enough
-      return cmath.phase(pixel)
+        # in degrees in [0, 360]
+        yield 180 * value, "degrees"
+        # in cycles, in [0,1]
+        yield value / 2, "cycles"
 
+        # all done
+        return
 
-   def project(self, pixel):
-      """
-      Compute the phase of a {pixel}
-      """
-      # get the value as angle in radians in [-π, π]
-      # N.B.: the range interval is closed thanks to the peculiarities of {atan2}
-      value = cmath.phase(pixel) / cmath.pi
+    def tile(self, **kwds):
+        """
+        Generate a tile of the given characteristics
+        """
+        # unpack my configuration
+        low = self.phase.low
+        high = self.phase.high
+        saturation = self.saturation.value
+        brightness = self.brightness.value
+        # add my configuration and chain up
+        return super().tile(
+            low=low, high=high, saturation=saturation, brightness=brightness, **kwds
+        )
 
-      # project
-      # in π radians
-      yield value, "π radians"
-
-      # transform to [0, 2π]
-      if value < 0:
-         # by adding a whole cycle to negative values
-         value += 2
-
-      # in degrees in [0, 360]
-      yield 180*value, "degrees"
-      # in cycles, in [0,1]
-      yield value/2, "cycles"
-
-      # all done
-      return
-
-
-   def tile(self, **kwds):
-      """
-      Generate a tile of the given characteristics
-      """
-      # unpack my configuration
-      low = self.phase.low
-      high = self.phase.high
-      saturation = self.saturation.value
-      brightness = self.brightness.value
-      # add my configuration and chain up
-      return super().tile(low=low, high=high, saturation=saturation, brightness=brightness, **kwds)
-
-
-   # constants
-   tag = "phase"
+    # constants
+    tag = "phase"
+    category = "slc"
 
 
 # end of file
