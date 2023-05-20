92e70cf47b897df26e293755a83b80857034f06a
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-20 16:40:07 +0300

    qed.readers.native: noise by {black}

--- pkg/readers/native/channels/Imaginary.py
+++ pkg/readers/native/channels/Imaginary.py
@@ -6,75 +6,69 @@
 
 # support
 import qed
+
 # superclass
 from .Channel import Channel
 
 
 # a channel for displaying the imaginary part of complex values
 class Imaginary(Channel, family="qed.channels.native.imaginary"):
-   """
-   Make a visualization pipeline to display the imaginary part of complex values
-   """
+    """
+    Make a visualization pipeline to display the imaginary part of complex values
+    """
 
+    # configurable state
+    range = qed.protocols.controller(default=qed.controllers.linearRange)
+    range.doc = "the manager of the range of values to render"
 
-   # configurable state
-   range = qed.protocols.controller(default=qed.controllers.linearRange)
-   range.doc = "the manager of the range of values to render"
+    # interface
+    def autotune(self, **kwds):
+        """
+        Use the {stats} gathered on a data sample to adjust the range configuration
+        """
+        # chain up
+        super().autotune(**kwds)
+        # notify my range
+        self.range.autotune(**kwds)
+        # all done
+        return
 
+    def controllers(self, **kwds):
+        """
+        Generate the controllers that manipulate my state
+        """
+        # chain up
+        yield from super().controllers(**kwds)
+        # my range
+        yield self.range, self.pyre_trait(alias="range")
+        # all done
+        return
 
-   # interface
-   def autotune(self, **kwds):
-      """
-      Use the {stats} gathered on a data sample to adjust the range configuration
-      """
-      # chain up
-      super().autotune(**kwds)
-      # notify my range
-      self.range.autotune(**kwds)
-      # all done
-      return
+    def eval(self, pixel):
+        """
+        Get the {pixel} value
+        """
+        # easy enough
+        return pixel.imag
 
+    def project(self, pixel):
+        """
+        Compute the imaginary part of a {pixel}
+        """
+        # easy
+        yield pixel.imag, ""
+        # all done
+        return
 
-   def controllers(self, **kwds):
-      """
-      Generate the controllers that manipulate my state
-      """
-      # chain up
-      yield from super().controllers(**kwds)
-      # my range
-      yield self.range, self.pyre_trait(alias="range")
-      # all done
-      return
+    def tile(self, **kwds):
+        """
+        Generate a tile of the given characteristics
+        """
+        # add my configuration and chain up
+        return super().tile(min=self.range.low, max=self.range.high, **kwds)
 
-
-   def eval(self, pixel):
-      """
-      Get the {pixel} value
-      """
-      # easy enough
-      return pixel.imag
-
-
-   def project(self, pixel):
-      """
-      Compute the imaginary part of a {pixel}
-      """
-      # easy
-      yield pixel.imag, ""
-      # all done
-      return
-
-
-   def tile(self, **kwds):
-      """
-      Generate a tile of the given characteristics
-      """
-      # add my configuration and chain up
-      return super().tile(min=self.range.low, max=self.range.high, **kwds)
-
-
-   # constants
-   tag = "imaginary"
+    # constants
+    tag = "imaginary"
 
 
 # end of file
--- pkg/readers/native/channels/Real.py
+++ pkg/readers/native/channels/Real.py
@@ -6,75 +6,69 @@
 
 # support
 import qed
+
 # superclass
 from .Channel import Channel
 
 
 # a channel for displaying the real part of complex values
 class Real(Channel, family="qed.channels.native.real"):
-   """
-   Make a visualization pipeline to display the real part of complex values
-   """
+    """
+    Make a visualization pipeline to display the real part of complex values
+    """
 
+    # configurable state
+    range = qed.protocols.controller(default=qed.controllers.linearRange)
+    range.doc = "the manager of the range of values to render"
 
-   # configurable state
-   range = qed.protocols.controller(default=qed.controllers.linearRange)
-   range.doc = "the manager of the range of values to render"
+    # interface
+    def autotune(self, **kwds):
+        """
+        Use the {stats} gathered on a data sample to adjust the range configuration
+        """
+        # chain up
+        super().autotune(**kwds)
+        # notify my range
+        self.range.autotune(**kwds)
+        # all done
+        return
 
+    def controllers(self, **kwds):
+        """
+        Generate the controllers that manipulate my state
+        """
+        # chain up
+        yield from super().controllers(**kwds)
+        # my range
+        yield self.range, self.pyre_trait(alias="range")
+        # all done
+        return
 
-   # interface
-   def autotune(self, **kwds):
-      """
-      Use the {stats} gathered on a data sample to adjust the range configuration
-      """
-      # chain up
-      super().autotune(**kwds)
-      # notify my range
-      self.range.autotune(**kwds)
-      # all done
-      return
+    def eval(self, pixel):
+        """
+        Get the {pixel} value
+        """
+        # easy enough
+        return pixel.real
 
+    def project(self, pixel):
+        """
+        Compute the real part of a {pixel}
+        """
+        # only one rep
+        yield pixel.real, ""
+        # all done
+        return
 
-   def controllers(self, **kwds):
-      """
-      Generate the controllers that manipulate my state
-      """
-      # chain up
-      yield from super().controllers(**kwds)
-      # my range
-      yield self.range, self.pyre_trait(alias="range")
-      # all done
-      return
+    def tile(self, **kwds):
+        """
+        Generate a tile of the given characteristics
+        """
+        # add my configuration and chain up
+        return super().tile(min=self.range.low, max=self.range.high, **kwds)
 
-
-   def eval(self, pixel):
-      """
-      Get the {pixel} value
-      """
-      # easy enough
-      return pixel.real
-
-
-   def project(self, pixel):
-      """
-      Compute the real part of a {pixel}
-      """
-      # only one rep
-      yield pixel.real, ""
-      # all done
-      return
-
-
-   def tile(self, **kwds):
-      """
-      Generate a tile of the given characteristics
-      """
-      # add my configuration and chain up
-      return super().tile(min=self.range.low, max=self.range.high, **kwds)
-
-
-   # constants
-   tag = "real"
+    # constants
+    tag = "real"
 
 
 # end of file
