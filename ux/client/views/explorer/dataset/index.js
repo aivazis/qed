22697e33032bbf8c3ab92308dc6469fce82e9d37
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-10-26 14:36:25 +0300

    ux/views: primed the new home for the {dataset} connection form

---
+++ ux/client/views/explorer/dataset/index.js
@@ -0,0 +1,10 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2023 all rights reserved
+
+
+// publish
+export { Selector } from './selector'
+
+// end of file
