3d55d669b211e33f88ff2a69074b54393e5f3b1b
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-10-26 14:36:12 +0300

    ux/views: primed the new home for the {archive} connection form

---
+++ ux/client/views/explorer/archive/selector.js
@@ -0,0 +1,45 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2023 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// locals
+// hooks
+import { useViews } from '../explorer/useViews'
+// styles
+import { selector as paintSelector } from './styles'
+
+
+// the decorator of a {dataset} tab
+export const Selector = ({ viewport, view }) => {
+    // get the active view
+    const { activeViewport } = useViews()
+
+    // unpack the view
+    const { archive } = view
+    // get the archive uri
+    const uri = "scheme://somewhere/some/path"
+
+    // deduce my state
+    const state = (viewport === activeViewport) ? "selected" : "enabled"
+    // mix my paint
+    const paint = paintSelector(state)
+    // and render
+    return (
+        <span style={paint.box}>
+            {/* the archive name */}
+            <span style={paint.name}>connect archive</span>
+            {/* a separator */}
+            <span style={paint.separator}>&middot;</span>
+            {/* the archive uri */}
+            <span style={paint.selector}>{uri}</span>
+        </span>
+    )
+}
+
+
+// end of file
---
+++ ux/client/views/explorer/archive/styles.js
@@ -0,0 +1,17 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2023 all rights reserved
+
+
+// get colors
+import { theme, wheel } from "~/palette"
+// and the selector styling from the explorer
+import { selector as baseSelector } from '../explorer/styles'
+
+
+// just use the base selector
+export const selector = baseSelector
+
+
+// end of file
