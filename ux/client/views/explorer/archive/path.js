84b37f69ef3ed09688f5a30bdea8313ff7c1c531
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-10-29 16:25:03 +0200

    ux/views: added {path} for the new {archive} form

--- ux/client/views/explorer/archive/path.js
+++ ux/client/views/explorer/archive/path.js
@@ -7,10 +7,33 @@
 // external
 import React from 'react'
 
+// local
+// components
+import { EnabledInput, Field, Value } from '../form'
+
 // the path the archive
-export const Path = () => {
+export const Path = ({ value, update }) => {
+    // build the path mutator
+    const setPath = evt => {
+        // set up the validation regex
+        const regex = /^.*$/g
+        // get the value
+        const candidate = evt.target.value
+        // update the form state
+        update("path", regex.test(candidate) ? candidate : value)
+    }
+    // assemble my behaviors
+    const behaviors = {
+        onChange: setPath,
+    }
     // all done
-    return null
+    return (
+        <Field name="path" value={value} tip="the path to the data archive">
+            <Value>
+                <EnabledInput type="text" value={value === null ? "" : value} {...behaviors} />
+            </Value>
+        </Field>
+    )
 }
 
 
