faf513f5b9dab76df88430c5d42a4b7e84d8b58c
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-10-29 16:22:47 +0200

    qed.gql: primed the mutation that disconnects a data archive

---
+++ pkg/gql/DisconnectArchive.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2023 all rights reserved
+
+
+# externals
+import graphene
+import journal
+
+# the input payload
+from .DisconnectArchiveInput import DisconnectArchiveInput
+
+# the result types
+from .Archive import Archive
+
+
+# remove a data archive from the pile
+class DisconnectArchive(graphene.Mutation):
+    """
+    Disconnect a data archive
+    """
+
+    # inputs
+    class Arguments:
+        # the update context
+        uri = graphene.String(required=True)
+
+    # the result is always an archive
+    archive = graphene.Field(Archive)
+
+    # the range controller mutator
+    def mutate(root, info, uri):
+        """
+        add a new archive to the pile
+        """
+        # grab the panel
+        panel = info.context["panel"]
+        # show me
+        channel = journal.info("qed.gql.archives")
+        channel.log(f"disconnecting {uri}")
+        # and resolve the mutation
+        return None
+
+
+# end of file
