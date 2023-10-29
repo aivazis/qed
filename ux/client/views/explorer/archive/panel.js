31a0d407aafc7f5b5cff335e6d096366fdfb74bd
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-10-29 16:25:45 +0200

    ux/views: factored out the {panel} so it can be shared by all {archive} forms

---
+++ ux/client/views/explorer/archive/panel.js
@@ -0,0 +1,21 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2023 all rights reserved
+
+
+// external
+import styled from 'styled-components'
+
+
+// the editor panel
+export const Panel = styled.div`
+    font-size: 70%;
+    padding: 1.0rem 0.0 0.5rem 0.0em;
+    // border-top: 1px solid hsl(0, 0%, 15%);
+    // border-bottom: 1px solid hsl(0, 0%, 15%);
+    // background-color: hsl(0deg, 0%, 7%);
+`
+
+
+// end of file
