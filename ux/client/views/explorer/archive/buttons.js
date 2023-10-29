b64ca987b005f3e988f5d4c98f4e8ad3d80063fc
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-10-29 12:02:36 +0200

    ux/views: primed the {buttons} on the new {archive} form

---
+++ ux/client/views/explorer/archive/buttons.js
@@ -0,0 +1,56 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2023 all rights reserved
+
+
+// external
+import React from 'react'
+import styled from 'styled-components'
+
+
+// local
+// components
+import { Button, EnabledPrimaryButton, EnabledSecondaryButton } from '../form'
+
+
+// the cancel button
+export const Cancel = styled(EnabledSecondaryButton)`
+`
+
+export const EnabledConnect = ({ connect, hide }) => {
+    // build the handler that creates a new record
+    const createRecord = evt => {
+        // stop this event from bubbling up
+        evt.stopPropagation()
+        // and quash any side effects
+        evt.preventDefault()
+        // add the record
+        connect()
+        // hide the editor
+        hide()
+        // all done
+        return
+    }
+    // assemble the behaviors
+    const behaviors = {
+        onClick: createRecord,
+    }
+    // render
+    return (
+        <EnabledPrimaryButton {...behaviors}>
+            connect
+        </EnabledPrimaryButton>
+    )
+}
+
+export const DisabledConnect = ({ children }) => {
+    // render
+    return (
+        <Button>
+            connect
+        </Button>
+    )
+}
+
+// end of file
