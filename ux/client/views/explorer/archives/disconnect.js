e90103767ed6a176802ddfdfa9c8bb9eb857a684
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-10-31 14:12:48 +0200

    ux/views: fleshed out the {disconnect} control that removes a data archive from the pile

--- ux/client/views/explorer/archives/disconnect.js
+++ ux/client/views/explorer/archives/disconnect.js
@@ -6,32 +6,73 @@
 
 // external
 import React from 'react'
+import { graphql, useMutation } from 'react-relay/hooks'
 
 // project
 // shapes
 import { X as Icon } from '~/shapes'
 // widgets
 import { Badge } from '~/widgets'
+
+// local
 // styles
 import { disconnect as paintDisconnect } from './styles'
 
 
 // control to connect a new data archive
 export const Disconnect = ({ uri }) => {
+    // build the mutation request
+    const [request, isInFlight] = useMutation(disconnectMutation)
     // connect a new archive
-    const connect = evt => {
+    const disconnect = evt => {
         // stop this event from propagating
         evt.stopPropagation()
         // and quash any side effects
         evt.preventDefault()
-        // show me
-        console.log(`disconnecting ${uri}`)
+        // if there is already a pending operation
+        if (isInFlight) {
+            // skip the update
+            return
+        }
+        // otherwise, send the mutation to the server
+        request({
+            // input
+            variables: {
+                // the payload
+                uri
+            },
+            // update the store
+            updater: store => {
+                // get the root field of the query result
+                const payload = store.getRootField("disconnectArchive")
+                // ask for the target archive
+                const archive = payload.getLinkedRecord("archive")
+                // get the session manager
+                const qed = store.get("QED")
+                // get its connected archives
+                const archives = qed.getLinkedRecords("archives")
+                // remove our target from the pile
+                const filtered = archives.filter(
+                    // by filtering out entries that match its id
+                    entry => entry.getDataID() !== archive.getDataID()
+                )
+                // add the new one to the pile
+                qed.setLinkedRecords(filtered, "archives")
+                // all done
+                return
+            },
+            // when done
+            onCompleted: data => {
+                // not much to do, for now
+                return
+            }
+        })
         // all done
         return
     }
     // set up my behaviors
     const behaviors = {
-        onClick: connect,
+        onClick: disconnect,
     }
     // render
     return (
@@ -41,5 +82,18 @@ export const Disconnect = ({ uri }) => {
 
     )
 }
+
+
+// the mutation that disconnects an  archive
+const disconnectMutation = graphql`
+    mutation disconnectArchiveMutation($uri: String!) {
+        disconnectArchive(uri: $uri) {
+            archive {
+                id
+            }
+        }
+    }
+`
+
 
 // end of file
