1b8d961dee6761168356123647bd93bd45a2cf04
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-11-01 08:57:35 +0200

    qed.gql: the {qed} session manager now resolves {readers} against the {panel}

--- pkg/gql/QED.py
+++ pkg/gql/QED.py
@@ -58,10 +58,8 @@ class QED(graphene.ObjectType):
         """
         Generate a list of all known dataset readers
         """
-        # get the plexus
-        plexus = info.context["plexus"]
-        # get the datasets and return them
-        return plexus.datasets
+        # get the registered readers and return them
+        return tuple(panel.readers.values())
 
 
 # end of file
