13eb08998ee4770cb0b72abf9ad2556f44f50f9c
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-01-05 22:46:38 +0200

    ux/shapes: added a {logo} candidate

---
+++ ux/client/shapes/qed/index.js
@@ -0,0 +1,24 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// externals
+import React from 'react'
+// locals
+import styles from './styles'
+
+
+// render the logo
+export const Logo = ({ style }) => {
+    // paint me
+    return (
+        <g transform="rotate(-45 500 500)">
+            <text x={500} y={700} style={styles.logo}>οεδ</text>
+        </g>
+    )
+}
+
+
+// end of file
---
+++ ux/client/shapes/qed/styles.js
@@ -0,0 +1,26 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// get colors
+import { theme } from '~/palette'
+
+// the text color
+const paint = theme.page.name
+
+// publish
+export default {
+    // logo
+    logo: {
+        fontFamily: "didot",
+        fontSize: 700,
+        stroke: "none",
+        fill: paint,
+        textAnchor: "middle",
+    }
+}
+
+
+// end of file
