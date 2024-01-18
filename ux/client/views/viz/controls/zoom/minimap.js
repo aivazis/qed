1fb0d234d46a6e95172b499d50547ebdf9066ab7
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-01-18 11:48:14 +0200

    ux/views: the {viewport} rep in the {minimap} of the {zoom} control can now be dragged around to scroll the viewport

--- ux/client/views/viz/controls/zoom/minimap.js
+++ ux/client/views/viz/controls/zoom/minimap.js
@@ -15,12 +15,21 @@ import { theme } from '~/palette'
 
 
 // export the minimap
-export const Minimap = ({ shape, zoom }) => {
+export const Minimap = ({ ils, shape, zoom }) => {
     // we have
     // - the shape of the dataset in the active view
     // - the current zoom level
     // we need
     // - the origin + shape of the active viewport
+
+    // make a ref for the placemat
+    const placemat = React.useRef(null)
+    // and one for the viewport rep
+    const rep = React.useRef(null)
+    // the dragging flag, used for dragging the minimap rendering of the viewport
+    const [dragging, setDragging] = React.useState(null)
+    // and a place to park position information to support the dragging calculations
+    const cursor = React.useRef(null)
 
     // storage for the origin of the viewport
     const [origin, setOrigin] = React.useState({ x: 0, y: 0 })
@@ -28,14 +37,24 @@ export const Minimap = ({ shape, zoom }) => {
     const [extent, setExtent] = React.useState({ width: 0, height: 0 })
     // get the viewport information
     const { activeViewport, viewports } = useViewports()
-    // build a listener that tracks the size of the active viewport
-    // and one that tracks its origin
+
+    // unpack the dataset shape
+    const [dHeight, dWidth] = shape
+    // unpack the zoom
+    const [zoomX, zoomY] = [2 ** zoom, 2 ** zoom]
+    // unpack the viewport origin
+    const { x, y } = origin
+    // and its extent
+    const { width, height } = extent
+    // set the scale
+    const scale = 1 / Math.max(dWidth, zoomX * (x + width),
+        dHeight, zoomY * (y + height))
 
     // install the trackers on the active viewport
     React.useEffect(() => {
         // get the active viewport ref
         const viewport = viewports[activeViewport]
-        // if it's not a valid
+        // if we don't have one
         if (!viewport) {
             // bail
             return
@@ -89,7 +108,7 @@ export const Minimap = ({ shape, zoom }) => {
         observer.observe(viewport)
 
         // now, handle viewport scrolling
-        const scroll = evt => {
+        const scroll = () => {
             // deduce the scroll position
             const origin = { x: viewport.scrollLeft, y: viewport.scrollTop }
             // update the viewport origin
@@ -100,40 +119,116 @@ export const Minimap = ({ shape, zoom }) => {
         // by registering a listener
         viewport.addEventListener("scroll", scroll)
 
+        // event handlers for dragging the viewport rep within the minimap
+        // initiate the dragging
+        const startDragging = evt => {
+            // stop this event from bubbling up
+            evt.stopPropagation()
+            // and quash any side effect
+            evt.preventDefault()
+            // set the flag
+            setDragging(true)
+            // record the event location
+            cursor.current = { x: evt.clientX, y: evt.clientY }
+            // all done
+            return
+        }
+        // terminate the dragging
+        const stopDragging = evt => {
+            // stop this event from bubbling up
+            evt.stopPropagation()
+            // and quash any side effect
+            evt.preventDefault()
+            // set the flag
+            setDragging(false)
+            // record the event location
+            cursor.current = { x: evt.clientX, y: evt.clientY }
+            // all done
+            return
+        }
+
+        // attach the mouse down handler to the viewport rep
+        rep.current.addEventListener("mousedown", startDragging)
+        // and the rest to the placemat
+        placemat.current.addEventListener("mouseup", stopDragging)
+        placemat.current.addEventListener("mouseleave", stopDragging)
+
         // register a clean up
         return () => {
             // if we have a valid viewport
             if (viewport) {
                 // disconnect it from the observer
                 observer.disconnect()
-                // and remove the scroll listener
+                // remove the scroll listener
                 viewport.removeEventListener("scroll", scroll)
+            }
+            // if the viewport rep is still around
+            if (rep.current) {
+                // remove its event listeners
+                rep.current.removeEventListener("mousedown", startDragging)
+            }
+            // if the placemat is still around
+            if (placemat.current) {
+                // remove its event listeners
+                placemat.current.removeEventListener("mouseup", stopDragging)
+                placemat.current.removeEventListener("mouseleave", stopDragging)
             }
             // all done
             return
         }
     }, [viewports, activeViewport])
 
-    // unpack the dataset shape
-    const [dHeight, dWidth] = shape
-    // unpack the zoom
-    const [zoomX, zoomY] = [2 ** zoom, 2 ** zoom]
-    // unpack the viewport origin
-    const { x, y } = origin
-    // and its extent
-    const { width, height } = extent
+    // the mouse move listener is attached separately because it depends on the dragging state
+    React.useEffect(() => {
+        // get the active viewport ref
+        const viewport = viewports[activeViewport]
+        // if we don't have one
+        if (!viewport) {
+            // bail
+            return
+        }
+        // move the viewport ref
+        const drag = evt => {
+            // if we are not dragging the viewport ref
+            if (!dragging) {
+                return
+            }
+            // otherwise, get the position of the mouse
+            const x = evt.clientX
+            const y = evt.clientY
+            // compute the displacement in viewport pixels since the last time we took a measurement
+            const dx = x - cursor.current.x
+            const dy = y - cursor.current.y
+            // ask the active viewport to scroll by this much
+            viewport.scrollLeft += dx / (ils * scale * zoomX)
+            viewport.scrollTop += dy / (ils * scale * zoomY)
+            // record the event location
+            cursor.current = { x, y }
+            // all done
+            return
+        }
+        // attach the mouse movement handler to the placemat
+        placemat.current.addEventListener("mousemove", drag)
+        // register the clean up
+        return () => {
+            // if the placemat is still around
+            if (placemat.current) {
+                // remove the mouse movement listener
+                placemat.current.removeEventListener("mousemove", drag)
+            }
+            // all done
+            return
+        }
 
-    // set the scale
-    const scale = Math.max(dWidth, zoomX * (x + width),
-        dHeight, zoomY * (y + height))
+    }, [viewports, activeViewport, dragging, scale])
 
     // render
     return (
-        <g>
+        <g ref={placemat}>
             <Placemat x={0} y={0} width={1} height={1} />
-            <Data x={0} y={0} width={dWidth / scale} height={dHeight / scale} />
-            <Viewport x={zoomX * x / scale} y={zoomY * y / scale}
-                width={zoomX * width / scale} height={zoomY * height / scale} />
+            <Data x={0} y={0} width={dWidth * scale} height={dHeight * scale} />
+            <Viewport ref={rep} x={zoomX * x * scale} y={zoomY * y * scale}
+                width={zoomX * width * scale} height={zoomY * height * scale} />
         </g>
     )
 }
@@ -145,11 +240,13 @@ const Placemat = styled.rect`
 `
 
 const Viewport = styled.rect`
-    fill: none;
+    fill: hsl(0deg, 0%, 0%, 0%);
 
     stroke: ${theme.page.name};
     stroke-width: 1;
     vector-effect: non-scaling-stroke;
+
+    cursor: pointer;
 `
 
 const Data = styled.rect`
