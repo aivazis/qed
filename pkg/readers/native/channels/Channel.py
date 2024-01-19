1ef04638d1fc78da8a968964cf810d836d91a025
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-01-19 13:59:16 +0200

    qed.readers: narrative improvements in the {native} channel base

--- pkg/readers/native/channels/Channel.py
+++ pkg/readers/native/channels/Channel.py
@@ -61,8 +61,7 @@ class Channel(qed.flow.dynamic, implements=qed.protocols.channel):
         origin = qed.libpyre.grid.Index2D(index=origin)
         # and the zoom into strides
         stride = qed.libpyre.grid.Index2D(index=tuple(2**level for level in zoom))
-
-        # ask it to make a tile and return it
+        # build the visualization pipeline and return it
         return pipeline(
             source=source.data, origin=origin, shape=shape, stride=stride, **kwds
         )
