65787a88f080d43e3d207e433bf9d6516334291a
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-01-28 23:26:49 +0200

    ux/views: the {useVisualize} hook of {viz} now updates all channel synced viewports

--- ux/client/views/viz/viz/useVisualize.js
+++ ux/client/views/viz/viz/useVisualize.js
@@ -16,20 +16,34 @@ import { Context } from './context'
 // currently, only called by {reader} instances when selected/updated
 export const useVisualize = () => {
     // grab the active viewport index and the {views} mutator
-    const { activeViewport, setViews } = React.useContext(Context)
+    const { activeViewport, setViews, synced } = React.useContext(Context)
 
     // make the handler
     const visualize = (view, viewport = activeViewport) => {
+        // unpack the view
+        const { channel } = view
         // adjust the view
         setViews(old => {
             // make a copy of the old state
             const clone = [...old]
             // adjust the entry specified by the caller
             clone[viewport] = { ...old[viewport], ...view }
+            // if i'm channel synced
+            if (synced[viewport].channel) {
+                // go through the sync table
+                synced.forEach((sync, port) => {
+                    // if this port is channel synced and it understands this channel
+                    if (sync.channel && clone[port].dataset.channels.includes(channel)) {
+                        // adjust the channel
+                        clone[port].channel = channel
+                    }
+                    // all done
+                    return
+                })
+            }
             // and hand off the new state
             return clone
         })
-
         // all done
         return
     }
