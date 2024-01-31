d7f2cbb6f73c2d919f0b4b60fe58331b51ba8dd6
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-01-31 08:04:35 +0200

    ux/client: renamed the {useToggleMeasureLayer} hook to {useSetMeasureLayer} and added handler for setting the layer visibility to a specific value

--- ux/client/views/viz/viz/useSetMeasureLayer.js
+++ ux/client/views/viz/viz/useSetMeasureLayer.js
@@ -15,12 +15,33 @@ import { useSetPixelPathSelection } from './useSetPixelPathSelection'
 
 
 // toggle the measure layer over this viewport
-export const useToggleMeasureLayer = viewport => {
+export const useSetMeasureLayer = viewport => {
     // grab the measure layer mutator from context
     const { setMeasureLayer } = React.useContext(Context)
     // make handlers that clear the pixel path and the selection
     const { clear: clearPixelPath } = useSetPixelPath(viewport)
     const { clear: clearPixelPathSelection } = useSetPixelPathSelection(viewport)
+
+    // a handler that ses the visibility of the measure layer
+    const set = value => {
+        // toggle
+        setMeasureLayer(old => {
+            // make a copy of the measure layer status table
+            const table = [...old]
+            // adjust the entry that corresponds to this viewport
+            table[viewport] = value
+            // and return the new table
+            return table
+        })
+
+        // on toggle, clear the pixel path
+        clearPixelPath()
+        // and the selection
+        clearPixelPathSelection()
+
+        // all done
+        return
+    }
 
     // a handler that toggles whether the measure layer is visible
     const toggle = () => {
@@ -43,8 +64,8 @@ export const useToggleMeasureLayer = viewport => {
         return
     }
 
-    // return the toggle
-    return toggle
+    // return the handlers
+    return { set, toggle }
 }
 
 
