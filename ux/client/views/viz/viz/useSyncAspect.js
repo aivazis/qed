ac30e8b1e8d5438342560bc6d8460393cf85b4c0
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-01-31 13:34:26 +0200

    ux/views: reimplemented the turning on of the measure layer by {useSyncAspect} directly through the context mutator

--- ux/client/views/viz/viz/useSyncAspect.js
+++ ux/client/views/viz/viz/useSyncAspect.js
@@ -11,31 +11,53 @@ import React from 'react'
 // context
 import { Context } from './context'
 // hooks
-import { useSetMeasureLayer } from './useSetMeasureLayer'
-
+import { useSetPixelPath } from './useSetPixelPath'
 
 // build a handler that toggles the specified entry in the sync table
 export const useToggleSyncedAspect = () => {
     // grab the sync table setter
-    const { setSynced } = React.useContext(Context)
-    // and the measure layer control
-    const { set } = useSetMeasureLayer()
+    const { setSynced, setMeasureLayer } = React.useContext(Context)
+    // and the handler that copies pixel paths
+    const { copy } = useSetPixelPath()
     // make a handler factory and return it
     return (viewport, aspect) => () => {
+        // the new value of the aspect
+        let value
+        // and a path synced viewport
+        let src
         // update the sync table
         setSynced(old => {
             // make a copy of the old state
             const clone = old.map(sync => ({ ...sync }))
             // flip the {aspect} of the viewport
             clone[viewport][aspect] = !old[viewport][aspect]
-            // if we are turning on path sync
-            if (aspect === "path" && clone[viewport][aspect] === true) {
-                // turn on the measure layer for this viewport
-                set(true, viewport)
-            }
+            // remember the new value
+            value = clone[viewport][aspect]
+            // find a viewport that is path synced; use {old} for the search so i don't bump
+            // into myself
+            src = old.findIndex(port => port.path)
             // return the new state
             return clone
         })
+        // if we are turning on path sync
+        if (aspect === "path" && value === true) {
+            // turn on the measure layer for this viewport
+            setMeasureLayer(old => {
+                // make a copy
+                const pile = [...old]
+                // adjust the entry for this viewport
+                pile[viewport] = true
+                // and return the bew pile
+                return pile
+            })
+            // if we have a path synced viewport
+            if (src >= 0) {
+                // copy its pixel path
+                copy(src, viewport)
+            }
+        }
+        // all done
+        return
     }
 }
 
@@ -43,24 +65,41 @@ export const useToggleSyncedAspect = () => {
 export const useSetSyncedAspect = () => {
     // grab the setter
     const { setSynced } = React.useContext(Context)
-    // and the measure layer control
-    const { set } = useSetMeasureLayer()
     // make a handler factory and return it
     return (viewport, aspect) => value => {
+        // a path synced viewport
+        let src
         // update the sync table
         setSynced(old => {
             // make a copy of the old state
             const clone = [...old]
             // flip the {aspect} of the viewport
             clone[viewport][aspect] = value
-            // if we are turning on path sync
-            if (aspect === "path" && value === true) {
-                // turn on the measure layer for this viewport
-                set(true, viewport)
-            }
+            // find a viewport that is path synced; use {old} for the search so i don't bump
+            // into myself
+            src = old.findIndex(port => port.path)
             // return the new state
             return clone
         })
+        // if we are turning on path sync
+        if (aspect === "path" && value === true) {
+            // turn on the measure layer for this viewport
+            setMeasureLayer(old => {
+                // make a copy
+                const pile = [...old]
+                // adjust the entry for this viewport
+                pile[viewport] = true
+                // and return the bew pile
+                return pile
+            })
+            // if we have a path synced viewport
+            if (src >= 0) {
+                // copy its pixel path
+                copy(src, viewport)
+            }
+        }
+        // all done
+        return
     }
 }
 
