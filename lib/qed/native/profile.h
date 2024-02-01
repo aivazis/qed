2b2ca13f19bbbbb7ddb14eab265f003bcf7dda51
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-01 10:53:51 +0200

    qed::readers: the {native} reader can now generate the {profile} of a closed path

--- lib/qed/native/profile.h
+++ lib/qed/native/profile.h
@@ -4,8 +4,7 @@
 // (c) 1998-2024 all rights reserved
 
 // code guard
-#if !defined(qed_native_profile_h)
-#define qed_native_profile_h
+#pragma once
 
 
 // decorators
@@ -13,12 +12,16 @@ namespace qed::native {
     // type aliases
     // display coordinates
     using point_t = std::tuple<int, int>;
+    // line segments
+    using segment_t = std::tuple<point_t, point_t>;
     // the resulting record that appends the values extracted from  the dataset
     template <class valueT>
     using value_t = std::tuple<int, int, valueT>;
 
-    // the set of points
+    // containers of points
     using points_t = std::vector<point_t>;
+    // containers of segments
+    using segments_t = std::vector<segment_t>;
     // the table of values
     template <typename valueT>
     using values_t = std::vector<value_t<valueT>>;
@@ -30,15 +33,14 @@ namespace qed::native {
         // the source
         const sourceT & source,
         // the points
-        const points_t &) -> values_t<typename sourceT::value_type>;
+        const points_t &,
+        // the closed flag
+        bool closed = false) -> values_t<typename sourceT::value_type>;
 }
 
 
 // pull in the implementations
-#define qed_native_profile_icc
 #include "profile.icc"
-#undef qed_native_profile_icc
 
-#endif
 
 // end of file
