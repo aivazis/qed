fcf3d4e05d787c94a754f0ec39bb28f2f23f6acc
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-02 10:47:34 +0200

    ux/views: activate the table headers in the {sync} control

--- ux/client/views/viz/controls/sync/header.js
+++ ux/client/views/viz/controls/sync/header.js
@@ -8,18 +8,45 @@
 import React from 'react'
 import styled from 'styled-components'
 
+// local
+// hooks
+import { useSyncAspect } from '../../viz/useSyncAspect'
+import { useViews } from '../../viz/useViews'
+
 
 // the sync control table header
 export const Header = () => {
+    // get the set of views
+    const { activeViewport } = useViews()
+    // get the sync handler factories
+    const { force } = useSyncAspect()
+    // a behavior factory
+    const behaviors = aspect => {
+        // build the handler
+        const update = force(activeViewport, aspect)
+        // build the table of behaviors and return it
+        return {
+            onClick: evt => {
+                // stop this event from bubbling up
+                evt.stopPropagation()
+                // and quash any side effects
+                evt.preventDefault()
+                // update the entire column based on the state of the active viewport
+                update()
+                // all done
+                return
+            }
+        }
+    }
     // render
     return (
         <Head>
             <Title>
                 <Column></Column>
-                <Column>channel</Column>
-                <Column>zoom</Column>
-                <Column>scroll</Column>
-                <Column>path</Column>
+                <ActiveColumn {...behaviors("channel")}>channel</ActiveColumn>
+                <ActiveColumn {...behaviors("zoom")}>zoom</ActiveColumn>
+                <ActiveColumn {...behaviors("scroll")}>scroll</ActiveColumn>
+                <ActiveColumn {...behaviors("path")}>path</ActiveColumn>
                 <Column>offsets</Column>
             </Title>
         </Head>
@@ -41,6 +68,16 @@ const Column = styled.th`
     vertical-align: middle;
     padding: 0.5em 0.0em ;
     cursor: default;
+`
+
+const ActiveColumn = styled(Column)`
+    & {
+        cursor: pointer;
+    }
+
+    &:hover {
+        color: hsl(28deg, 90%, 45%);
+    }
 `
 
 
