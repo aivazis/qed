cced821a75493944a5609f39c1f1b0e10370a559
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-06 21:47:16 +0200

    xu/views: the {info} viewport metadata display now tracks the cursor when at its new default level

--- ux/client/views/viz/viewer/info.js
+++ ux/client/views/viz/viewer/info.js
@@ -15,34 +15,86 @@ import { Meta } from '~/widgets'
 // hooks
 import { useGetTileURI } from '../viz/useGetTileURI'
 import { useGetZoomLevel } from '../viz/useGetZoomLevel'
+import { useViewports } from '../viz/useViewports'
 // styles
 import styles from './styles'
 
 
 // export the data viewer
 export const Info = ({ viewport, view }) => {
+    // make room for the cursor position
+    const [location, setLocation] = React.useState({ x: 0, y: 0 })
     // get the viewport zoom level
     const viewportZoom = useGetZoomLevel(viewport)
     // unpack
     const zoom = [viewportZoom.vertical, viewportZoom.horizontal]
     // assemble the data request URI
     const base = useGetTileURI({ viewport })
+    // get the viewport registry
+    const { viewports } = useViewports()
 
     // unpack the view
     const { reader, dataset } = view
     // extract the relevant metadata
     const { name: readerName, id, uri } = reader
     const { name: datasetName, datatype, shape, origin, tile } = dataset
-
     // scale the shape to the current zoom level
     const effectiveShape = shape.map((s, idx) => s / (2 ** -zoom[idx]))
+
+    // build the handler of the mouse movement
+    const track = evt => {
+        // get the viewport, not whatever the mouse is over
+        const element = evt.currentTarget
+        // measure it
+        const box = element.getBoundingClientRect()
+        // compute the location of the mouse relative to the viewport
+        // and take the zoom level into account
+        const x = Math.trunc(element.scrollLeft + evt.clientX - box.left)
+        const y = Math.trunc(element.scrollTop + evt.clientY - box.top)
+        // if the position overflows the dataset along the x-axis
+        if (x < 0 || x > effectiveShape[1]) {
+            // bail
+            return
+        }
+        // repeat for the y axis
+        if (y < 0 || y > effectiveShape[0]) {
+            // bail
+            return
+        }
+        // record the current mouse location; this triggers the {sample} query to refresh
+        setLocation({ x, y })
+        // all done
+        return
+    }
+
+    // install a tracker on the active viewport
+    React.useEffect(() => {
+        // get the active viewport ref
+        const target = viewports[viewport]
+        // install the tracker
+        target.addEventListener("mousemove", track)
+        // make an abort controller
+        const controller = new AbortController()
+        // and register a clean up
+        return () => {
+            // that removes the listeners
+            target.removeEventListener("mousemove", track)
+            // and aborts any pending requests
+            controller.abort()
+            // all done
+            return
+        }
+    })
 
     // mix my paint
     const paint = styles.viewer
     // and render
     return (
-        < Meta.Table min={0} initial={0} max={5} style={paint} >
-            <Meta.Entry threshold={1} attribute="uri" style={paint}>
+        < Meta.Table min={0} initial={1} max={5} style={paint} >
+            <Meta.Entry threshold={1} attribute="cursor" style={paint}>
+                {location && `(${location.y}, ${location.x})`}
+            </Meta.Entry>
+            <Meta.Entry threshold={2} attribute="uri" style={paint}>
                 {uri}
             </Meta.Entry>
             <Meta.Entry threshold={2} attribute="shape" style={paint}>
