f66a7dadffed1df239ea2f9e0dbe3cb2d7640872
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-13 07:27:00 -0800

    qed.readers: published the {auto} and {slc} readers from {isce2}

--- pkg/readers/isce2/__init__.py
+++ pkg/readers/isce2/__init__.py
@@ -5,12 +5,17 @@
 
 
 # publish the readers
+from .Auto import Auto as auto
+from .SLC import SLC as slc
 from .interferogram import int
-from .RSLC import RSLC as rslc
 from .unwrapped import unw
 
 # the metadata parser
 from .xml import parse as metadata
+
+# reader aliases
+rslc = slc
+gslc = slc
 
 
 # end of file
