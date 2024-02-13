432e299f55de4d153ca1f1b8fa791d055e891f83
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-13 07:26:32 -0800

    qed.readers: primed the {auto} reader for {isce2}

---
+++ pkg/readers/isce2/Auto.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# autodetect
+class Auto:
+    """
+    A reader that attempts to determine the correct reader
+    """
+
+    # metamethods
+    def __init__(self, **kwds):
+        # all done
+        return
+
+
+# end of file
