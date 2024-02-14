bd64c2b1356f91db1bbd55b79207d00a407156c3
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-13 19:25:21 -0800

    qed.readers: added {metadata} support to {nisar}

--- pkg/readers/nisar/__init__.py
+++ pkg/readers/nisar/__init__.py
@@ -4,6 +4,9 @@
 # (c) 1998-2024 all rights reserved
 
 
+# support
+import qed
+
 # publish the readers
 from .L0A import L0A as l0a
 
@@ -18,6 +21,23 @@ from .GSLC import GSLC as gslc
 from .GOFF import GOFF as goff
 from .GUNW import GUNW as gunw
 from .GCOV import GCOV as gcov
+
+
+# metadata factory
+def metadata(uri):
+    """
+    Build a metadata object
+    """
+    # instantiate the object
+    metadata = qed.readers.metadata()
+    # attach the uri
+    metadata.uri = uri
+    # open the file
+    data = qed.h5.read(uri=uri)
+    # set the product type
+    metadata.product = data.science.LSAR.identification.productType.lower()
+    # that's all we know, for now
+    return metadata
 
 
 # end of file
