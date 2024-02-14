d071e51e5fb397ddded81c59206046f19b418917
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-13 18:55:02 -0800

    qed.readers: the {isce2} auxiliary file parser now uses the common {metadata}

--- pkg/readers/isce2/xml/__init__.py
+++ pkg/readers/isce2/xml/__init__.py
@@ -4,12 +4,10 @@
 # (c) 1998-2024 all rights reserved
 
 # externals
-import pyre
-import pyre.xml
+import qed
 
 # document parts
 from .Document import Document as document
-from .Metadata import Metadata
 
 
 # parser
@@ -18,11 +16,11 @@ def parse(uri):
     Parse the metadata for a data product and return its configuration
     """
     # build the metadata object
-    metadata = Metadata()
+    metadata = qed.readers.metadata()
     # cast and attach the dataset uri
     metadata.uri = uri
     # form the file name; no S3 support here
-    data = pyre.primitives.path(metadata.uri.address)
+    data = qed.primitives.path(metadata.uri.address)
     # get its size and attach it
     metadata.bytes = data.stat().st_size
     # grab its suffix, and skip the dot
@@ -40,7 +38,7 @@ def parse(uri):
     # otherwise, open the companion file
     stream = open(aux, mode="r")
     # make an XML parser
-    parser = pyre.xml.newReader()
+    parser = qed.xml.newReader()
     # parse and return the result
     return parser.read(stream=stream, document=document(metadata=metadata))
 
