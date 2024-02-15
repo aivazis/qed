b5f1105b3f565225c5bed5e922ddc7e11c7261bc
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-15 07:35:09 -0800

    qed::api: added bindings for {factor}

---
+++ ext/qed/api.cc
@@ -0,0 +1,38 @@
+// -*- c++ -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// externals
+#include "external.h"
+// namespace setup
+#include "forward.h"
+
+// the qed library
+#include <qed/api.h>
+
+
+// access to the version tags of the library and the bindings
+void
+qed::py::api(py::module & m)
+{
+    // generator of guesses of raster shapes
+    m.def(
+        // the name
+        "factor",
+        // the implementation
+        &qed::api::factor,
+        // the signature
+        "product"_a, "aspect"_a,
+        // the docstring
+        "generate pairs of numbers with a given {product} whose {aspect} ratio does not exceed "
+        "a given value");
+
+
+    // all done
+    return;
+}
+
+
+// end of file
