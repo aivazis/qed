8e91be68f3cebb1ffce2ecd8cb3e910ce134317d
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-28 00:35:10 -0800

    qed.readers: the {unw} reader in {isce2} now attempts to deduce the dataset shape

--- pkg/readers/isce2/unwrapped/Reader.py
+++ pkg/readers/isce2/unwrapped/Reader.py
@@ -5,7 +5,12 @@
 
 
 # support
+import math
 import qed
+import journal
+
+# metadata parser
+from .. import xml
 
 # dataset
 from .Dataset import Dataset
@@ -71,6 +76,65 @@ class Reader(
         # add the dataset to the pile
         self.datasets.append(dataset)
 
+        # all done
+        return
+
+    # framework hooks
+    def pyre_configured(self):
+        """
+        Hook invoked after the reader configuration is complete
+        """
+        # get the current value of my shape
+        shape = list(self.shape) if self.shape else [0, 0]
+        # if it's trivial
+        if not shape or shape[0] == 0 or shape[1] == 0:
+            # look for an auxiliary file with metadata and extract the available information
+            metadata = xml.metadata(self.uri)
+            # if it knows the number of lines
+            if metadata.height:
+                # set it
+                shape[0] = metadata.height
+            # if it knows the number of samples
+            if metadata.width:
+                # set it
+                shape[1] = metadata.width
+            # if the width is missing but we know the height
+            if shape[1] == 0 and shape[0]:
+                # set it from the file size
+                shape[1] = metadata.bytes / shape[0] / (2 * self.cell.bytes)
+            # if the height is missing but we know the width
+            if shape[0] == 0 and shape[1]:
+                # set it from the file size
+                shape[0] = metadata.bytes / shape[1] / (2 * self.cell.bytes)
+            # if the shape is now fully resolved
+            if (
+                shape[0]
+                and shape[1]
+                and shape[0] == math.floor(shape[0])
+                and shape[1] == math.floor(shape[1])
+            ):
+                # set it
+                self.shape = tuple(shape)
+            # if not
+            else:
+                # make a channel
+                channel = journal.warning("qed.readers.unw")
+                # generate a list of possible shapes
+                channel.line(f"while attempting to load '{self.uri.address}'")
+                channel.line(f"missing shape information; here are some possibilities")
+                channel.indent()
+                # generate some options
+                for lines, samples in qed.libqed.factor(
+                    product=metadata.bytes // (2 * self.cell.bytes), aspect=10
+                ):
+                    # and show them
+                    channel.line(f"{lines} x {samples}")
+                channel.outdent()
+                # flush
+                channel.log()
+
+        # chain up
+        yield from super().pyre_configured()
         # all done
         return
 
