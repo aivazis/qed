4b1367f93585b20d1a93b49402ead003be0ff83d
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-02-27 23:56:00 -0800

    qed.readers: the {slc} reader in {isce2} now attempts to extract missing configuration information from the companion xml file, if available

--- pkg/readers/isce2/SLC.py
+++ pkg/readers/isce2/SLC.py
@@ -7,6 +7,9 @@
 # support
 import qed
 
+# access to the metadata parser
+from . import xml
+
 # superclass
 from .. import native
 
@@ -21,6 +24,34 @@ class SLC(native.flat, family="qed.readers.isce2.slc"):
     cell = qed.protocols.datatype()
     cell.default = "complex64"
     cell.doc = "the type of the dataset payload"
+
+    # framework hooks
+    def pyre_configured(self):
+        """
+        Hook invoked after the reader configuration is complete
+        """
+        # get the current value of my shape
+        shape = list(self.shape) if self.shape else [0, 0]
+        # if it's trivial
+        if not shape or shape[0] == 0 or shape[1] == 0:
+            # look for an auxiliary file with metadata and extract the available information
+            metadata = xml.metadata(self.uri)
+            # if it knows the number of lines
+            if metadata.height:
+                # set it
+                shape[0] = metadata.height
+            # if it knows the number of samples
+            if metadata.width:
+                # set it
+                shape[1] = metadata.width
+            # if the shape is now fully resolved
+            if shape[0] and shape[1]:
+                # set it
+                self.shape = tuple(shape)
+        # and chain up
+        yield from super().pyre_configured()
+        # all done
+        return
 
 
 # end of file
