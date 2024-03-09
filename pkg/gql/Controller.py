d205b65da9d9cfa588a4fdc1cd01ea4369f08a9a
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-08 20:31:37 -0800

    qed.gql: cosmetics in {controller}

--- pkg/gql/Controller.py
+++ pkg/gql/Controller.py
@@ -6,6 +6,7 @@
 
 # externals
 import graphene
+
 # base types
 from .RangeController import RangeController
 from .ValueController import ValueController
@@ -17,24 +18,21 @@ class Controller(graphene.Union):
     The union of all controller types
     """
 
-
     # metadata
     class Meta:
         # my possible types
         types = RangeController, ValueController
 
-
     # the type resolver
     @classmethod
     def resolve_type(cls, context, info):
-       """
-       Deduce the controller type from the {instance} information
-       """
-       # extract the type id
-       tag = context["controller"].tag
-       # look up the associated class in my registryand return it
-       return cls.registry[tag]
-
+        """
+        Deduce the controller type from the {instance} information
+        """
+        # extract the type id
+        tag = context["controller"].tag
+        # look up the associated class in my registry and return it
+        return cls.registry[tag]
 
     # the resolver table
     registry = {
