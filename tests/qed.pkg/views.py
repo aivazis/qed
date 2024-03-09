94d9bfaa92e0024fc7668851cdf0b80e1bba91cb
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-09 14:50:08 -0800

    qed: added a driver for testing the configuration of visualization pipelines

---
+++ tests/qed.pkg/views.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+"""
+Check that we can load view persistent state
+"""
+
+
+# support
+import qed
+
+# load the app
+app = qed.shells.qed(name="qed.app")
+# build its dispatcher
+ux = qed.ux.dispatcher(plexus=app, docroot=qed.filesystem.local(root="."), pfs=app.pfs)
+
+
+# end of file
