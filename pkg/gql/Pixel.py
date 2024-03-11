76d3796427951077285d229247dff17430a06799
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-10 21:54:06 -0700

    qed.pkg: added {view} and its parts to {channel}

---
+++ pkg/gql/Pixel.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# externals
+import graphene
+
+
+# a pixel
+class Pixel(graphene.ObjectType):
+    """
+    A pixel
+    """
+
+    # the fields
+    x = graphene.Int(required=True)
+    y = graphene.Int(required=True)
+
+    # the resolvers
+    @staticmethod
+    def resolve_x(pixel: tuple, *_):
+        """
+        Grab the x coordinate
+        """
+        print(pixel)
+        # easy enough
+        return pixel[1]
+
+    @staticmethod
+    def resolve_y(pixel: tuple, *_):
+        """
+        Grab the y coordinate
+        """
+        # easy enough
+        return pixel[0]
+
+
+# end of file
