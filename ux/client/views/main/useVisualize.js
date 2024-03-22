0952dec1c15298de2b1ac053529f6825934558e0
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-22 16:01:00 -0700

    ux/views: the {useVisualize} hook of {main} now mutates the server side store to update the contents of the {view} in the {activeViewport}

--- ux/client/views/main/useVisualize.js
+++ ux/client/views/main/useVisualize.js
@@ -5,44 +5,64 @@
 
 
 // externals
-import React from 'react'
+import { graphql, useMutation } from 'react-relay/hooks'
 
 // local
-// context
-import { Context } from './context'
+// hooks
+import { useViewports } from './useViewports'
 
 
 // hook that adjusts the contents of a given viewport
 // currently, only called by {reader} instances when selected/updated
 export const useVisualize = () => {
     // grab the active viewport index and the {views} mutator
-    const { activeViewport, setViews, synced } = React.useContext(Context)
+    const { activeViewport } = useViewports()
+    // placing a view in a viewport mutates the server side application store
+    const [request, pending] = useMutation(visualizeMutation)
 
     // make the handler
     const visualize = (view, viewport = activeViewport) => {
-        // unpack the view
-        const { channel } = view
-        // adjust the view
-        setViews(old => {
-            // make a copy of the old state
-            const clone = [...old]
-            // adjust the entry specified by the caller
-            clone[viewport] = { ...old[viewport], ...view }
-            // if i'm channel synced
-            if (synced[viewport].channel) {
-                // go through the sync table
-                synced.forEach((sync, port) => {
-                    // if this port is channel synced and it understands this channel
-                    if (sync.channel && clone[port].dataset.channels.includes(channel)) {
-                        // adjust the channel
-                        clone[port].channel = channel
-                    }
-                    // all done
+        // show me
+        console.log(`main.useVisualize: setting view in ${viewport}:`, view)
+        // if there is already a pending operation
+        if (pending) {
+            // nothing to do
+            return
+        }
+        // otherwise, place the, possible partial, view information in the server side store
+        request({
+            // input
+            variables: {
+                // the payload
+                viewport: viewport,
+                reader: view?.reader?.uri || null,
+                dataset: view?.dataset?.name || null,
+                channel: view?.channel?.tag || null,
+            },
+            // update the store
+            updater: store => {
+                // get the root field of the mutation result
+                const response = store.getRootField("updateView")
+                // ask for the list of views
+                const updated = response.getLinkedRecords("views")
+                //if it's trivial
+                if (updated == null) {
+                    // something went wrong at the server; bail, for now
                     return
-                })
+                }
+                // if all is good, get the remote store
+                const qed = store.get("QED")
+                // attach the updated pile
+                qed.setLinkedRecords(updated, "views")
+                // all done
+                return
+            },
+            // if something goes wrong
+            onError: error => {
+                // show me
+                console.log(`main.useVisualize: ERROR:`, error)
             }
-            // and hand off the new state
-            return clone
+
         })
         // all done
         return
@@ -52,5 +72,227 @@ export const useVisualize = () => {
     return visualize
 }
 
+
+// the mutation that places a view in a viewport
+const visualizeMutation = graphql`
+    mutation useVisualizeMutation(
+        $viewport: Int!, $reader: String, $dataset: String, $channel: String
+    ) {
+        updateView(reader: $reader, dataset: $dataset, channel: $channel, viewport: $viewport) {
+            views {
+                id
+                name
+                reader {
+                    id
+                    name
+                    uri
+                    api
+                    selectors {
+                        name
+                        values
+                    }
+                    datasets {
+                        id
+                        name
+                        datatype
+                        selector {
+                            name
+                            value
+                        }
+                        shape
+                        origin
+                        tile
+                        channels {
+                            id
+                            name
+                            tag
+                            controllers {
+                                __typename
+                                ... on Node {
+                                    id
+                                }
+                                ... on RangeController {
+                                    slot
+                                    min
+                                    max
+                                    low
+                                    high
+                                }
+                                ... on ValueController {
+                                    id
+                                    slot
+                                    min
+                                    max
+                                    value
+                                }
+                            }
+                            view {
+                                id
+                                name
+                                measure {
+                                    id
+                                    name
+                                    active
+                                    path {
+                                        x
+                                        y
+                                    }
+                                    closed
+                                    selection
+                                }
+                                sync {
+                                    id
+                                    name
+                                    channel
+                                    zoom
+                                    scroll
+                                    path
+                                    offsets {
+                                        x
+                                        y
+                                    }
+                                }
+                                zoom {
+                                    id
+                                    name
+                                    horizontal
+                                    vertical
+                                    coupled
+                                }
+                            }
+                        }
+                    }
+                }
+                dataset {
+                    id
+                    name
+                    datatype
+                    selector {
+                        name
+                        value
+                    }
+                    channels {
+                        id
+                        name
+                        tag
+                        controllers {
+                            __typename
+                            ... on Node {
+                                id
+                            }
+                            ... on RangeController {
+                                slot
+                                min
+                                max
+                                low
+                                high
+                            }
+                            ... on ValueController {
+                                id
+                                slot
+                                min
+                                max
+                                value
+                            }
+                        }
+                        view {
+                            id
+                            name
+                            measure {
+                                id
+                                name
+                                active
+                                path {
+                                    x
+                                    y
+                                }
+                                closed
+                                selection
+                            }
+                            sync {
+                                id
+                                name
+                                channel
+                                zoom
+                                scroll
+                                path
+                                offsets {
+                                    x
+                                    y
+                                }
+                            }
+                            zoom {
+                                id
+                                name
+                                horizontal
+                                vertical
+                                coupled
+                            }
+                        }
+                    }
+                }
+                channel {
+                    id
+                    name
+                    tag
+                    controllers {
+                        __typename
+                        ... on Node {
+                            id
+                        }
+                        ... on RangeController {
+                            slot
+                            min
+                            max
+                            low
+                            high
+                        }
+                        ... on ValueController {
+                            id
+                            slot
+                            min
+                            max
+                            value
+                        }
+                    }
+                    view {
+                        id
+                        name
+                        measure {
+                            id
+                            name
+                            active
+                            path {
+                                x
+                                y
+                            }
+                            closed
+                            selection
+                        }
+                        sync {
+                            id
+                            name
+                            channel
+                            zoom
+                            scroll
+                            path
+                            offsets {
+                                x
+                                y
+                            }
+                        }
+                        zoom {
+                            id
+                            name
+                            horizontal
+                            vertical
+                            coupled
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
 
 // end of file
