982470ec6acd9486341e9564e9d8d444c484e50a
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-21 20:49:46 -0700

    qed.protocols: added {view}

---
+++ pkg/protocols/View.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# support
+import qed
+
+
+# the protocol
+class View(qed.protocol, family="qed.views"):
+    """
+    Detailed information about the contents of a mounted view
+    """
+
+
+# end of file
