11fb247841fd1dd5d4c3fbc8244246091ac4cb84
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-24 00:33:34 -0700

    ux/views: added the {useSelectReader} hook that builds a handler that sends the active reader selection to the server side {store}

---
+++ ux/client/views/viz/reader/useSelectReader.js
@@ -0,0 +1,93 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// externals
+import { graphql, useMutation } from 'react-relay/hooks'
+
+// project
+// hooks
+import { useViewports } from '../../main'
+
+// local
+// hooks
+import { useReader } from './useReader'
+
+
+// select the reader that's active in a viewport
+export const useSelectReader = () => {
+    // get the active viewport
+    const { activeViewport } = useViewports()
+    // get my reader
+    const reader = useReader()
+    // selecting a reader mutates the server side store
+    const [commit, pending] = useMutation(selectReaderMutation)
+
+    // make the handler
+    const select = (viewport = activeViewport) => {
+        // if there is already a pending operation
+        if (pending) {
+            // nothing to do
+            return
+        }
+        // otherwise, send the mutation to the server
+        commit({
+            //input
+            variables: {
+                // the payload
+                viewport,
+                reader: reader.name
+            },
+            // update the store
+            updater: store => {
+                // get the root field of the mutation result
+                const response = store.getRootField("viewSelectReader")
+                // ask for the list of views
+                const view = response.getLinkedRecord("view")
+                // if it's trivial
+                if (view === null) {
+                    // something went wrong at the server; not much more to do
+                    return
+                }
+                // get the remote store
+                const qed = store.get("QED")
+                // get the current set of views
+                const views = qed.getLinkedRecords("views")
+                // replace the view at {viewport} with the new one
+                qed.setLinkedRecords(views.toSpliced(viewport, 1, view), "views")
+                // all done
+                return
+            },
+            onError: error => {
+                // show me
+                console.log(
+                    `viz.reader.useSelectReader: ERROR while selecting '${reader.name}':`,
+                    error
+                )
+            }
+        })
+        // all done
+        return
+    }
+
+    // all done
+    return select
+}
+
+// the mutation that selects a reader
+const selectReaderMutation = graphql`
+    mutation useSelectReaderMutation($viewport: Int!, $reader: String!) {
+        viewSelectReader(viewport: $viewport, reader: $reader) {
+            view {
+                id
+                # and get whatever readers need
+                ...contextGetViewFragment
+            }
+        }
+    }
+`
+
+
+// end of file
