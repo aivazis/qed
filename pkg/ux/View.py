33f93f10f942210b6665da3984787ffa130c34d2
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-24 22:45:44 -0700

    qed.ux: the {view} can now solve for the matching {dataset} when given enough {selections}

--- pkg/ux/View.py
+++ pkg/ux/View.py
@@ -17,24 +17,121 @@ class View(qed.component, family="qed.views.view", implements=qed.protocols.view
 
     # state
     reader = qed.protocols.reader()
+    reader.default = None
     reader.doc = "the reader whose dataset is being displayed"
 
     dataset = qed.protocols.dataset()
+    dataset.default = None
     dataset.doc = "the displayed dataset"
 
     channel = qed.protocols.channel()
+    channel.default = None
     channel.doc = "the displayed channel"
 
     selections = qed.properties.kv()
     selections.default = {}
-    selections.doc = "a key/value map with the user selections"
+    selections.doc = "an (axis -> selected value) map with the user selections"
 
-    def pyre_dump(self):
+    # interface
+    def resolve(self):
+        """
+        Attempt to resolve the view given the current state of its selections
+        """
+        # get my reader
+        reader = self.reader
+        # if i don't have one
+        if not reader:
+            # there is no solution
+            return self
+        # get the reader selectors
+        selectors = reader.selectors
+        # get my selections
+        selections = self.selections
+        # if we don't have have enough selections
+        if len(selections) != len(selectors):
+            # clear the dataset
+            self.dataset = None
+            # nothing else to do
+            return self
+        # if we do have enough selections, there must be a dataset that matches
+        # go through them
+        for dataset in reader.datasets:
+            # if its selectors match my selections
+            if dataset.selector == selections:
+                # it's the one
+                self.dataset = dataset
+                # go no further
+                break
+        # if we didn't find a match
+        else:
+            # there is no solution
+            self.dataset = None
+            # it could be a bug, but it's also possible that the reader has no datasets
+            if reader.datasets:
+                # it's a bug
+                firewall = journal.firewall("qed.ux.store")
+                # complain
+                firewall.line(f"could not solve for the dataset")
+                firewall.line(f"in {self}")
+                firewall.line(f"with {reader}")
+                firewall.line(f"given the following selections:")
+                firewall.indent()
+                firewall.report(
+                    report=(f"{key}: {value}" for key, value in selections.items())
+                )
+                firewall.outdent()
+                # flush
+                firewall.log()
+                # and bail, just in case firewalls aren't fatal
+                return self
+        # all done
+        return self
+
+    # framework hooks
+    def pyre_configured(self):
+        """
+        Validate my configuration
+        """
+        # get my reader
+        reader = self.reader
+        # if i don't have one
+        if not reader:
+            # bail with no errors
+            return []
+        # get the reader datasets
+        datasets = reader.datasets
+        # if there is only one
+        if len(datasets) == 1:
+            # grab it
+            dataset, *_ = datasets
+            # select it
+            self.dataset = dataset
+            # if it only has one channel
+            if len(dataset.channels) == 1:
+                # grab it
+                channel, *_ = dataset.channels.values()
+                # and select it
+                self.channel = channel
+        # get my selection
+        selections = self.selections
+        # so we can prime them
+        for axis, coordinates in reader.available.items():
+            # if there is only one option for this axis
+            if len(coordinates) == 1:
+                # get it
+                coordinate, *_ = coordinates
+                # and select it
+                selections[axis] = coordinate
+        # report success
+        return []
+
+    # debugging support
+    def pyre_dump(self, channel=None):
         """
         Report my state
         """
         # make a channel
-        channel = journal.info("qed.ux.store")
+        channel = channel or journal.info("qed.ux.store")
         # sign in
         channel.line(f"{self}")
         # report
@@ -42,9 +139,9 @@ class View(qed.component, family="qed.views.view", implements=qed.protocols.view
         channel.line(f"reader: {self.reader}")
         channel.line(f"dataset: {self.dataset}")
         channel.line(f"channel: {self.channel}")
-        channel.line(f"selections:")
+        channel.line(f"dataset selections:")
         channel.indent()
-        for key, value in self.selections:
+        for key, value in self.selections.items():
             channel.line(f"{key}: {value}")
         channel.outdent()
         channel.outdent()
