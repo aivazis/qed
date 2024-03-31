a5bb1346c8291aa2c7b370afcd5176b3312b030c
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-30 20:44:55 -0700

    qed.protocols: {ux} now has {view} and {viewport}

---
+++ pkg/protocols/ux/Viewport.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# support
+import qed
+
+
+# the protocol
+class Viewport(qed.protocol, family="qed.viewports"):
+    """
+    Detailed information about the contents of a viewport
+    """
+
+
+# end of file
