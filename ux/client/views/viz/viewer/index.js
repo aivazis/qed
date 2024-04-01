7b9c22c34efb4db1108ee9ac71db83e4dcfc8d2c
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-03-31 23:20:19 -0700

    ux/views: the {tileURI} factory now lives in {viewer}

--- ux/client/views/viz/viewer/index.js
+++ ux/client/views/viz/viewer/index.js
@@ -7,5 +7,17 @@
 // export the view
 export { Viewer } from './viewer'
 
+// tile URIs
+export const tileURI = ({ reader, dataset, channel, zoom }) => {
+    // unpack the zoom level
+    const level = [-zoom.vertical, -zoom.horizontal]
+    // normalize and build the tag
+    const tag = level.map(z => Math.trunc(Math.max(z, 0))).join("x")
+    // assemble the uri
+    const uri = [reader.api, dataset.name, channel.tag, tag].join("/")
+    // and return it
+    return uri
+}
+
 
 // end of file
