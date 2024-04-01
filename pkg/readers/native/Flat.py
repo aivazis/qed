f9249818fa9600cb9bf1c099324f7a2a0ffc0df3
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-01 10:05:19 -0700

    qed.readers: repaired the declaration of {selections} for {native} and {isce2} readers

--- pkg/readers/native/Flat.py
+++ pkg/readers/native/Flat.py
@@ -34,7 +34,7 @@ class Flat(
     selectors.doc = "a map of selector names to their allowed values"
 
     selections = qed.properties.kv()
-    selections = "a key value store of preferred values for selectors"
+    selections.doc = "a key value store of preferred values for selectors"
 
     datasets = qed.properties.list(schema=qed.protocols.dataset.output())
     datasets.doc = "the list of data sets provided by the reader"
