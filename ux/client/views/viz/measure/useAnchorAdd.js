582aaee68a2d26196b7f37c5f0812f50dd8c3a57
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-01 16:01:37 -0700

    ux/views: added the {useAnchorAdd} hook that triggers the {viewAnchorAdd} mutation

---
+++ ux/client/views/viz/measure/useAnchorAdd.js
@@ -0,0 +1,70 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// externals
+import { graphql, useMutation } from 'react-relay/hooks'
+
+// add a new anchor to the path
+export const useAnchorAdd = viewport => {
+    // adding an anchor to the path mutates the server side store
+    const [commit, pending] = useMutation(useAnchorAddMutation)
+
+    // make the handler
+    const add = (anchor, index = null) => {
+        // if there is already a pending operation
+        if (pending) {
+            // nothing to do
+            return
+        }
+        // otherwise, send the request to the server
+        commit({
+            // input
+            variables: {
+                // the payload
+                // the viewport
+                viewport,
+                // the anchor coordinates
+                ...anchor,
+                // the index
+                index,
+            },
+            onError: errors => {
+                // show me
+                console.log(`viz.measure.useAnchorAdd:`)
+                console.group()
+                console.log(`ERROR while adding an anchor to viewport ${viewport}`)
+                console.log(errors)
+                console.groupEnd()
+                // all done
+                return
+            }
+        })
+        // all done
+        return
+    }
+
+    // return the handler
+    return { add }
+}
+
+
+// the mutation that adds an anchor to the path
+const useAnchorAddMutation = graphql`
+    mutation useAnchorAddMutation($viewport: Int!, $x: Int!, $y: Int!, $index: Int) {
+        viewAnchorAdd(viewport: $viewport, x: $x, y: $y, index: $index) {
+            measure {
+                id
+                path {
+                    x
+                    y
+                }
+            }
+        }
+    }
+`
+
+
+// end of file
