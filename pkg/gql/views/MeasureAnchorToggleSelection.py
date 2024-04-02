cd53a9b7eb05f46994265cb09ff4254794e725cb
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-01 20:45:25 -0700

    qed.gql: primed support for the various anchor selection modes: extend, toggle, and toggle in multinode mode

---
+++ pkg/gql/views/MeasureAnchorToggleSelection.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# externals
+import graphene
+import journal
+
+
+# response types
+from .Measure import Measure
+
+
+# toggle the current anchor selection in single node mode
+class MeasureAnchorToggleSelection(graphene.Mutation):
+    """
+    Toggle the anchor selection in single node mode
+    """
+
+    # inputs
+    class Arguments:
+        # the viewport
+        viewport = graphene.Int(required=True)
+        index = graphene.Int(required=False)
+
+    # the result is the updated view
+    measure = graphene.Field(Measure)
+
+    # the mutator
+    @staticmethod
+    def mutate(root, info, viewport, index):
+        """
+        Toggle the anchor selection in single node mode
+        """
+        # get the store
+        store = info.context["store"]
+        # ask it to toggle the selection
+        measure = store.measureAnchorToggleSelection(viewport=viewport, index=index)
+        # form the mutation resolution context
+        context = {"measure": measure}
+        # and resolve the mutation
+        return context
+
+
+# end of file
---
+++ pkg/gql/views/MeasureAnchorToggleSelectionMulti.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# externals
+import graphene
+import journal
+
+
+# response types
+from .Measure import Measure
+
+
+# toggle the anchor selection in multinode mode
+class MeasureAnchorToggleSelectionMulti(graphene.Mutation):
+    """
+    Toggle the anchor selection in multinode mode
+    """
+
+    # inputs
+    class Arguments:
+        # the viewport
+        viewport = graphene.Int(required=True)
+        index = graphene.Int(required=False)
+
+    # the result is the updated view
+    measure = graphene.Field(Measure)
+
+    # the mutator
+    @staticmethod
+    def mutate(root, info, viewport, index):
+        """
+        Toggle the anchor selection in multinode mode
+        """
+        # get the store
+        store = info.context["store"]
+        # ask it to toggle the anchor selection
+        measure = store.measureAnchorToggleSelectionMulti(
+            viewport=viewport, index=index
+        )
+        # form the mutation resolution context
+        context = {"measure": measure}
+        # and resolve the mutation
+        return context
+
+
+# end of file
