d933a99d9a12682ada7e44dc87be8e711fa52452
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-01 20:46:36 -0700

    ux/views: primed support for the various anchor selection modes in {measure}: extend, toggle, and toggle in multinode mode

---
+++ ux/client/views/viz/measure/useAnchorExtendSelection.js
@@ -0,0 +1,65 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// externals
+import { graphql, useMutation } from 'react-relay/hooks'
+
+// extend the anchor selection to include a specific index
+export const useAnchorExtendSelection = viewport => {
+    // extending the anchor selection mutates the server side store
+    const [commit, pending] = useMutation(useAnchorExtendSelectionMutation)
+
+    // make the handler
+    const select = index => {
+        // if there is already a pending operation
+        if (pending) {
+            // nothing to do
+            return
+        }
+        // otherwise, send the request to the server
+        commit({
+            // input
+            variables: {
+                // the payload
+                // the viewport
+                viewport,
+                // the anchor index
+                index,
+            },
+            onError: errors => {
+                // show me
+                console.log(`viz.measure.useAnchorExtendSelection:`)
+                console.group()
+                console.log(`ERROR while modifying the anchor selection in viewport ${viewport}`)
+                console.log(errors)
+                console.groupEnd()
+                // all done
+                return
+            }
+        })
+        // all done
+        return
+    }
+
+    // return the handler
+    return { select }
+}
+
+
+// the mutation that adds an anchor to the path
+const useAnchorExtendSelectionMutation = graphql`
+    mutation useAnchorExtendSelectionMutation($viewport: Int!, $index: Int!) {
+        viewMeasureAnchorExtendSelection(viewport: $viewport, index: $index) {
+            measure {
+                id
+                selection
+            }
+        }
+    }
+`
+
+
+// end of file
---
+++ ux/client/views/viz/measure/useAnchorToggleSelectionMulti.js
@@ -0,0 +1,65 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// externals
+import { graphql, useMutation } from 'react-relay/hooks'
+
+// toggle the anchor selection in multinode mode
+export const useAnchorToggleSelectionMulti = viewport => {
+    // toggling the anchor selection mutates the server side store
+    const [commit, pending] = useMutation(useAnchorToggleSelectionMultiMutation)
+
+    // make the handler
+    const toggle = index => {
+        // if there is already a pending operation
+        if (pending) {
+            // nothing to do
+            return
+        }
+        // otherwise, send the request to the server
+        commit({
+            // input
+            variables: {
+                // the payload
+                // the viewport
+                viewport,
+                // the anchor index
+                index,
+            },
+            onError: errors => {
+                // show me
+                console.log(`viz.measure.useAnchorToggleSelectionMulti:`)
+                console.group()
+                console.log(`ERROR while modifying the anchor selection in viewport ${viewport}`)
+                console.log(errors)
+                console.groupEnd()
+                // all done
+                return
+            }
+        })
+        // all done
+        return
+    }
+
+    // return the handler
+    return { toggle }
+}
+
+
+// the mutation that adds an anchor to the path
+const useAnchorToggleSelectionMultiMutation = graphql`
+    mutation useAnchorToggleSelectionMultiMutation($viewport: Int!, $index: Int!) {
+        viewMeasureAnchorToggleSelectionMulti(viewport: $viewport, index: $index) {
+            measure {
+                id
+                selection
+            }
+        }
+    }
+`
+
+
+// end of file
