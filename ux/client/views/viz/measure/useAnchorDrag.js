1e6ad4fa71ed8b14e82aa5cd164aff16be5e86f6
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-01 22:22:28 -0700

    ux/views: added the {useAnchorDrag} hook to the {measure} layer to help with anchor movement

---
+++ ux/client/views/viz/measure/useAnchorDrag.js
@@ -0,0 +1,39 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// externals
+import React from 'react'
+
+// local
+// context
+import { Context } from './context'
+
+
+// access to the moving indicator
+export const useAnchorDrag = () => {
+    // get the flag
+    const { dragging, setDragging } = React.useContext(Context)
+
+    // start dragging an anchor
+    const start = anchor => {
+        // set the flag
+        setDragging(anchor)
+    }
+
+    // make a handler that clears the movement indicator
+    const stop = () => {
+        // clear the flag
+        setDragging(null)
+        // all done
+        return
+    }
+
+    // all done
+    return { start, dragging, stop }
+}
+
+
+// end of file
