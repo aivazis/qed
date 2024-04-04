aa812ac8e72c46ae241d3148bc8a245b61637287
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-04 16:54:33 -0700

    ux/views: {measure} now provides mutators for the state managed by the server side store

---
+++ ux/client/views/viz/measure/useAnchorSplit.js
@@ -0,0 +1,69 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// externals
+import { graphql, useMutation } from 'react-relay/hooks'
+
+// delete an existing anchor
+export const useAnchorSplit = viewport => {
+    // removing an anchor mutates the server side store
+    const [commit, pending] = useMutation(useAnchorSplitMutation)
+
+    // make the handler
+    const split = ({ anchor }) => {
+        // if there is already a pending operation
+        if (pending) {
+            // nothing to do
+            return
+        }
+        // otherwise, send the request to the server
+        commit({
+            // input
+            variables: {
+                // the payload
+                // the viewport
+                viewport,
+                // the node being removed
+                anchor,
+            },
+            onError: errors => {
+                // show me
+                console.log(`viz.measure.useAnchorSplit:`)
+                console.group()
+                console.log(`ERROR while removing anchor ${anchor} in viewport ${viewport}`)
+                console.log(errors)
+                console.groupEnd()
+                // all done
+                return
+            }
+        })
+        // all done
+        return
+    }
+
+    // return the handler
+    return { split }
+}
+
+
+// the mutation that adds an anchor to the path
+const useAnchorSplitMutation = graphql`
+    mutation useAnchorSplitMutation($viewport: Int!, $anchor:  Int!) {
+        viewMeasureAnchorSplit(viewport: $viewport, anchor: $anchor) {
+            measure {
+                id
+                path {
+                    x
+                    y
+                }
+                selection
+            }
+        }
+    }
+`
+
+
+// end of file
