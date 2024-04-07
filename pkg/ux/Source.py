a5a136ebd59b0ae5b602649b48c330cf575de34a
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-06 20:29:42 -0700

    qed.ux: the {dataset} view configuration now belongs to the data {source}

--- pkg/ux/Source.py
+++ pkg/ux/Source.py
@@ -7,12 +7,9 @@
 # support
 import qed
 
-# helpers
-from .Harvester import Harvester
-
 
 # the dataset ux state
-class Dataset(qed.component, family="qed.ux.datasets.dataset"):
+class Source(qed.component, family="qed.ux.sources.source"):
     """
     The state of a dataset view
     """
@@ -26,21 +23,6 @@ class Dataset(qed.component, family="qed.ux.datasets.dataset"):
 
     zoom = qed.protocols.ux.zoom()
     zoom.doc = "the zoom level"
-
-    # metamethods
-    def __init__(self, **kwds):
-        # chain up
-        super().__init__(**kwds)
-
-        # remember my configuration so we can restore it on demand
-        self.viewConfiguration = self.harvester.harvest(component=self)
-
-        # all done
-        return
-
-    # singleton
-    # the state harvester
-    harvester = Harvester()
 
 
 # end of file
