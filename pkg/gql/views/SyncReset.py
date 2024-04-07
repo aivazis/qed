43bb967ad2f67f2a542dadea22e147ad2449fb59
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-06 21:24:07 -0700

    qed.gql: added mutations that reset the {measure}, {sync}, and {zoom} state

---
+++ pkg/gql/views/SyncReset.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# externals
+import graphene
+import journal
+
+
+# response types
+from .Sync import Sync
+
+
+# reset the sync state
+class SyncReset(graphene.Mutation):
+    """
+    Reset the sync state back to its persisted value
+    """
+
+    # inputs
+    class Arguments:
+        # the viewport
+        viewport = graphene.Int(required=True)
+
+    # the result is the updated view
+    sync = graphene.Field(Sync)
+
+    # the mutator
+    @staticmethod
+    def mutate(root, info, viewport):
+        """
+        Reset the sync state
+        """
+        # get the store
+        store = info.context["store"]
+        # ask it to toggle the selection
+        sync = store.syncReset(viewport=viewport)
+        # form the mutation resolution context
+        context = {"sync": sync}
+        # and resolve the mutation
+        return context
+
+
+# end of file
---
+++ pkg/gql/views/ZoomReset.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# externals
+import graphene
+import journal
+
+
+# response types
+from .Zoom import Zoom
+
+
+# reset the zoom state
+class ZoomReset(graphene.Mutation):
+    """
+    Reset the zoom state back to its persisted value
+    """
+
+    # inputs
+    class Arguments:
+        # the viewport
+        viewport = graphene.Int(required=True)
+
+    # the result is the updated view
+    zoom = graphene.Field(Zoom)
+
+    # the mutator
+    @staticmethod
+    def mutate(root, info, viewport):
+        """
+        Reset the zoom state
+        """
+        # get the store
+        store = info.context["store"]
+        # ask it to toggle the selection
+        zoom = store.zoomReset(viewport=viewport)
+        # form the mutation resolution context
+        context = {"zoom": zoom}
+        # and resolve the mutation
+        return context
+
+
+# end of file
