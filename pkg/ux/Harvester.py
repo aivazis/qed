1bd1c8bc49e2c84d0d4510337b846ef01f123f66
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-08 23:09:50 -0700

    qed.ux: the {harvester} can now {configure} a component by mirroring the configuration of a {reference} component

--- pkg/ux/Harvester.py
+++ pkg/ux/Harvester.py
@@ -14,8 +14,8 @@ class Harvester:
     # interface
     def harvest(self, component):
         """
-        Generate a sequence of (fully qualified trait name, trait value) for all properties
-        in the configuration tree anchored at {component}
+        Generate a sequence of (fully qualified trait name, trait value) pairs for all properties in
+        the configuration tree anchored at {component}
         """
         # initialize the pile
         configuration = {}
@@ -39,6 +39,32 @@ class Harvester:
 
         # all done
         return configuration
+
+    def configure(self, component, reference):
+        """
+        Mirror the {reference} configuration in {component}
+        """
+        # go through the {component} properties
+        for property in component.pyre_properties():
+            # get the name of the property
+            name = property.name
+            # get the reference value
+            value = getattr(reference, name)
+            # mirror it
+            setattr(component, name, value)
+
+        # now, go through the parts
+        for facility in component.pyre_facilities():
+            # get the name of the facility
+            name = facility.name
+            # get the implementations
+            src = getattr(reference, name)
+            tgt = getattr(component, name)
+            # and mirror the configuration
+            self.configure(component=tgt, reference=src)
+
+        # all done
+        return component
 
 
 # end of file
