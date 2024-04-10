3cb7982da11bf6b2aca2726629ab646d497e5afc
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-10 14:46:39 -0700

    qed.gql: added a mutation that adjusts the {sync} table {offsets}

---
+++ pkg/gql/views/SyncUpdateOffset.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# externals
+import graphene
+import journal
+
+
+# the result types
+from .Sync import Sync
+
+
+# remove a view from the pile
+class SyncUpdateOffset(graphene.Mutation):
+    """
+    Toggle the state of the measure layer of a dataset
+    """
+
+    # inputs
+    class Arguments:
+        # the update context
+        viewport = graphene.Int(required=True)
+        x = graphene.Int(required=True)
+        y = graphene.Int(required=True)
+
+    # the result is the new measure layer object
+    sync = graphene.Field(Sync)
+
+    # the range controller mutator
+    @staticmethod
+    def mutate(root, info, viewport, x, y):
+        """
+        Remove a reader from the pile
+        """
+        # get the store
+        store = info.context["store"]
+        # ask it for the measure layer of the dataset view
+        sync = store.syncSetAspect(viewport=viewport, aspect="offsets", value=(x, y))
+        # form the mutation resolution context
+        context = {"sync": sync}
+        # and resolve the mutation
+        return context
+
+
+# end of file
