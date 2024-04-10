bb93632e11290fb3a07d1eb72c933f235b6803ad
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-09 18:05:38 -0700

    ux/views: the {tileURI} generator now folds the {viewport} into the URI

--- ux/client/views/viz/viewer/index.js
+++ ux/client/views/viz/viewer/index.js
@@ -8,13 +8,13 @@
 export { Viewer } from './viewer'
 
 // tile URIs
-export const tileURI = ({ reader, dataset, channel, zoom }) => {
+export const tileURI = ({ viewport, reader, dataset, channel, zoom }) => {
     // unpack the zoom level
     const level = [-zoom.vertical, -zoom.horizontal]
     // normalize and build the tag
     const tag = level.map(z => Math.trunc(Math.max(z, 0))).join("x")
     // assemble the uri
-    const uri = [reader.api, dataset.name, channel.tag, tag].join("/")
+    const uri = [reader.api, viewport, dataset.name, channel.tag, tag].join("/")
     // and return it
     return uri
 }
