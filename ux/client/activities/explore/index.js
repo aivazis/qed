d277e17a19bef4df3b86b2ef644901e76a6d97b6
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-23 04:03:45 +0300

    ux/activities: each {activity} isn ow responsible for examining the current {location} and declaring itself the selected activity

--- ux/client/activities/explore/index.js
+++ ux/client/activities/explore/index.js
@@ -6,6 +6,7 @@
 
 // externals
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 // locals
 // widgets
@@ -18,11 +19,17 @@ import styles from './styles'
 
 // display the dataset selectors
 export const Explore = ({ size, style }) => {
+    // get the current location
+    const location = useLocation().pathname
+    // my url
+    const url = "/explore"
+    // check whether i'm the current activity
+    const current = location === url
     // mix my paint
     const paint = styles.activity(style)
     // and render
     return (
-        <Activity size={size} url="/explore" style={paint} >
+        <Activity size={size} url={url} current={current} style={paint} >
             <Icon />
         </Activity>
     )
