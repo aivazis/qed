9815286c2d1eec8699c0e11b606b894183d426ee
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-23 21:39:47 +0300

    ux/doc: found a new home for the {contents} of the {guide}

---
+++ ux/client/views/doc/contents/index.js
@@ -0,0 +1,23 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+// pull the pages
+import Intro from './intro'
+
+
+// publish the content map
+export const topics = [
+    //
+    { link: "intro", title: "Getting started", page: Intro },
+    { link: "archives", title: "Connect to data archives", page: null },
+    { link: "readers", title: "Loading datasets", page: null },
+    { link: "datasets", title: "Selecting datasets", page: null },
+    { link: "views", title: "Adjusting the view", page: null },
+    { link: "measure", title: "The measure layer", page: null },
+    { link: "panels", title: "Working with multiple data panels", page: null },
+]
+
+
+// end of file
