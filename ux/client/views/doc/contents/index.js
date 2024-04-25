2630be175bc35f00b2ba3638300a167e2b584f11
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-25 23:12:40 +0300

    ux/doc: added the table of {intro} subsections to the {topics} in the {contents}

--- ux/client/views/doc/contents/index.js
+++ ux/client/views/doc/contents/index.js
@@ -10,7 +10,16 @@ import Intro from './intro'
 // publish the content map
 export const topics = [
     //
-    { link: "intro", title: "Getting started", page: Intro },
+    {
+        link: "intro", title: "Getting started", page: Intro,
+        contents: [
+            { link: "#dependencies", title: "Dependencies" },
+            { link: "#cloning", title: "Cloning the repositories" },
+            { link: "#mm", title: "Setting up the build system" },
+            { link: "#building", title: "Building" },
+            { link: "#launching", title: "Launching" },
+        ]
+    },
     { link: "archives", title: "Connect to data archives", page: null },
     { link: "readers", title: "Loading datasets", page: null },
     { link: "datasets", title: "Selecting datasets", page: null },
