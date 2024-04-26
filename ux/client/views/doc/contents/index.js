945ba72e65c0d05e376648e5b795ea0d73cece8d
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-26 04:19:26 +0300

    ux/doc: added more {topics} in the {contents}

--- ux/client/views/doc/contents/index.js
+++ ux/client/views/doc/contents/index.js
@@ -3,8 +3,11 @@
 // michael a.g. aïvázis <michael.aivazis@para-sim.com>
 // (c) 1998-2024 all rights reserved
 
+
 // pull the pages
 import Intro from './intro'
+import Readers from './readers'
+import Archives from './archives'
 
 
 // publish the content map
@@ -13,15 +16,26 @@ export const topics = [
     {
         link: "intro", title: "Getting started", page: Intro,
         contents: [
-            { link: "#dependencies", title: "Dependencies" },
-            { link: "#cloning", title: "Cloning the repositories" },
-            { link: "#mm", title: "Setting up the build system" },
-            { link: "#building", title: "Building" },
-            { link: "#launching", title: "Launching" },
+            { link: "#intro.dependencies", title: "Dependencies" },
+            { link: "#intro.cloning", title: "Cloning the repositories" },
+            { link: "#intro.mm", title: "Setting up the build system" },
+            { link: "#intro.building", title: "Building" },
+            { link: "#intro.launching", title: "Launching" },
         ]
     },
-    { link: "archives", title: "Connect to data archives", page: null },
-    { link: "readers", title: "Loading datasets", page: null },
+    {
+        link: "readers", title: "Loading datasets", page: Readers,
+        contents: [
+            { link: "#readers.nisar", title: "Connecting a NISAR reader" }
+        ]
+    },
+    {
+        link: "archives", title: "Connecting to data archives", page: Archives,
+        contents: [
+            { link: "#archives.local", title: "Local" },
+            { link: "#archives.s3", title: "S3" },
+        ]
+    },
     { link: "datasets", title: "Selecting datasets", page: null },
     { link: "views", title: "Adjusting the view", page: null },
     { link: "measure", title: "The measure layer", page: null },
