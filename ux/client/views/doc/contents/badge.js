e005536cef1b79857dee6eeb3acc7d4488e8e11c
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-04-26 04:17:19 +0300

    ux/doc: added {badge}, a container for inlining icons from {shapes}

---
+++ ux/client/views/doc/contents/badge.js
@@ -0,0 +1,40 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2024 all rights reserved
+
+
+// external
+import React from 'react'
+import styled from 'styled-components'
+
+// project
+// paint
+import { theme } from '~/palette'
+
+
+// a container of shapes
+export const Badge = ({ size = 12, children }) => {
+    // size the shape
+    const shrink = `scale(${size / 1000})`
+    // render
+    return (
+        <Housing>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width={size} height={size} >
+                <g transform={shrink} >
+                    {children}
+                </g>
+            </svg>
+        </Housing>
+    )
+}
+
+
+// the housing
+const Housing = styled.span`
+    padding: 0.0em 0.5em 0.0em 0.5em;
+    background-color: ${props => theme.page.active};
+`
+
+
+// end of file
