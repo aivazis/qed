4a55921d4c3ad968dcae5f970ce0c2a239314cd7
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-06-05 12:59:35 +0300

    ux/views: primed the {flow} panel with a {camera} and a cursor {grid}

--- ux/client/views/viz/flow/flow.js
+++ ux/client/views/viz/flow/flow.js
@@ -7,12 +7,46 @@
 // externals
 import React from 'react'
 
+// project
+import { Camera, Compass } from '~/widgets'
+
+// local
+// components
+import { Grid } from './grid'
+// paint
+import styles from './styles'
+
 
 // the flow panel
 export const Flow = ({ viewport, view }) => {
+    // build a reference to my container so we can measure it and install listeners
+    const ref = React.useRef(null)
+
+    // clear the node selection
+    const clearSelection = () => {
+        // all done
+        return
+    }
+
+    // assemble the canvas behaviors
+    const behaviors = {
+        // clear the selection
+        onClick: clearSelection,
+    }
+
     // render
     return (
-        <div>{`${viewport}: oh! the flow...`}</div>
+        <section ref={ref} tabIndex="-1" style={styles.panel}>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
+                {...styles.canvas} {...behaviors}
+            >
+                <Camera ref={ref} viewport={viewport}>
+                    <Compass />
+                    <Grid />
+                </Camera>
+            </svg>
+        </section>
+
     )
 }
 
