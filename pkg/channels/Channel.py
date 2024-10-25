80a2d20f12379862cd5b5a0ecd50ace85a836d76
Michael Aïvázis <michael.aivazis@para-sim.com>
2024-10-25 08:22:31 +0300

    qed.channels: primed the new home of the flow based channel implementations

---
+++ pkg/channels/Channel.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# support
+import qed
+import journal
+
+
+# channels are visualization pipeline fragments, i.e. partial flows
+class Channel(qed.flow.workflow, family="qed.channels.channel"):
+    """
+    A visualization pipeline fragment that is suitable for rendering data tiles
+
+    The base channel contributes the encoder that generates the image tile that is sent to the
+    client
+    """
+
+    # products
+    # expose the output node; everything else is internal, as far as clients are concerned
+    # lean on the protocols for sensible defaults
+    bmp = qed.protocols.products.image.output()
+    bmp.doc = "the final rendered image of the channel"
+
+    # factories
+    codec = qed.protocols.factories.codec()
+    codec.doc = "the encoder of the data tile as an image to be rendered by the client"
+
+    # framework hooks
+    def pyre_configured(self, **kwds):
+        """
+        Hook invoked after configuration is finished
+        """
+        # wire my output into the flow
+        self.codec.bmp = self.bmp
+        # all done
+        return super().pyre_configured(**kwds)
+
+
+# end of file
---
+++ pkg/channels/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2024 all rights reserved
+
+
+# channels are visualization pipeline fragments, i.e. partial flows
+from .Channel import Channel as channel
+
+
+# end of file
