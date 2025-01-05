9c3ef3eb9e20c973060ac36e0eea25a457d7bab9
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-01-05 08:12:31 +0200

    meta: happy new year

--- ux/client/views/status/index.js
+++ ux/client/views/status/index.js
@@ -1,7 +1,7 @@
 // -*- web -*-
 //
 // michael a.g. aïvázis <michael.aivazis@para-sim.com>
-// (c) 1998-2024 all rights reserved
+// (c) 1998-2025 all rights reserved
 
 
 // externals
@@ -24,7 +24,7 @@ export const Status = () => (
 
         {/* the box with the copyright note */}
         <Colophon author="Michael&nbsp;Aïvázis" link="https://github.com/aivazis"
-            span="1998-2024"
+            span="1998-2025"
             style={styles.colophon} />
 
     </footer>
