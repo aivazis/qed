f06a10e1cbeac4fe34b1aef91a2800efda0156f5
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-10-01 09:30:59 +0300

    ux/views: primed the {earth} access based virtual dataset archive

---
+++ ux/client/views/explorer/archive/earth.js
@@ -0,0 +1,168 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+
+// external
+import React from 'react'
+import { graphql, useMutation } from 'react-relay/hooks'
+
+// local
+// hooks
+import { useSetActiveView } from '../explorer/useSetActiveView'
+// components
+import { Busy } from './busy'
+import { Panel } from './panel'
+import { EnabledConnect, DisabledConnect, Cancel } from './buttons'
+import { TypeSelector } from './type'
+import { Name } from './name'
+import { Form, Body, Error } from '../form'
+
+// a earth access data archive
+export const Earth = ({ view, setType, hide }) => {
+    // error placeholder
+    const [error, setError] = React.useState(null)
+    // set up my state
+    const [form, setForm] = React.useState({
+        // the nickname
+        name: "",
+    })
+    // get the view mutator
+    const decorate = useSetActiveView()
+    // build the mutation request
+    const [request, isInFlight] = useMutation(connectMutation)
+    // set up the state update
+    const update = (field, value) => {
+        // clear any errors
+        setError(null)
+        // replace my state
+        setForm(old => {
+            // with
+            const clone = {
+                // a copy of the old state
+                ...old,
+                // with the value of the given field replaced with the new one
+                [field]: value,
+            }
+            // hand it off
+            return clone
+        })
+        // if the field is the archive nickname
+        if (field === "name") {
+            // update the uri of the archive in the current view
+            view.archive.uri = `earth:${value}`
+            // and set it
+            decorate(view)
+        }
+        // all done
+        return
+    }
+    // set up the archive connector
+    const connect = () => {
+        // if there is already a pending operation
+        if (isInFlight) {
+            // skip this update
+            return
+        }
+        // otherwise, collect the current state
+        const name = form.name
+        // send the mutation to the server
+        request({
+            // input
+            variables: {
+                // the payload
+                name: `earth:${name}`,
+                uri: `earth:${name}`,
+            },
+            // updater
+            updater: store => {
+                // get the root field of the query result
+                const payload = store.getRootField("connectArchive")
+                // if it's trivial
+                if (!payload) {
+                    // raise an issue
+                    throw new Error("could not connect to the data archive")
+                }
+                // ask for the new archive
+                const archive = payload.getLinkedRecord("archive")
+                // get the session manager
+                const qed = store.get("QED")
+                // get its connected archives
+                const archives = qed.getLinkedRecords("archives")
+                // add the new one to the pile
+                qed.setLinkedRecords([...archives, archive], "archives")
+                // all done
+                return
+            },
+            // when done
+            onCompleted: data => {
+                // clear the error
+                setError(null)
+                // remove the form from the view
+                hide()
+                // all done
+                return
+            },
+            // if something went wrong
+            onError: error => {
+                // record the error
+                setError([
+                    `got: ${error}`,
+                    `while connecting to the data archive '${name}'`,
+                ])
+                // all done
+                return
+            }
+        })
+    }
+    // build a handler that removes the form from view
+    const cancel = evt => {
+        // stop this event from bubbling up
+        evt.stopPropagation()
+        // and quash any side effects
+        evt.preventDefault()
+        // remove the view in my viewport from the pile
+        hide()
+        // all done
+        return
+    }
+    // determine whether i have enough information to make the connection
+    const ready = (
+        !isInFlight &&
+        form.name !== null && form.name.length
+    )
+    // use this to figure out which button to render
+    const Connect = ready ? EnabledConnect : DisabledConnect
+    // render
+    return (
+        <Panel>
+            <Form>
+                <Body>
+                    <TypeSelector value="earth" update={setType} />
+                    <Name value={form.name} update={update} />
+                </Body>
+            </Form>
+            <Connect connect={connect} />
+            {!isInFlight && <Cancel onClick={cancel}>cancel</Cancel>}
+            {error && <Error errors={error} />}
+            {isInFlight && <Busy />}
+        </Panel>
+    )
+}
+
+
+// the mutation that connects an earth access archive
+const connectMutation = graphql`
+    mutation earthArchiveMutation($name: String!, $uri: String!) {
+        connectArchive(name: $name, uri: $uri) {
+            archive {
+                id
+                name
+                readers
+            }
+        }
+    }
+`
+
+// end of file
