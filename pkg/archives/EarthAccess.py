cd571f3b4f78e5b70828378f97ea8fdcf8a0c3ac
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-10-01 09:16:09 +0300

    qed.archives: primed {earth}, a collection of datasets accessible via {earthaccess}

---
+++ pkg/archives/EarthAccess.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+# support
+import qed
+import journal
+
+
+# an earth access data archive
+class EarthAccess(
+    qed.component, family="qed.archives.earth", implements=qed.protocols.archive
+):
+    """
+    A data archive that consists of datasets available through earth access
+    """
+
+    # user configurable state
+    uri = qed.properties.uri()
+    uri.default = qed.primitives.uri(scheme="earth", address=())
+    uri.doc = "the archive identifier"
+
+    # constants
+    readers = ("nisar",)
+
+    # hooks
+    @classmethod
+    def isSupported(cls):
+        """
+        Check whether there is runtime support for this archive type
+        """
+        # attempt to
+        try:
+            # access the external packages we need
+            import earthaccess
+        # if anything goes wrong
+        except ImportError as error:
+            # no dice
+            return False
+        # otherwise, chances are good the runtime support is present
+        return True
+
+    # constants
+    tag = "earth"
+    label = "earth-access"
+
+
+# end of file
