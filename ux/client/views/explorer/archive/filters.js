b3a6ebab1e0530a3f2157f17317b30dd298c5786
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-10-03 17:24:54 +0300

    ux/views: added a {filter} selector to the {earth} archive connector in {explorer}

---
+++ ux/client/views/explorer/archive/filters.js
@@ -0,0 +1,77 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+
+// externals
+import React from 'react'
+
+
+// local
+// components
+import { Field, Values, enumValue } from '../form'
+
+// the set of supported filters
+export const Filters = ({ value, update, types }) => {
+    // validate my contents
+    const ok = value.size ? "ok" : ""
+    // make a handler that sets the region type
+    const toggle = type => {
+        // if {value} already contains this filter type
+        if (value.has(type)) {
+            // build a handler that removes it
+            return () => {
+                // remove the value
+                value.delete(type)
+                // update the form
+                update("filter", value)
+                // all done
+                return
+            }
+        }
+        // otherwise, build a handler that adds it
+        return () => {
+            // add the value
+            value.add(type)
+            // update the form
+            update("filter", type)
+            // all done
+            return
+        }
+    }
+    // render
+    return (
+        <Field name="filters" value={ok} tip="select the search filters">
+            <Values>
+                {Object.keys(types).map(type => (
+                    <Type
+                        key={type}
+                        name={type} rep={type} current={value} toggle={toggle} />
+                ))}
+            </Values>
+        </Field>
+    )
+}
+
+
+// the filter type
+const Type = ({ name, rep, current, toggle }) => {
+    // assemble the behaviors
+    const behaviors = {
+        onClick: toggle(rep),
+    }
+    // deduce my state
+    const state = current.has(rep) ? "selected" : "enabled"
+    // pick a field selector based on my state
+    const Selector = enumValue(state)
+    // and render it
+    return (
+        <Selector {...behaviors}>
+            {name}
+        </Selector>
+    )
+}
+
+
+// end of file
