2560276bb5890dcbb2deebefcb361aecb78ab18c
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-10-07 09:13:50 +0300

    mm: added builders for {noble} to the {native} pile

--- .mm/qed.mm
+++ .mm/qed.mm
@@ -20,6 +20,7 @@ qed.docker-images := \
     qed.dev.native.jammy-clang qed.dev.native.jammy-gcc  \
     qed.dev.native.lunar-clang qed.dev.native.lunar-gcc  \
     qed.dev.native.mantic-clang qed.dev.native.mantic-gcc  \
+    qed.dev.native.noble-clang qed.dev.native.noble-gcc  \
     qed.dev.micromamba.noble-clang qed.dev.micromamba.noble-gcc  \
     qed.dev.micromamba.plucky-clang qed.dev.micromamba.plucky-gcc  \
 
