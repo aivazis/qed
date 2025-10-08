40837b1e6d5609682418f32bb6bfe44914b70e53
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-10-08 21:52:24 +0300

    qed.archives: added {limit}, {collection}, and {granule} as {earth}access filters

---
+++ pkg/archives/DataCollection.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+# support
+import qed
+import journal
+
+# superclass
+from .Filter import Filter
+
+
+# a point on the surface of a sphere
+class DataCollection(Filter, family="qed.archives.filters.collection"):
+    """
+    A filter for earthdata searches that identifies the data collection that matching datasets
+    should belong to
+    """
+
+    # user-configurable state
+    shortName = qed.properties.str()
+    shortName.doc = "the short name of the collection"
+
+    conceptId = qed.properties.str()
+    conceptId.doc = "the concept id of the collection"
+
+    # implementation details
+    # visitor support
+    def onEarthAccess(self, **kwds):
+        """
+        Generate the required representation so an {EarthAccess} archive can filter its contents
+        """
+        # unpack my state
+        shortName = self.shortName
+        conceptId = self.conceptId
+        # if the name is non-trivial
+        if shortName:
+            # produce the {earthaccess} keyword
+            yield (
+                # the name
+                "short_name",
+                # the contents
+                shortName,
+            )
+        # if the concept id is non-trivial
+        if conceptId:
+            # produce the {earthaccess} keyword
+            yield (
+                # the name
+                "concept_id",
+                # the contents
+                conceptId,
+            )
+
+        # all done
+        return
+
+
+# end of file
---
+++ pkg/archives/DataGranule.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2025 all rights reserved
+
+
+# support
+import qed
+import journal
+
+# superclass
+from .Filter import Filter
+
+
+# a point on the surface of a sphere
+class DataGranule(Filter, family="qed.archives.filters.granule"):
+    """
+    A filter for earthdata searches that identifies a pattern to match against the granule name
+    """
+
+    # user-configurable state
+    pattern = qed.properties.str()
+    pattern.doc = "a pattern to match against the granule name"
+
+    # implementation details
+    # visitor support
+    def onEarthAccess(self, **kwds):
+        """
+        Generate the required representation so an {EarthAccess} archive can filter its contents
+        """
+        # unpack my state
+        pattern = self.pattern
+        # if the pattern is non-trivial
+        if pattern:
+            # produce the {earthaccess} keyword
+            yield (
+                # the pattern
+                "granule_name",
+                # the contents
+                pattern,
+            )
+
+        # all done
+        return
+
+
+# end of file
