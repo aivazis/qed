94b108d59acb06f8faa991327f8db888cf049d3b
Michael Aïvázis <michael.aivazis@para-sim.com>
2025-11-29 21:31:09 -0800

    ux/client: added {lazysizes} configuration; for now...

---
+++ ux/client/lazysizes.config.js
@@ -0,0 +1,17 @@
+
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2025 all rights reserved
+
+
+// configure lazysizes
+window.lazySizesConfig = {
+    loadMode: 1,           // only load when visible
+    expand: -20,           // require tile to actually be entering view
+    expFactor: 1,          // prevents auto-expansion of visible radius
+    preloadAfterLoad: false,
+}
+
+
+// end of file
