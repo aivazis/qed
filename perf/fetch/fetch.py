c951655042a123ecbd826fb2d1e673fa49d0dd2a
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-04-07 19:09:47 +0300

    qed.perf: started fleshing out the {fetch} performance tool

--- perf/fetch/fetch.py
+++ perf/fetch/fetch.py
@@ -7,6 +7,7 @@
 
 # support
 import qed
+import journal
 
 
 # the driver
@@ -15,12 +16,102 @@ class Fetch(qed.application, family="qed.apps.perf.fetch"):
     Measure tile creation performance
     """
 
+    # the path to the file
+    uri = qed.properties.uri()
+    uri.default = "rslc.h5"
+    uri.doc = "the path to the sample file, assumed to be a NISAR RSLC"
+
     # interface
     @qed.export
     def main(self, *args, **kwds):
         """
         The main entry point
         """
+        # open the file
+        rslc = qed.h5.read(uri=str(self.uri))
+        # navigate to the {HH} dataset
+        hh = rslc.science.LSAR.RSLC.swaths.frequencyA.HH
+        # zoom
+        self.zoom(dataset=hh, level=0)
+        # all done
+        return 0
+
+    # metamethods
+    def __init__(self, **kwds):
+        # chain up
+        super().__init__(**kwds)
+        # make a couple of timers
+        self.wall = qed.timers.wall("qed.perf.fetch.wall")
+        self.cpu = qed.timers.cpu("qed.perf.fetch.cpu")
+        # and a channel
+        self.channel = journal.info("qef.perf.fetch")
+        # all done
+        return
+
+    # implementation details
+    def zoom(self, dataset, level=0):
+        """
+        Generate a tile at the given zoom {level}
+        """
+        # get my channel
+        channel = self.channel
+        # and my timers
+        wall = self.wall
+        cpu = self.cpu
+
+        # pick a starting location
+        origin = qed.libpyre.grid.Index2D(index=(0, 0))
+        # report
+        channel.log(f"origin: {origin}")
+
+        # if the dataset is chunked
+        if dataset.dcpl.getLayout().name == "chunked":
+            # get its rank
+            rank = len(dataset.shape)
+            # and use the chunk shape as the tile shape
+            tile = dataset.dcpl.getChunk(rank=rank)
+            # report
+            channel.log(f"chunked; using tile: {tile}")
+        # otherwise
+        else:
+            # set the tile to 512 x 512
+            tile = 512, 512
+            # report
+            channel.log(f"not chunked; using tile: {tile}")
+        # turn this in to a grid shape
+        shape = qed.libpyre.grid.Shape2D(shape=tile)
+        # report
+        channel.log(f"shape: {shape}")
+
+        # build the grid layout
+        packing = qed.libpyre.grid.Canonical2D(shape=shape)
+        # allocate the storage
+        storage = qed.libpyre.memory.ComplexFloatHeap(cells=shape.cells)
+        # make a grid
+        grid = qed.libpyre.grid.ComplexFloatHeapGrid2D(packing=packing, storage=storage)
+        # report
+        channel.log(f"grid: {grid}")
+
+        # start the timers
+        wall.start()
+        cpu.start()
+
+        # read the data
+        dataset.read()
+
+        # stop the timers
+        cpu.stop()
+        wall.stop()
+
+        # report
+        channel.line(f"zoom: {level}")
+        channel.indent()
+        channel.line(f"in {cpu.ms():.3f} ms ({cpu.type})")
+        channel.line(f"in {wall.ms():.3f} ms ({wall.type})")
+        channel.outdent()
+        # flush
+        channel.log()
+
         # all done
         return 0
 
