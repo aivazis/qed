13ea4a66f3cbe1f3be9d21559e1c1b37bbb101fe
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-04-19 23:40:28 +0300

    qef.readers: cosmetics in the {unw} product in {nisar}

--- pkg/readers/nisar/products/UNW.py
+++ pkg/readers/nisar/products/UNW.py
@@ -34,11 +34,11 @@ class UNW(Product, family="qed.datasets.nisar.products.unw"):
         """
         # generate cursor information
         yield from self.cursor(pixel=pixel)
-        # get my channels
-        channels = self.channels
+
         # get the value of the {pixel}
         _, _, value = self.profile(points=[pixel])[0]
-
+        # get my channels
+        channels = self.channels
         # now, go through my channels
         for name, channel in channels.items():
             # and ask each one for {value} representations
