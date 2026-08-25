5da207012243dd8726b799ec7f261ed19781273f
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-08-26 00:14:03 +0300

    qed.nexus: tile tasks rebuild their data sources and spool their renders in worker processes

---
+++ pkg/nexus/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2026 all rights reserved
+
+
+# publish
+# the local exceptions
+from . import exceptions
+
+# the unit of work
+from .Tile import Tile as tile
+
+# the parking place for rendered payloads
+from .Spool import Spool as spool
+
+# end of file
