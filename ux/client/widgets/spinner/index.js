745f23b3b6d104fc38708510d5a7fc1ae3c1cffb
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-09-19 15:24:29 +0300

    qed.ux: a spinner widget, so that every place that waits looks like every other

---
+++ ux/client/widgets/spinner/index.js
@@ -0,0 +1,11 @@
+// -*- web -*-
+//
+// michael a.g. aïvázis <michael.aivazis@para-sim.com>
+// (c) 1998-2026 all rights reserved
+
+
+// publish
+export { Spinner } from './spinner'
+
+
+// end of file
